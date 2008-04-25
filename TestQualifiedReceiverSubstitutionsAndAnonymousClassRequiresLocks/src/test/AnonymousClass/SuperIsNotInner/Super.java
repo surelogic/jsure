72573d9d4f070cc0c2334ea4cb66871bf4a86adb
Aaron Greenhouse <aaron.greenhouse@surelogic.com>
2008-04-25 19:08:15 +0000

    Test that "held as" messages work correctly for lock preconditions in anonymous class expressions.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/regression@7587 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ TestQualifiedReceiverSubstitutionsAndAnonymousClassRequiresLocks/src/test/AnonymousClass/SuperIsNotInner/Super.java
@@ -0,0 +1,34 @@
+package test.AnonymousClass.SuperIsNotInner;
+
+import com.surelogic.Borrowed;
+import com.surelogic.RegionLock;
+import com.surelogic.RegionLocks;
+import com.surelogic.RequiresLock;
+import com.surelogic.SingleThreaded;
+
+@RegionLocks({
+  @RegionLock("COUNT is lockCount protects count"),
+  @RegionLock("F is lockF protects f")
+})
+public class Super {
+  public final Object lockF = new Object();
+  public int f;
+  
+  public static final Object lockCount = new Object();
+  public static int count = 0;
+  
+  @RequiresLock("COUNT")
+  public static void accessCount() {}
+  
+  @RequiresLock("F")
+  @Borrowed("this")
+  public void accessF() {}
+  
+  @SingleThreaded
+  @Borrowed("this")
+  @RequiresLock("COUNT")
+  public Super() {
+    accessCount();
+    accessF();
+  }
+}
