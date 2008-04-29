103076efec0230b3a1038c773f61e1d38e53edca
Edwin Chan <edwin.chan@surelogic.com>
2008-04-29 14:41:35 +0000

    Updated to match IAdHoc
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@7627 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/client/eclipse/AdHocGlue.java
+++ src/com/surelogic/jsure/client/eclipse/AdHocGlue.java
@@ -3,10 +3,18 @@ package com.surelogic.jsure.client.eclipse;
 import java.io.File;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.concurrent.*;
 
 import com.surelogic.adhoc.AbstractAdHocGlue;
 
 public final class AdHocGlue extends AbstractAdHocGlue {
+	private static final ExecutorService exec = 
+        Executors.newSingleThreadExecutor();
+	
+	public Executor getExecutor() {
+		return exec;
+	}
+	
 	public Connection getConnection() throws SQLException {
 		return Data.getConnection();
 	}
