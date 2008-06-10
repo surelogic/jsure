3757faf9d6d93c972f7825bc311c26d7c297720f
Edwin Chan <edwin.chan@surelogic.com>
2008-06-10 15:09:35 +0000

    Minor changes to method names and parameters
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@8155 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/client/eclipse/TestListener.java
+++ src/com/surelogic/jsure/client/eclipse/TestListener.java
@@ -4,12 +4,12 @@ import com.surelogic.jsure.xml.*;
 
 public class TestListener extends AbstractXMLResultListener {
 	@Override
-	protected void define(Entity e) {
+	protected void define(int id, Entity e) {
 		// TODO Auto-generated method stub
 	}
 
 	@Override
-	protected void handleDanglingRef(String from, Entity to) {
+	protected void handleRef(String from, int fromId, Entity to) {
 		// TODO handle
 		System.out.println("Handled "+to+" ref from "+from+" to "+to.getId());
 	}	
