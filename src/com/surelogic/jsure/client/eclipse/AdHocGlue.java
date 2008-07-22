e24d8bc30cee237b26b79d6129b5a329bdb175a6
Tim Halloran <tim.halloran@surelogic.com>
2008-07-22 18:34:21 +0000

    Working on gettting VisualVM to work again.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@8848 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/client/eclipse/AdHocGlue.java
+++ src/com/surelogic/jsure/client/eclipse/AdHocGlue.java
@@ -3,18 +3,21 @@ package com.surelogic.jsure.client.eclipse;
 import java.io.File;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.concurrent.*;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-import com.surelogic.adhoc.AbstractAdHocGlue;
+import com.surelogic.adhoc.IAdHoc;
 
-public final class AdHocGlue extends AbstractAdHocGlue {
-	private static final ExecutorService exec = 
-        Executors.newSingleThreadExecutor();
-	
+public final class AdHocGlue implements IAdHoc {
+
+	private static final ExecutorService exec = Executors
+			.newSingleThreadExecutor();
+
 	public Executor getExecutor() {
 		return exec;
 	}
-	
+
 	public Connection getConnection() throws SQLException {
 		return Data.getInstance().getConnection();
 	}
