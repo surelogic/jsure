e5620452f923917e99802c0faa5f7874aaaa0a29
Tim Halloran <tim.halloran@surelogic.com>
2008-07-23 15:35:53 +0000

    Working on NetBeans setup
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@8879 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/client/eclipse/Data.java
+++ src/com/surelogic/jsure/client/eclipse/Data.java
@@ -6,42 +6,29 @@ import com.surelogic.jsure.schema.JSureSchemaData;
 
 public final class Data extends DerbyConnection {
 
-	private Data() {
-		// no instances
+	private static final Data INSTANCE = new Data();
+
+	public static Data getInstance() {
+		INSTANCE.loggedBootAndCheckSchema();
+		return INSTANCE;
 	}
 
-	private static final String SCHEMA_NAME = "JSURE";
-	private static final String DATABASE_DIR = "db";
-
-	@Override
-	protected boolean deleteDatabaseOnStartup() {
-		return false;
+	private Data() {
+		// singleton
 	}
 
 	@Override
 	protected String getDatabaseLocation() {
-		return Activator.getDefault().getLocation(DATABASE_DIR);
+		return Activator.getDefault().getLocation(DATABASE_PATH_FRAGMENT);
+	}
+
+	@Override
+	protected String getSchemaName() {
+		return "JSURE";
 	}
 
 	@Override
 	protected SchemaData getSchemaLoader() {
 		return new JSureSchemaData();
 	}
-
-	@Override
-	protected String getSchemaName() {
-		return SCHEMA_NAME;
-	}
-
-	@Override
-	protected void setDeleteDatabaseOnStartup(final boolean bool) {
-		// Do nothing
-	}
-
-	private static final Data INSTANCE = new Data();
-
-	public static Data getInstance() {
-		return INSTANCE;
-	}
-
 }
