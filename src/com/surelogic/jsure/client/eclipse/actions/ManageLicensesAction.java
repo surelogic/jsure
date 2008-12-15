f8bde24393aa666c97f4b0c4ca9bce70ac98cefc
Tim Halloran <tim.halloran@surelogic.com>
2008-12-15 16:10:02 +0000

    Fixed menus to include servicability and licensing in the same place
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@10743 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/jsure/client/eclipse/actions/ManageLicensesAction.java
@@ -0,0 +1,29 @@
+package com.surelogic.jsure.client.eclipse.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+import com.surelogic.common.eclipse.SWTUtility;
+import com.surelogic.common.eclipse.dialogs.ManageLicensesDialog;
+
+public final class ManageLicensesAction implements
+		IWorkbenchWindowActionDelegate {
+
+	public void dispose() {
+		// nothing to do
+	}
+
+	public void init(IWorkbenchWindow window) {
+		// nothing to do
+	}
+
+	public void run(IAction action) {
+		ManageLicensesDialog.open(SWTUtility.getShell());
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+		// nothing to do
+	}
+}
