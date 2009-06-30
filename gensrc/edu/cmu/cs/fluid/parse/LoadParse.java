2342a5696e69db0ca9067d61a649d5ded66ccaa8
Edwin Chan <edwin.chan@surelogic.com>
2009-06-30 19:30:59 +0000

    Deleted generated files
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/fluid@12621 d02ae147-ee12-4a35-9452-91c89e8b2592

--- gensrc/edu/cmu/cs/fluid/parse/LoadParse.java
+++
@@ -1,7 +0,0 @@
-// Generated code.  Do *NOT* edit!
-package edu.cmu.cs.fluid.parse;
-
-@SuppressWarnings("all")
-public class LoadParse {
-  private static Ellipsis xEllipsis = Ellipsis.prototype;
-}
