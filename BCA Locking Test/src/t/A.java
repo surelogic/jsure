adcc17af090250e9a6d1089037954e7ee0cf07ed
Aaron Greenhouse <aaron.greenhouse@surelogic.com>
2009-07-30 17:51:33 +0000

    Tests that BCA-based elaboration doesn't screw up locking results now that locks are purely based on elaborated effects.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/regression@12765 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ BCA Locking Test/src/t/A.java
@@ -0,0 +1,65 @@
+package t;
+
+import com.surelogic.Aggregate;
+import com.surelogic.Borrowed;
+import com.surelogic.Region;
+import com.surelogic.RegionEffects;
+import com.surelogic.RegionLock;
+import com.surelogic.Regions;
+import com.surelogic.Unique;
+
+@Regions({
+  @Region("protected A_R1"),
+  @Region("protected A_R2")
+})
+@RegionLock("ALock is this protects A_R2")
+public class A {
+  @Unique
+  @Aggregate("B_R1 into A_R1, B_R2 into A_R2, Instance into Instance")
+  protected final B b = new B();
+  
+  @Borrowed("this")
+  @RegionEffects("none")
+  public A() {
+    super();
+  }
+
+  @Borrowed("this")
+  protected void doStuff1(final int v1, final int v2) {
+    this.b.c.d.e.f1 = v1;
+    // Needs ALock
+    this.b.c.d.e.f2 = v2;
+  }
+
+  @Borrowed("this")
+  protected void doStuff2(final int v1, final int v2) {
+    final B local_b = this.b;
+    final C local_c = local_b.c;
+    final D local_d = local_c.d;
+    final E local_e = local_d.e;
+    local_e.f1 = v1;
+    // Needs ALock
+    local_e.f2 = v2;
+  }
+
+  @Borrowed("this")
+  protected void doStuff3(final int v1, final int v2) {
+    final B local_b;
+    if (v1 > v2) {
+      local_b = B.getB();
+    } else {
+      local_b = this.b;
+    }
+    local_b.c.d.e.f1 = v1;
+    
+    /* Here we need lock this.ALock because local_b might cause aggregation
+     * into the receiver.
+     * 
+     * We also need local_b.BLock because local_b might be a random B object
+     * that is not aggregated into this A object, in which case the aggregation
+     * causes the use of f2 to be reflected as a use of the B_R2 region of the
+     * object referenced by local_b.
+     */
+    local_b.c.d.e.f2 = v2;
+  }
+}
---
+++ BCA Locking Test/src/t/D.java
@@ -0,0 +1,91 @@
+package t;
+
+import com.surelogic.Aggregate;
+import com.surelogic.Borrowed;
+import com.surelogic.Region;
+import com.surelogic.RegionEffects;
+import com.surelogic.RegionLock;
+import com.surelogic.RegionLocks;
+import com.surelogic.Regions;
+import com.surelogic.Unique;
+
+@Regions({
+  @Region("protected D_R1"),
+  @Region("protected D_R2"),
+  @Region("protected OTHER")
+})
+@RegionLocks({
+  @RegionLock("DLock is this protects D_R2"),
+  @RegionLock("OLock is this protects OTHER")
+})
+public class D {
+  @Unique
+  @Aggregate("f1 into D_R1, f2 into D_R2, Instance into Instance")
+  protected final E e = new E();
+
+  @Unique
+  @Aggregate("Instance into OTHER")
+  protected final E e2 = new E();
+  
+  @Borrowed("this")
+  @RegionEffects("none")
+  public D() {
+    super();
+  }
+  
+  public static D getD() {
+    return new D();
+  }
+  
+  @Borrowed("this")
+  protected void doStuff1(final int v1, final int v2) {
+    this.e.f1 = v1;
+    // Needs DLock
+    this.e.f2 = v2;
+  }
+  
+  
+  @Borrowed("this")
+  protected void doStuff2(final int v1, final int v2) {
+    final E local_e = this.e;
+    local_e.f1 = v1;
+    // Needs DLock
+    local_e.f2 = v2;
+  }
+  
+  @Borrowed("this")
+  protected void doStuff3(final int v1, final int v2) {
+    final E local_e;
+    if (v1 > v2) {
+      local_e = E.getE();
+    } else {
+      local_e = this.e;
+    }
+    local_e.f1 = v1;
+    
+    /* Here we need lock this.DLock because local_e might cause aggregation
+     * into the receiver.
+     */
+    local_e.f2 = v2;
+  }
+  
+  @Borrowed("this")
+  protected void doStuff4(final int v1, final int v2) {
+    final E local_e;
+    if (v1 > v2) {
+      local_e = this.e2;
+    } else {
+      local_e = this.e;
+    }
+    /* Here we need OLock because local_e could refer to the object referenced
+     * by this.e2.
+     */
+    local_e.f1 = v1;
+    
+    /* Here we need both the locks DLock and OLock because local_e could refer
+     * object referenced by either 'e' or 'e2', and thus we could be aggregated
+     * into either D_R2 or OTHER.
+     */
+    local_e.f2 = v2;
+  }
+}
