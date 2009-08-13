37bb0c0eb73f8b113e31054d5e1878aa652804c4
Aaron Greenhouse <aaron.greenhouse@surelogic.com>
2009-08-13 18:29:11 +0000

    Updated to include tests for volatile and final fields as the super regions of abstract regions.  Tests for cycles.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/regression@12804 d02ae147-ee12-4a35-9452-91c89e8b2592

--- NewRegionRules/src/test/Test.java
+++ NewRegionRules/src/test/Test.java
@@ -2,8 +2,27 @@ package test;
 
 import com.surelogic.Aggregate;
 import com.surelogic.InRegion;
+import com.surelogic.Region;
+import com.surelogic.Regions;
 import com.surelogic.Unique;
 
+@Regions({
+  // cannot have a final region as a parent
+  @Region("private R extends finalField" /* is UNASSOCIATED */),
+  
+  // cannot have a volatile region as a parent
+  @Region("private A extends volatileField" /* is UNASSOCIATED */),
+  
+  @Region("private B extends regularField" /* is CONSISTENT */),
+  
+  /* Cannot have a cycle of abstract regions because the binder won't bind
+   * forward references to abstract region names.  Can have a cycle if a field
+   * is involved.  Error is flagged on the @InRegion annotation that closes the 
+   * cycle.
+   */
+  @Region("private ZZZ extends topField" /* is CONSISTENT */)
+})
+@SuppressWarnings("unused")
 public class Test {
   /* A volatile field cannot be unique */
   
@@ -47,4 +66,18 @@ public class Test {
   @Unique
   @Aggregate("Instance into regularField" /* is CONSISTENT */)
   private final Object aggIntoRegular = new Object();
+  
+  
+  /* Cycle checking */
+  
+  // Create a cycle with an abstract region
+  @InRegion("ZZZ" /* is UNASSOCIATED */)
+  private int topField;
+  
+  // Create a cycle of two fields
+  @InRegion("otherOne" /* is CONSISTENT */)
+  private int thisOne;
+  
+  @InRegion("thisOne" /* is UNASSOCIATED */)
+  private int otherOne;
 }
