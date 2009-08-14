02ef1fe163f07c0bc27e8be83268ab801e747374
Aaron Greenhouse <aaron.greenhouse@surelogic.com>
2009-08-14 17:09:23 +0000

    Updated to reflect that fields can now be used as superregions.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/regression@12807 d02ae147-ee12-4a35-9452-91c89e8b2592

--- TestRegionSanityChecks/src/region/nonFieldParent/Test.java
+++ TestRegionSanityChecks/src/region/nonFieldParent/Test.java
@@ -6,9 +6,9 @@ import com.surelogic.Regions;
 @Regions({
   @Region("LocalRegion"),
   @Region("R1 extends LocalRegion" /* is CONSISTENT */), // GOOD: Parent is abstract
-  @Region("R2 extends field" /* is UNASSOCIATED */), // BAD: Parent is field
+  @Region("R2 extends field" /* is CONSISTENT */), // GOOD: Parent is field (Good as of 2009-08-13)
   @Region("R3 extends SuperRegion" /* is CONSISTENT */), // GOOD: Parent is abstract
-  @Region("R4 extends superField" /* is UNASSOCIATED */) // BAD: Parent is field
+  @Region("R4 extends superField" /* is CONSISTENT */) // BAD: Parent is field (Good as of 2009-08-13)
 })
 public class Test extends Super {
   int field;
