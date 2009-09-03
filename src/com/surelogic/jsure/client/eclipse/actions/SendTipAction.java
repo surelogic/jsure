ff91c3b4292cfa38096ecc8c32cd5ea7c0e0c3aa
Edwin Chan <edwin.chan@surelogic.com>
2009-09-03 18:54:17 +0000

    Added version to tool info
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@12889 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/client/eclipse/actions/SendTipAction.java
+++ src/com/surelogic/jsure/client/eclipse/actions/SendTipAction.java
@@ -5,9 +5,11 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
+import com.surelogic.common.eclipse.EclipseUtility;
 import com.surelogic.common.eclipse.SWTUtility;
 import com.surelogic.common.eclipse.dialogs.SendTipDialog;
 import com.surelogic.common.CommonImages;
+import com.surelogic.jsure.client.eclipse.Activator;
 
 public final class SendTipAction implements IWorkbenchWindowActionDelegate {
 
@@ -20,7 +22,8 @@ public final class SendTipAction implements IWorkbenchWindowActionDelegate {
 	}
 
 	public void run(IAction action) {
-		SendTipDialog.open(SWTUtility.getShell(), "JSure",
+		SendTipDialog.open(SWTUtility.getShell(), 
+				"JSure"+EclipseUtility.getVersion(Activator.getDefault()),
 				CommonImages.IMG_JSURE_LOGO);
 	}
 
