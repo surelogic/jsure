b02b8346b75b7822997898895661f800814dc28a
Tim Halloran <tim.halloran@surelogic.com>
2009-10-12 20:07:19 +0000

    Fixed perspectives to properly show modeling problems view.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@13074 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/perspectives/CodeVerificationPerspective.java
+++ src/com/surelogic/jsure/perspectives/CodeVerificationPerspective.java
@@ -2,19 +2,28 @@ package com.surelogic.jsure.perspectives;
 
 import org.eclipse.ui.*;
 
+import edu.cmu.cs.fluid.dcf.views.coe.ProblemsView;
+import edu.cmu.cs.fluid.dcf.views.coe.ResultsView;
+
 /**
  * Defines the JSure perspective within the workbench.
  */
 public final class CodeVerificationPerspective implements IPerspectiveFactory {
 
 	public void createInitialLayout(IPageLayout layout) {
-		final String statusView = "edu.cmu.cs.fluid.dcf.views.coe.ResultsView";
+		final String statusView = ResultsView.class.getName();
+		final String problemsView = ProblemsView.class.getName();
 		final String packageExplorer = "org.eclipse.jdt.ui.PackageExplorer";
 		final String editorArea = layout.getEditorArea();
 
 		final IFolderLayout belowEditorArea = layout.createFolder(
 				"belowEditorArea", IPageLayout.BOTTOM, 0.5f, editorArea);
 		belowEditorArea.addView(statusView);
+
+		final IPlaceholderFolderLayout problemsArea = layout
+				.createPlaceholderFolder("problemsArea", IPageLayout.BOTTOM,
+						0.5f, "belowEditorArea");
+		problemsArea.addPlaceholder(problemsView);
 
 		final IFolderLayout leftOfEditorArea = layout.createFolder(
 				"leftOfEditorArea", IPageLayout.LEFT, 0.3f, editorArea);
