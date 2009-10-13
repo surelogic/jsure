2281af58e1490721f214de5cb88703515a753c94
Tim Halloran <tim.halloran@surelogic.com>
2009-10-13 16:54:52 +0000

    Removed warnings
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/fluid@13087 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/edu/cmu/cs/fluid/java/analysis/AnalysisContext.java
+++ src/edu/cmu/cs/fluid/java/analysis/AnalysisContext.java
@@ -2,21 +2,22 @@ package edu.cmu.cs.fluid.java.analysis;
 
 import edu.cmu.cs.fluid.java.bind.IBinder;
 
-/** Keep track of a set of analyses and the binder in one class.
- * We may have more than one instance of this class around at any time.
+/**
+ * Keep track of a set of analyses and the binder in one class. We may have more
+ * than one instance of this class around at any time.
  */
 @Deprecated
 public class AnalysisContext {
-  public final IBinder binder;
-  public final TypeBasedAliasAnalysis tbAlias;
-  
-  private AnalysisContext(final IBinder b) {
-    binder = b;
-    tbAlias = new TypeBasedAliasAnalysis(b);
-  }
-  
-  public static AnalysisContext getContext(final IBinder b) {
-    // cache?
-    return new AnalysisContext(b);
-  }
+	public final IBinder binder;
+	public final TypeBasedAliasAnalysis tbAlias;
+
+	private AnalysisContext(final IBinder b) {
+		binder = b;
+		tbAlias = new TypeBasedAliasAnalysis(b);
+	}
+
+	public static AnalysisContext getContext(final IBinder b) {
+		// cache?
+		return new AnalysisContext(b);
+	}
 }
