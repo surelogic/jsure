ae97c661dbe691b96d81fea70abc90e612e3ae8e
Tim Halloran <tim.halloran@surelogic.com>
2009-11-04 22:49:53 +0000

    Working on upgrading the promises JAR file
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@13402 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/client/eclipse/LibResources.java
+++ src/com/surelogic/jsure/client/eclipse/LibResources.java
@@ -5,8 +5,21 @@ import java.io.InputStream;
 import java.net.URL;
 
 public class LibResources {
-	public static final String PATH = "/lib/";
+	/**
+	 * The name of the current promises Jar file.
+	 * <p>
+	 * If you change this you <i>must</i> add the current name to the {@code
+	 * PROMISES_JAR_OLD_VERSIONS} array.
+	 */
 	public static final String PROMISES_JAR = "promises-3.0.0.jar";
+
+	/**
+	 * Holds the names of the old library files that may need to be upgraded to
+	 * the new promises Jar file.
+	 */
+	public static final String[] PROMISES_JAR_OLD_VERSIONS = { "promises.jar" };
+
+	public static final String PATH = "/lib/";
 	public static final String PROMISES_JAR_PATHNAME = PATH + PROMISES_JAR;
 
 	public static InputStream getPromisesJar() throws IOException {
