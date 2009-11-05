5375b85daf76fb6ff984c72ba2db7ef6583ce963
Tim Halloran <tim.halloran@surelogic.com>
2009-11-05 20:05:35 +0000

    Working on vouches.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/fluid@13424 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/annotation/rules/VouchRules.java
+++ src/com/surelogic/annotation/rules/VouchRules.java
@@ -20,81 +20,96 @@ import edu.cmu.cs.fluid.sea.drops.promises.*;
 import edu.cmu.cs.fluid.tree.Operator;
 
 public class VouchRules extends AnnotationRules {
-  public static final String VOUCH = "Vouch";
-  
-  private static final AnnotationRules instance = new VouchRules();
-  
-  private static final Vouch_ParseRule vouchRule = new Vouch_ParseRule();
- 
-  private static final VouchProcessor vouchProcessor = new VouchProcessor();
-  
-  public static AnnotationRules getInstance() {
-    return instance;
-  }
-  
-  public static VouchPromiseDrop getVouchSpec(IRNode decl) {
-    return getDrop(vouchRule.getStorage(), decl);
-  }
-  
-  /**
-   * Returns the closest vouch applicable for the given IRNode, if any  
-   */
-  public static VouchPromiseDrop getEnclosingVouch(final IRNode n) {
-	  IRNode decl = VisitUtil.getClosestDecl(n);
-	  while (n != null) {		  
-		  Operator op = JJNode.tree.getOperator(decl);
-		  if (ClassBodyDeclaration.prototype.includes(op) || TypeDeclaration.prototype.includes(op)) {			 
-			  VouchPromiseDrop rv = getVouchSpec(decl);
-			  if (rv != null) {
-				  return rv;
-			  }
-		  }
-		  decl = VisitUtil.getEnclosingDecl(n);
-	  }
-	  return null;
-  }
-  
-  @Override
-  public void register(PromiseFramework fw) {
-    registerParseRuleStorage(fw, vouchRule);
-    Sea.getDefault().setProofInitializer(vouchProcessor);
-  }
-  
-  static class VouchProcessor implements Sea.ProofInitializer {
-	  public void init(Sea sea) {
-		  for(ResultDrop d : sea.getDropsOfType(ResultDrop.class)) {
-			  if (!d.isConsistent()) {
-				  VouchPromiseDrop vouch = getEnclosingVouch(d.getNode());
-				  if (vouch != null) {
-					  // TODO what to do now
-				  }
-			  }
-		  }
-	  } 
-  }
-  
-  static class Vouch_ParseRule 
-  extends DefaultSLAnnotationParseRule<VouchSpecificationNode, VouchPromiseDrop> {
-    protected Vouch_ParseRule() {
-      super(VOUCH, methodOrClassDeclOps, VouchSpecificationNode.class);
-    }
-    @Override
-    protected Object parse(IAnnotationParsingContext context, SLAnnotationsParser parser) throws RecognitionException {
-      return new VouchSpecificationNode(context.mapToSource(0), context.getAllText());
-    }
-    @Override
-    protected IPromiseDropStorage<VouchPromiseDrop> makeStorage() {
-      return SinglePromiseDropStorage.create(name(), VouchPromiseDrop.class);
-    }
-    @Override
-    protected IAnnotationScrubber<VouchSpecificationNode> makeScrubber() {
-      return new AbstractAASTScrubber<VouchSpecificationNode>(this) {
-        @Override
-        protected PromiseDrop<VouchSpecificationNode> makePromiseDrop(VouchSpecificationNode a) {
-          VouchPromiseDrop d = new VouchPromiseDrop(a);
-          return storeDropIfNotNull(getStorage(), a, d);          
-        }
-      };
-    }
-  }
+	public static final String VOUCH = "Vouch";
+
+	private static final AnnotationRules instance = new VouchRules();
+
+	private static final Vouch_ParseRule vouchRule = new Vouch_ParseRule();
+
+	private static final VouchProcessor vouchProcessor = new VouchProcessor();
+
+	public static AnnotationRules getInstance() {
+		return instance;
+	}
+
+	public static VouchPromiseDrop getVouchSpec(IRNode decl) {
+		return getDrop(vouchRule.getStorage(), decl);
+	}
+
+	/**
+	 * Returns the closest vouch applicable for the given IRNode, if any
+	 */
+	public static VouchPromiseDrop getEnclosingVouch(final IRNode n) {
+		IRNode decl = VisitUtil.getClosestDecl(n);
+		while (n != null) {
+			Operator op = JJNode.tree.getOperator(decl);
+			if (ClassBodyDeclaration.prototype.includes(op)
+					|| TypeDeclaration.prototype.includes(op)) {
+				VouchPromiseDrop rv = getVouchSpec(decl);
+				if (rv != null) {
+					return rv;
+				}
+			}
+			decl = VisitUtil.getEnclosingDecl(n);
+		}
+		return null;
+	}
+
+	@Override
+	public void register(PromiseFramework fw) {
+		registerParseRuleStorage(fw, vouchRule);
+		Sea.getDefault().setProofInitializer(vouchProcessor);
+	}
+
+	static class VouchProcessor implements Sea.ProofInitializer {
+		public void init(Sea sea) {
+			for (ResultDrop d : sea.getDropsOfType(ResultDrop.class)) {
+				if (!d.isConsistent()) {
+					VouchPromiseDrop vouch = getEnclosingVouch(d.getNode());
+					if (vouch != null) {
+						d.setVouched();
+						d.addTrustedPromise(vouch);
+						if (vouch.getAST() != null) {
+							final String vouchReason = vouch.getAST()
+									.getReason();
+							d.addSupportingInformation(vouchReason, null);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	static class Vouch_ParseRule
+			extends
+			DefaultSLAnnotationParseRule<VouchSpecificationNode, VouchPromiseDrop> {
+		protected Vouch_ParseRule() {
+			super(VOUCH, methodOrClassDeclOps, VouchSpecificationNode.class);
+		}
+
+		@Override
+		protected Object parse(IAnnotationParsingContext context,
+				SLAnnotationsParser parser) throws RecognitionException {
+			return new VouchSpecificationNode(context.mapToSource(0), context
+					.getAllText());
+		}
+
+		@Override
+		protected IPromiseDropStorage<VouchPromiseDrop> makeStorage() {
+			return SinglePromiseDropStorage.create(name(),
+					VouchPromiseDrop.class);
+		}
+
+		@Override
+		protected IAnnotationScrubber<VouchSpecificationNode> makeScrubber() {
+			return new AbstractAASTScrubber<VouchSpecificationNode>(this) {
+				@Override
+				protected PromiseDrop<VouchSpecificationNode> makePromiseDrop(
+						VouchSpecificationNode a) {
+					VouchPromiseDrop d = new VouchPromiseDrop(a);
+					return storeDropIfNotNull(getStorage(), a, d);
+				}
+			};
+		}
+	}
 }
--- src/edu/cmu/cs/fluid/sea/ResultDrop.java
+++ src/edu/cmu/cs/fluid/sea/ResultDrop.java
@@ -5,172 +5,181 @@ import java.util.*;
 import edu.cmu.cs.fluid.sea.xml.SeaSnapshot;
 
 /**
- * A code/model consistency result drop recording an analysis result in terms
- * of what promises are (partially or wholly) established in terms of a
- * (possibly empty) set of precondition promises.
+ * A code/model consistency result drop recording an analysis result in terms of
+ * what promises are (partially or wholly) established in terms of a (possibly
+ * empty) set of precondition promises.
  * <p>
  * Not intended to be subclassed.
  */
 @SuppressWarnings("unchecked")
 public class ResultDrop extends ProofDrop {
-  
-  /**
-   * Holds key that describes which message this result is.
-   */
-  private final String type;
-  
-  
-  /**
-   * @param t Key that describes which message this result drop represents.
-   */
-  public ResultDrop(String t) {
-	  if (t == null) {
-		  throw new RuntimeException();
-	  }
-	  type = t;
-  }
-  public ResultDrop() {
-	  this(null);
-  }
-	
-  /**
-   * The set of promise drops being checked, or established, by this result.
-   */
-  private Set<PromiseDrop> checks = new HashSet<PromiseDrop>();
 
-  /**
-   * The set of promise drops trusted by this result, its preconditions.
-   */
-  private Set<PromiseDrop> trusts = new HashSet<PromiseDrop>();
+	/**
+	 * Holds key that describes which message this result is.
+	 */
+	private final String type;
 
-  /**
-   * Map from "or" logic trust labels (String) to sets of drop promises.  One
-   * complete set of promises must be proved consistent for this result to
-   * be consistent.
-   */
-  private Map<String, Set<PromiseDrop>> or_TrustLabelToTrusts = new HashMap<String, Set<PromiseDrop>>(); // String -> Set (promise drops)
+	/**
+	 * @param t
+	 *            Key that describes which message this result drop represents.
+	 */
+	public ResultDrop(String t) {
+		if (t == null) {
+			throw new RuntimeException();
+		}
+		type = t;
+	}
 
-  /**
-   * Flags if this result indicates consistency with code.
-   */
-  private boolean consistent = false;
+	public ResultDrop() {
+		this(null);
+	}
 
-  /**
-   * Adds a promise to the set of promises this result establishes, or
-   * <i>checks</i>.
-   * 
-   * @param promise the promise being supported by this result
-   */
-  public void addCheckedPromise(PromiseDrop promise) {
-    checks.add(promise);
-    promise.addDependent(this);
-  }
-  
-  public void addCheckedPromises(Collection<? extends PromiseDrop> promises) {
-    //no null check -- fail-fast
-    for (PromiseDrop promise : promises) {
-//   Iterator promiseIter = promises.iterator();
-//    while (promiseIter.hasNext()) {
-//      PromiseDrop promise = (PromiseDrop) promiseIter.next();
-      addCheckedPromise(promise);
-    }
-  }
+	/**
+	 * The set of promise drops being checked, or established, by this result.
+	 */
+	private Set<PromiseDrop> checks = new HashSet<PromiseDrop>();
 
-  /**
-   * Adds a promise to the set of promises this result uses as a
-   * precondition, or <i>trusts</i>.
-   * 
-   * @param promise the promise being trusted by this result
-   */
-  public void addTrustedPromise(PromiseDrop promise) {
-    trusts.add(promise);
-    promise.addDependent(this);
-  }
-  
-  public void addTrustedPromises(Collection<? extends PromiseDrop> promises) {
-    //no null check -- fail-fast
-    for (PromiseDrop promise : promises) {
-//    Iterator promiseIter = promises.iterator();
-//    while (promiseIter.hasNext()) {
-//      PromiseDrop promise = (PromiseDrop) promiseIter.next();
-      addTrustedPromise(promise);
-    }
-  }
-  
-  /**
-   * Adds a promise to the set of promises this result uses as a
-   * precondition, or <i>trusts</i>.
-   * 
-   * @param promise the promise being trusted by this result
-   */
-  public void addTrustedPromise_or(String orKey, PromiseDrop promise) {
-    String key = orKey.intern();
-    Set<PromiseDrop> s = or_TrustLabelToTrusts.get(key);
-    if (s == null) {
-      s = new HashSet<PromiseDrop>();
-      or_TrustLabelToTrusts.put(key, s);
-    }
-    s.add(promise);
-    promise.addDependent(this);
-  }
+	/**
+	 * The set of promise drops trusted by this result, its preconditions.
+	 */
+	private Set<PromiseDrop> trusts = new HashSet<PromiseDrop>();
 
-  /**
-   * @return the set of promise drops established, or checked, by this result.
-   *   All members of the returned set will are of the PromiseDrop type.
-   */
-  public Set<? extends PromiseDrop> getChecks() {
-    return checks;
-  }
+	/**
+	 * Map from "or" logic trust labels (String) to sets of drop promises. One
+	 * complete set of promises must be proved consistent for this result to be
+	 * consistent.
+	 */
+	private Map<String, Set<PromiseDrop>> or_TrustLabelToTrusts = new HashMap<String, Set<PromiseDrop>>(); // String
+	// ->
+	// Set
+	// (promise
+	// drops)
 
-  /**
-   * Returns the preconditions of this result, including any "or" preconditions.
-   * However, using this call it is impossible to distingish "and" preconditions
-   * from "or"preconditions.
-   * 
-   * @return the set of promises trusted by this result, its preconditions.
-   *   All members of the returned set will are of the PromiseDrop type.
-   *
-   * @see #getTrusts() 
-   * @see #hasOrLogic()
-   * @see #get_or_TrustLabelSet()
-   * @see #get_or_Trusts(String)
-   */
-  public Set<? extends PromiseDrop> getTrustsComplete() {
-    Set<PromiseDrop> result = new HashSet<PromiseDrop>();
-    result.addAll(trusts);
-    if (hasOrLogic()) {
-      Set<String> orLabels = get_or_TrustLabelSet();
-      for (String orKey : orLabels) {
-        //String orKey = (String) i.next();
-        result.addAll(get_or_Trusts(orKey));
-      }
-    }
-    return result;
-  }
+	/**
+	 * Flags if this result indicates consistency with code.
+	 */
+	private boolean consistent = false;
 
-  /**
-   * Returns the preconditions of this result, this set does not include
-   * any "or" preconditions.  Use the "get_or_" methods to obtain
-   * those preconditions.
-   * 
-   * @return the set of promises trusted by this result, its preconditions.
-   *   All members of the returned set will are of the PromiseDrop type.
-   * 
-   * @see #hasOrLogic()
-   * @see #get_or_TrustLabelSet()
-   * @see #get_or_Trusts(String)
-   */
-  public Set<PromiseDrop> getTrusts() {
-    return trusts;
-  }
+	/**
+	 * Adds a promise to the set of promises this result establishes, or
+	 * <i>checks</i>.
+	 * 
+	 * @param promise
+	 *            the promise being supported by this result
+	 */
+	public void addCheckedPromise(PromiseDrop promise) {
+		checks.add(promise);
+		promise.addDependent(this);
+	}
 
-  /**
-   * Flags if this result has groups of "or" precondition sets which must
-   * be considered in the whole-program consistency proof.  Most results
-   * do not include "or" precondition logic.
-   * <p>
-   * Typical use of this method is:
-   * <pre>
+	public void addCheckedPromises(Collection<? extends PromiseDrop> promises) {
+		// no null check -- fail-fast
+		for (PromiseDrop promise : promises) {
+			// Iterator promiseIter = promises.iterator();
+			// while (promiseIter.hasNext()) {
+			// PromiseDrop promise = (PromiseDrop) promiseIter.next();
+			addCheckedPromise(promise);
+		}
+	}
+
+	/**
+	 * Adds a promise to the set of promises this result uses as a precondition,
+	 * or <i>trusts</i>.
+	 * 
+	 * @param promise
+	 *            the promise being trusted by this result
+	 */
+	public void addTrustedPromise(PromiseDrop promise) {
+		trusts.add(promise);
+		promise.addDependent(this);
+	}
+
+	public void addTrustedPromises(Collection<? extends PromiseDrop> promises) {
+		// no null check -- fail-fast
+		for (PromiseDrop promise : promises) {
+			// Iterator promiseIter = promises.iterator();
+			// while (promiseIter.hasNext()) {
+			// PromiseDrop promise = (PromiseDrop) promiseIter.next();
+			addTrustedPromise(promise);
+		}
+	}
+
+	/**
+	 * Adds a promise to the set of promises this result uses as a precondition,
+	 * or <i>trusts</i>.
+	 * 
+	 * @param promise
+	 *            the promise being trusted by this result
+	 */
+	public void addTrustedPromise_or(String orKey, PromiseDrop promise) {
+		String key = orKey.intern();
+		Set<PromiseDrop> s = or_TrustLabelToTrusts.get(key);
+		if (s == null) {
+			s = new HashSet<PromiseDrop>();
+			or_TrustLabelToTrusts.put(key, s);
+		}
+		s.add(promise);
+		promise.addDependent(this);
+	}
+
+	/**
+	 * @return the set of promise drops established, or checked, by this result.
+	 *         All members of the returned set will are of the PromiseDrop type.
+	 */
+	public Set<? extends PromiseDrop> getChecks() {
+		return checks;
+	}
+
+	/**
+	 * Returns the preconditions of this result, including any "or"
+	 * preconditions. However, using this call it is impossible to distingish
+	 * "and" preconditions from "or"preconditions.
+	 * 
+	 * @return the set of promises trusted by this result, its preconditions.
+	 *         All members of the returned set will are of the PromiseDrop type.
+	 * 
+	 * @see #getTrusts()
+	 * @see #hasOrLogic()
+	 * @see #get_or_TrustLabelSet()
+	 * @see #get_or_Trusts(String)
+	 */
+	public Set<? extends PromiseDrop> getTrustsComplete() {
+		Set<PromiseDrop> result = new HashSet<PromiseDrop>();
+		result.addAll(trusts);
+		if (hasOrLogic()) {
+			Set<String> orLabels = get_or_TrustLabelSet();
+			for (String orKey : orLabels) {
+				// String orKey = (String) i.next();
+				result.addAll(get_or_Trusts(orKey));
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Returns the preconditions of this result, this set does not include any
+	 * "or" preconditions. Use the "get_or_" methods to obtain those
+	 * preconditions.
+	 * 
+	 * @return the set of promises trusted by this result, its preconditions.
+	 *         All members of the returned set will are of the PromiseDrop type.
+	 * 
+	 * @see #hasOrLogic()
+	 * @see #get_or_TrustLabelSet()
+	 * @see #get_or_Trusts(String)
+	 */
+	public Set<PromiseDrop> getTrusts() {
+		return trusts;
+	}
+
+	/**
+	 * Flags if this result has groups of "or" precondition sets which must be
+	 * considered in the whole-program consistency proof. Most results do not
+	 * include "or" precondition logic.
+	 * <p>
+	 * Typical use of this method is:
+	 * 
+	 * <pre>
    * if (rd.hasOrLogic()) {
    *   Set orLabels = rd.get_or_TrustLabelSet()
    *   for (Iterator i = orLabels.iterator(); i.hasNext();) {
@@ -183,19 +192,20 @@ public class ResultDrop extends ProofDrop {
    *   }
    * }
    * </pre>
-   * 
-   * @return <code>true</code> if "or" precondition logic exists,
-   *   <code>false</code> otherwise.
-   */
-  public boolean hasOrLogic() {
-    return !or_TrustLabelToTrusts.keySet().isEmpty();
-  }
+	 * 
+	 * @return <code>true</code> if "or" precondition logic exists,
+	 *         <code>false</code> otherwise.
+	 */
+	public boolean hasOrLogic() {
+		return !or_TrustLabelToTrusts.keySet().isEmpty();
+	}
 
-  /**
-   * Returns the set of "or" keys used for this promise.
-   * <p>
-   * Typical use of this method is:
-   * <pre>
+	/**
+	 * Returns the set of "or" keys used for this promise.
+	 * <p>
+	 * Typical use of this method is:
+	 * 
+	 * <pre>
    * if (rd.hasOrLogic()) {
    *   Set orLabels = rd.get_or_TrustLables()
    *   Set orLabels = rd.get_or_TrustLabelSet()
@@ -209,18 +219,19 @@ public class ResultDrop extends ProofDrop {
    *   }
    * }
    * </pre>
-   * 
-   * @return the set of "or" keys used by this promise
-   */
-  public Set<String> get_or_TrustLabelSet() {
-    return or_TrustLabelToTrusts.keySet();
-  }
+	 * 
+	 * @return the set of "or" keys used by this promise
+	 */
+	public Set<String> get_or_TrustLabelSet() {
+		return or_TrustLabelToTrusts.keySet();
+	}
 
-  /**
-   * Returns the set of promise drops for a specific "or" key.
-   * <p>
-   * Typical use of this method is:
-   * <pre>
+	/**
+	 * Returns the set of promise drops for a specific "or" key.
+	 * <p>
+	 * Typical use of this method is:
+	 * 
+	 * <pre>
    * if (rd.hasOrLogic()) {
    *   Set orLabels = rd.get_or_TrustLabelSet()
    *   for (Iterator i = orLabels.iterator(); i.hasNext();) {
@@ -233,116 +244,150 @@ public class ResultDrop extends ProofDrop {
    *   }
    * }
    * </pre>
-   * 
-   * @param key the key to provide the promise drop set for
-   * @return the promise drop set
-   */
-  public Set<PromiseDrop> get_or_Trusts(String key) {
-    return or_TrustLabelToTrusts.get(key);
-  }
+	 * 
+	 * @param key
+	 *            the key to provide the promise drop set for
+	 * @return the promise drop set
+	 */
+	public Set<PromiseDrop> get_or_Trusts(String key) {
+		return or_TrustLabelToTrusts.get(key);
+	}
 
-  /**
-   * @return <code>true</code> if the result indicates model/code
-   *   consistency, <code>false</code> otherwise.
-   */
-  public boolean isConsistent() {
-    return consistent;
-  }
+	/**
+	 * @return <code>true</code> if the result indicates model/code consistency,
+	 *         <code>false</code> otherwise.
+	 */
+	public boolean isConsistent() {
+		return consistent;
+	}
 
-  /**
-   * Sets this result to indicate model/code consistency.
-   */
-  public void setConsistent() {
-    consistent = true;
-  }
+	/**
+	 * Sets this result to indicate model/code consistency.
+	 */
+	public void setConsistent() {
+		consistent = true;
+	}
 
-  /**
-   * Sets this result to indicate model/code inconsistency.
-   */
-  public void setInconsistent() {
-    consistent = false;
-  }
+	/**
+	 * Sets this result to indicate model/code inconsistency.
+	 */
+	public void setInconsistent() {
+		consistent = false;
+	}
 
-  public void setConsistent(final boolean isConsistent) {
-    consistent = isConsistent;
-  }
-  
-  /**
-   * Flags of the proof of "or" trusted promises uses a red dot.
-   */
-  boolean or_proofUsesRedDot = false;
+	/**
+	 * Sets this result to indicate model/code inconsistency.
+	 * 
+	 * @param value
+	 *            the consistency setting.
+	 */
+	public void setConsistent(final boolean value) {
+		consistent = value;
+	}
 
-  /**
-   * Returns if the proof of "or" trusted promises uses a red dot.
-   * 
-   * @return <code>true</code> if a red dot is used, <code>false</code> otherwise.
-   */
-  public boolean get_or_proofUsesRedDot() {
-    return or_proofUsesRedDot;
-  }
+	/**
+	 * Flags if this result drop was "vouched" for by a programmer even though
+	 * it is inconsistent.
+	 */
+	boolean vouched = false;
 
-  /**
-   * Flags if the proof of "or" trusted promises is consistent.
-   */
-  boolean or_provedConsistent = false;
+	/**
+	 * Returns if this result drop was "vouched" for by a programmer even though
+	 * it is inconsistent.
+	 * 
+	 * @return <code>true</code> if this result drop was "vouched" for by a
+	 *         programmer even though it is inconsistent, <code>false</code>
+	 *         otherwise.
+	 */
+	public boolean isVouched() {
+		return vouched;
+	}
 
-  /**
-   * Returns if the proof of "or" trusted promises is consistent.
-   * 
-   * @return<code>true</code> if consistent, <code>false</code> otherwise.
-   */
-  public boolean get_or_provedConsistent() {
-    return or_provedConsistent;
-  }
-  
-  @Override
-  public String getEntityName() {
-	  return "result-drop";
-  }	
-  
-  @Override
-  public void preprocessRefs(SeaSnapshot s) {
-	  for(Drop c : getChecks()) {
-		  s.snapshotDrop(c);
-	  }
-	  for(Drop t : getTrusts()) {
-		  s.snapshotDrop(t);
-	  }
-	  if (hasOrLogic()) {
-		  for(String label : get_or_TrustLabelSet()) {
-			  for(Drop t : get_or_Trusts(label)) {
-				  s.snapshotDrop(t);
-			  }
-		  }
-	  }
-  }
-  
-  @Override
-  public void snapshotAttrs(SeaSnapshot s) {
-	  super.snapshotAttrs(s);
-	  s.addAttribute("consistent", isConsistent());
-	  if (type != null) {
-		  s.addAttribute("result-type", type);
-	  } else {
-		  System.out.println("result-type is null");
-	  }
-  }
-  
-  @Override
-  public void snapshotRefs(SeaSnapshot s) {
-	  super.snapshotRefs(s);
-	  for(Drop c : getChecks()) {
-		  s.refDrop("checked-promise", c);
-	  }
-	  for(Drop t : getTrusts()) {
-		  s.refDrop("trusted-promise", t);
-	  }
-	  if (hasOrLogic()) {
-		  for(String label : get_or_TrustLabelSet()) {
-			  for(Drop t : get_or_Trusts(label)) {
-				  s.refDrop("or-trusted-promise", t, "label", label);
-			  }
-		  }
-	  }
-  }
+	/**
+	 * Sets this result as being "vouched" for by a programmer even though it is
+	 * inconsistent.
+	 */
+	public void setVouched() {
+		vouched = true;
+	}
+
+	/**
+	 * Flags of the proof of "or" trusted promises uses a red dot.
+	 */
+	boolean or_proofUsesRedDot = false;
+
+	/**
+	 * Returns if the proof of "or" trusted promises uses a red dot.
+	 * 
+	 * @return <code>true</code> if a red dot is used, <code>false</code>
+	 *         otherwise.
+	 */
+	public boolean get_or_proofUsesRedDot() {
+		return or_proofUsesRedDot;
+	}
+
+	/**
+	 * Flags if the proof of "or" trusted promises is consistent.
+	 */
+	boolean or_provedConsistent = false;
+
+	/**
+	 * Returns if the proof of "or" trusted promises is consistent.
+	 * 
+	 * @return<code>true</code> if consistent, <code>false</code> otherwise.
+	 */
+	public boolean get_or_provedConsistent() {
+		return or_provedConsistent;
+	}
+
+	@Override
+	public String getEntityName() {
+		return "result-drop";
+	}
+
+	@Override
+	public void preprocessRefs(SeaSnapshot s) {
+		for (Drop c : getChecks()) {
+			s.snapshotDrop(c);
+		}
+		for (Drop t : getTrusts()) {
+			s.snapshotDrop(t);
+		}
+		if (hasOrLogic()) {
+			for (String label : get_or_TrustLabelSet()) {
+				for (Drop t : get_or_Trusts(label)) {
+					s.snapshotDrop(t);
+				}
+			}
+		}
+	}
+
+	@Override
+	public void snapshotAttrs(SeaSnapshot s) {
+		super.snapshotAttrs(s);
+		s.addAttribute("consistent", isConsistent());
+		if (type != null) {
+			s.addAttribute("result-type", type);
+		} else {
+			System.out.println("result-type is null");
+		}
+	}
+
+	@Override
+	public void snapshotRefs(SeaSnapshot s) {
+		super.snapshotRefs(s);
+		for (Drop c : getChecks()) {
+			s.refDrop("checked-promise", c);
+		}
+		for (Drop t : getTrusts()) {
+			s.refDrop("trusted-promise", t);
+		}
+		if (hasOrLogic()) {
+			for (String label : get_or_TrustLabelSet()) {
+				for (Drop t : get_or_Trusts(label)) {
+					s.refDrop("or-trusted-promise", t, "label", label);
+				}
+			}
+		}
+	}
 }
