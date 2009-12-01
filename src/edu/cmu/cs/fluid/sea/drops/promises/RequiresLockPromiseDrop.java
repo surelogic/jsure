e64e0a41dbe04b2c3c96982af2f488e358927953
Edwin Chan <edwin.chan@surelogic.com>
2009-12-01 21:08:29 +0000

    Factored out AbstractLockListPromiseDrop
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/fluid@13625 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/edu/cmu/cs/fluid/sea/drops/promises/RequiresLockPromiseDrop.java
+++ src/edu/cmu/cs/fluid/sea/drops/promises/RequiresLockPromiseDrop.java
@@ -2,13 +2,7 @@ package edu.cmu.cs.fluid.sea.drops.promises;
 
 import com.surelogic.aast.promise.RequiresLockNode;
 
-import edu.cmu.cs.fluid.ir.IRNode;
-import edu.cmu.cs.fluid.java.DebugUnparser;
 import edu.cmu.cs.fluid.java.JavaGlobals;
-import edu.cmu.cs.fluid.java.JavaNames;
-import edu.cmu.cs.fluid.java.bind.Messages;
-import edu.cmu.cs.fluid.sea.Drop;
-import edu.cmu.cs.fluid.sea.PromiseDrop;
 
 /**
  * Promise drop for "requiresLock" promises.
@@ -16,80 +10,9 @@ import edu.cmu.cs.fluid.sea.PromiseDrop;
  * @see edu.cmu.cs.fluid.java.analysis.LockVisitor
  * @see edu.cmu.cs.fluid.java.bind.LockAnnotation
  */
-public final class RequiresLockPromiseDrop extends PromiseDrop<RequiresLockNode> {
+public final class RequiresLockPromiseDrop extends AbstractLockListPromiseDrop<RequiresLockNode> {
   public RequiresLockPromiseDrop(RequiresLockNode n) {
     super(n);
     setCategory(JavaGlobals.LOCK_REQUIRESLOCK_CAT);
-  }
-  
-  private String lockName;
-
-  private IRNode lockNode; // used for early lock declaration
-  
-  /**
-   * @return Returns the lockName.
-   */
-  @Deprecated
-  public final String getLockName() {
-    return lockName;
-  }
-
-  /**
-   * @param lockName The lockName to set.
-   */
-  @Deprecated
-  public final void setLockName(String lockName) {
-    this.lockName = lockName;
-  }
-
-//  /**
-//   * Gets the associated lock model (a promise drop) for this drop.
-//   * 
-//   * @return the correct associated promise drop for this lock.
-//   * 
-//   * @see edu.cmu.cs.fluid.sea.drops.promises.LockModel
-//   */
-//  public LockModel getModel() {
-//    return LockModel.getInstance(lockName);
-//  }
-
-  /**
-   * @return Returns the lockNode.
-   */
-  public final IRNode getLockNode() {
-    return lockNode;
-  }
-
-  /**
-   * @param lockNode The lockNode to set.
-   */
-  public final void setLockNode(IRNode lockNode) {
-    this.lockNode = lockNode;
-  }
-
-  /**
-   * Need to clean up lock models.
-   * 
-   * @see edu.cmu.cs.fluid.sea.Drop#deponentInvalidAction(Drop)
-   */
-  @Override
-  protected void deponentInvalidAction(Drop invalidDeponent) {
-    super.deponentInvalidAction(invalidDeponent);
-    LockModel.purgeUnusedLocks();
-  }
-  
-  /**
-   * @see edu.cmu.cs.fluid.sea.PromiseDrop#isCheckedByAnalysis()
-   */
-  @Override
-  public boolean isCheckedByAnalysis() {
-    return true;
-  }
-  
-  @Override
-  protected void computeBasedOnAST() {
-    String lock = getAST() != null ? getAST().toString() : DebugUnparser.toString(lockNode);
-    setMessage(Messages.LockAnnotation_requiresLockDrop, 
-               lock, JavaNames.genMethodConstructorName(getNode()));
   }
 }
