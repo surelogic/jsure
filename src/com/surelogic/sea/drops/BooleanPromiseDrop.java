803131fda97ba782c6cb48a752d33415c7f4a70b
Edwin Chan <edwin.chan@surelogic.com>
2009-12-10 16:42:08 +0000

    Added clarification
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/fluid@13681 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sea/drops/BooleanPromiseDrop.java
+++ src/com/surelogic/sea/drops/BooleanPromiseDrop.java
@@ -5,6 +5,11 @@ import com.surelogic.aast.*;
 
 import edu.cmu.cs.fluid.sea.PromiseDrop;
 
+/**
+ * Using the OLD drop-sea
+ * 
+ * @author edwin
+ */
 public class BooleanPromiseDrop<A extends IAASTRootNode> extends PromiseDrop<A> {
   public BooleanPromiseDrop(A a) {
     super(a);
