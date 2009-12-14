a3ea480e07892e24a1365064183e89a423234c74
Edwin Chan <edwin.chan@surelogic.com>
2009-12-14 16:29:52 +0000

    Added code to allow copying text from the view
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@13711 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/edu/cmu/cs/fluid/dcf/views/coe/ProblemsView.java
+++ src/edu/cmu/cs/fluid/dcf/views/coe/ProblemsView.java
@@ -1,10 +1,13 @@
 package edu.cmu.cs.fluid.dcf.views.coe;
 
-import org.eclipse.jface.action.IMenuManager;
-import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.*;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.actions.ActionFactory;
 
 import edu.cmu.cs.fluid.dcf.views.AbstractDoubleCheckerView;
 import edu.cmu.cs.fluid.sea.PromiseWarningDrop;
@@ -13,18 +16,52 @@ public class ProblemsView extends AbstractDoubleCheckerView {
 
 	private final ProblemsViewContentProvider f_content = new ProblemsViewContentProvider();
 
+	private final Action f_copy = new Action() {
+		@Override
+		public void run() {
+			clipboard.setContents(new Object[] { getSelectedText() },
+					new Transfer[] { TextTransfer.getInstance() });
+		}
+	};
+	
 	public ProblemsView() {
 		super(true);
 	}
 
-	@Override
-	protected void makeActions() {
-		// nothing
+	protected String getSelectedText() {
+		IStructuredSelection selection = (IStructuredSelection) viewer
+		.getSelection();
+		StringBuilder sb = new StringBuilder();
+		for (Object elt : selection.toList()) {
+			if (sb.length() > 0) {
+				sb.append('\n');
+			}
+			for(int i=0; i<3; i++) {
+				if (i > 0) {
+					sb.append(' ');
+				}
+				sb.append(f_content.getColumnText(elt, i));
+			}
+		}
+		return sb.toString();
 	}
 
 	@Override
+	protected void makeActions() {
+	    f_copy.setText("Copy");
+	    f_copy.setToolTipText("Copy the selected problem to the clipboard");
+	}
+
+	@Override
+	  protected void fillGlobalActionHandlers(IActionBars bars) {
+	    bars.setGlobalActionHandler(ActionFactory.COPY.getId(), f_copy);
+	  }
+	
+	@Override
 	protected void fillContextMenu(IMenuManager manager, IStructuredSelection s) {
-		// nothing
+		if (!s.isEmpty()) {
+			manager.add(f_copy);
+		}
 	}
 
 	@Override
