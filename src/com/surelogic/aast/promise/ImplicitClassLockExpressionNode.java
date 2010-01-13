e652b0d552049c4b48c451888b9723e489dd4414
Edwin Chan <edwin.chan@surelogic.com>
2010-01-13 18:06:15 +0000

    Fixed unparsing
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/fluid@13926 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/aast/promise/ImplicitClassLockExpressionNode.java
+++ src/com/surelogic/aast/promise/ImplicitClassLockExpressionNode.java
@@ -30,8 +30,12 @@ public class ImplicitClassLockExpressionNode extends ClassLockExpressionNode {
   @Override
   public String unparse(boolean debug, int indent) {
     StringBuilder sb = new StringBuilder();
-    if (debug) { indent(sb, indent); }
-    sb.append("ImplicitClassLockExpression\n");
+    if (debug) { 
+    	indent(sb, indent); 
+    	sb.append("ImplicitClassLockExpression\n");
+    } else {
+    	sb.append("class");
+    }
     return sb.toString();
   }
 
