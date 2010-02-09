ffaf3afe2684dcfa439c28375757ab7e784aea6b
Edwin Chan <edwin.chan@surelogic.com>
2010-02-09 23:55:45 +0000

    Added code to set data directory
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@14223 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/client/eclipse/analysis/JavacEclipse.java
+++ src/com/surelogic/jsure/client/eclipse/analysis/JavacEclipse.java
@@ -3,13 +3,20 @@ package com.surelogic.jsure.client.eclipse.analysis;
 import java.net.URL;
 
 import com.surelogic.common.eclipse.Resources;
+import com.surelogic.fluid.eclipse.preferences.PreferenceConstants;
 import com.surelogic.fluid.javac.Javac;
+
+import edu.cmu.cs.fluid.ide.IDEPreferences;
 
 public class JavacEclipse extends Javac {
     static final JavacEclipse instance = new JavacEclipse();  
     
     public static void initialize() {
         // Nothing to do right now, besides create the instance above
+    }
+    
+    {
+        prefs.put(IDEPreferences.DATA_DIRECTORY, PreferenceConstants.getJSureDataDirectory().getAbsolutePath());
     }
     
     @Override
