741eb6132843dd57df85591cbf1ed97a0688944b
Edwin Chan <edwin.chan@surelogic.com>
2010-03-02 19:11:30 +0000

    Added code to implement findOverridingMethodsFromType()
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/fluid@14303 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/edu/cmu/cs/fluid/java/bind/AbstractBinder.java
+++ src/edu/cmu/cs/fluid/java/bind/AbstractBinder.java
@@ -5,11 +5,13 @@
 package edu.cmu.cs.fluid.java.bind;
 
 import java.util.*;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.surelogic.common.logging.SLLogger;
 
 import edu.cmu.cs.fluid.ir.IRNode;
+import edu.cmu.cs.fluid.java.*;
 import edu.cmu.cs.fluid.java.operator.*;
 import edu.cmu.cs.fluid.java.util.BindUtil;
 import edu.cmu.cs.fluid.java.util.VisitUtil;
@@ -127,7 +129,7 @@ public abstract class AbstractBinder implements IBinder {
 	  IRNode overridingParams = SomeFunctionDeclaration.getParams(overridingM);
 	  IRNode parentParams     = SomeFunctionDeclaration.getParams(parentM);
 	  if (JJNode.tree.numChildren(overridingParams) == JJNode.tree.numChildren(parentParams)) {
-		  final ITypeEnvironment tEnv = getTypeEnvironment();
+		  //final ITypeEnvironment tEnv = getTypeEnvironment();
 		  final Iterator<IRNode> oIt = JJNode.tree.children(overridingParams);
 		  final Iterator<IRNode> pIt = JJNode.tree.children(parentParams);
 		  while (oIt.hasNext()) {
@@ -185,8 +187,176 @@ public abstract class AbstractBinder implements IBinder {
   /* (non-Javadoc)
    * @see edu.cmu.cs.fluid.java.bind.IBinder#findOverridingMethodsFromType(edu.cmu.cs.fluid.ir.IRNode, edu.cmu.cs.fluid.ir.IRNode)
    */
-  public Iteratable<IRNode>  findOverridingMethodsFromType(IRNode callee, IRNode receiver) {
-    // Optional method
-    return new EmptyIterator<IRNode>();
+  public Iteratable<IRNode>  findOverridingMethodsFromType(IRNode callee, IRNode receiverType) {
+	  final ITypeEnvironment tEnv = getTypeEnvironment();
+	  final IJavaType dt = tEnv.convertNodeTypeToIJavaType(receiverType);
+	  return findOverriddenMethodsFromType(new FindOverriddenMethodsStrategy(
+			  this, callee, true), callee, dt);
   }
+  
+  @SuppressWarnings("unchecked")
+  private Iteratable<IRNode> findOverriddenMethodsFromType(
+		  ISubTypeSearchStrategy s, IRNode method, IJavaType startType) {
+	  final Operator op = JJNode.tree.getOperator(method);
+	  if (op instanceof ConstructorDeclaration) {
+		  // TODO ignore "overridden methods" of constructors?
+		  return EmptyIterator.prototype();
+	  } else if (!(op instanceof MethodDeclaration)) {
+		  //if (!warnedSet.contains(method)) {
+			  LOG.warning("findOverriddenMethod: trying to match against "
+					  + op.name() + ": " + DebugUnparser.toString(method));
+		  //  warnedSet.add(method);
+		  //}
+		  return EmptyIterator.prototype();
+	  }
+
+	  final IRNode methodsTypeD = VisitUtil.getEnclosingType(method);
+	  if (startType == null || methodsTypeD == null) {
+		  return EmptyIterator.prototype();
+	  }	  	 
+	  // Need to make sure that startType is a subtype of methodsType
+	  final ITypeEnvironment tEnv = getTypeEnvironment();
+	  final IJavaType methodsType = tEnv.convertNodeTypeToIJavaType(methodsTypeD);
+	  if (!startType.equals(methodsType)
+			  && !tEnv.isSubType(startType, methodsType)) {
+		  return EmptyIterator.prototype();
+	  }
+
+	  if (LOG.isLoggable(Level.FINE))
+		  LOG.fine("Starting from " + startType.getName());
+
+	  findXinSubclasses(s, startType);	  
+
+	  Iteratable<IRNode> it;
+	  Object o = s.getResult();
+	  if (o instanceof Iteratable) {
+		  it = (Iteratable<IRNode>) o;
+	  } else {
+		  it = new SimpleIteratable<IRNode>((Iterator<IRNode>) o);
+	  }
+
+	  if (it == null) {
+		  final String context =  startType.getName();
+		  LOG.severe("Couldn't find " + s.getLabel() + " in " + context);
+	  }
+
+	  if (LOG.isLoggable(Level.INFO)) {
+		  String msg;
+		  if (it.hasNext()) {
+			  msg = "Matched 1+ methods against ";
+		  } else {
+			  msg = "No matches against ";
+		  }
+		  if (LOG.isLoggable(Level.FINE))
+			  LOG.fine(msg + DebugUnparser.toString(method));
+	  }
+	  return it;
+  }
+
+  /*
+   * Searches for X, starting at the given type and its subclasses
+   */
+  @SuppressWarnings("unchecked")
+  public void findXinSubclasses(ISubTypeSearchStrategy s, IJavaType type) {
+	  if (type == null) {
+		  return;
+	  }
+	  if (type instanceof IJavaDeclaredType) {
+		  IJavaDeclaredType dt = (IJavaDeclaredType) type;
+		  IRNode typeD = dt.getDeclaration();	  
+		  Operator op = JJNode.tree.getOperator(typeD);
+		  if (ClassDeclaration.prototype.includes(op)) {
+			  findXinClass_down(s, dt);
+		  } else if (InterfaceDeclaration.prototype.includes(op)) {
+			  findXinInterface_down(s, dt);
+		  } else if (AnonClassExpression.prototype.includes(op)) {
+			  findXinClass_down(s, dt);
+		  } else if (AnnotationDeclaration.prototype.includes(op)) {
+			  findXinAnnotation_down(s, dt);
+		  } else if (EnumDeclaration.prototype.includes(op)) {
+			  findXinEnum_down(s, dt);
+		  } else if (EnumConstantClassDeclaration.prototype.includes(op)) {
+			  findXinEnumConstantClassDecl_down(s, dt);
+		  } else {
+			  LOG.severe("Calling findXinSubclasses on unhandled op "
+					  + op.name());
+			  return;
+		  }
+	  } else if (type instanceof IJavaTypeFormal) {
+		  findXinTypeFormal_down(s, (IJavaTypeFormal) type);
+	  } else {
+		  LOG.severe("Calling findXinSubclasses on unhandled type "+type);
+		  return;
+	  }
+  }
+  
+	/*
+	 * Helper for findXinType Only to be called by findXinType and its helpers
+	 */
+	@SuppressWarnings("unchecked")
+	private void findXinClass_down(ISubTypeSearchStrategy s, IJavaDeclaredType type) {
+		if (type == null) {
+			return;
+		}
+		s.visitClass(type.getDeclaration());
+
+		if (s.visitSubclasses()) {
+			for(IJavaType sub : getSubclasses(type)) {
+				findXinClass_down(s, (IJavaDeclaredType) sub);
+			}
+		}
+	}
+	@SuppressWarnings("unchecked")
+	private void findXinAnnotation_down(ISubTypeSearchStrategy s, IJavaDeclaredType type) {
+		if (type == null) {
+			return;
+		}
+		s.visitClass(type.getDeclaration());
+	}
+	@SuppressWarnings("unchecked")
+	private void findXinInterface_down(ISubTypeSearchStrategy s, IJavaDeclaredType type) {
+		if (type == null) {
+			return;
+		}
+		// LOG.info("findXinInterface_down @"+InterfaceDeclaration.getId(type));
+		s.visitInterface(type.getDeclaration());
+
+		if (s.visitSubclasses()) {
+			for(IJavaType sub : getSubclasses(type)) {
+				findXinSubclasses(s, sub);
+			}
+		}
+	}
+	@SuppressWarnings("unchecked")
+	private void findXinTypeFormal_down(ISubTypeSearchStrategy s, IJavaTypeFormal formal) {
+		/*
+		for (IRNode bound : getFormalBounds(formal)) {
+			findXinSubclasses(s, bound);
+		}
+		*/
+		// No subclasses
+		return;
+	}
+	@SuppressWarnings("unchecked")
+	private void findXinEnum_down(ISubTypeSearchStrategy s, IJavaDeclaredType type) {
+		findXinClass_down(s, type);
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void findXinEnumConstantClassDecl_down(ISubTypeSearchStrategy s,
+			IJavaDeclaredType type) {
+		findXinClass_down(s, type);
+	}
+	
+	private Iterable<IJavaType> getSubclasses(IJavaDeclaredType root) {
+		final ITypeEnvironment tEnv = getTypeEnvironment();
+		final Iterator<IRNode> it   = tEnv.getRawSubclasses(root.getDeclaration()).iterator();
+		return new FilterIterator<IRNode, IJavaType>(it) {
+			@Override
+			protected Object select(IRNode o) {
+				// TODO what about root's type arguments?
+				return tEnv.convertNodeTypeToIJavaType(o);
+			}
+		};
+	}
 }
