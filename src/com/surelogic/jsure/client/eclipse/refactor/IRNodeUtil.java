96bb6e5500ddb7d3df79dd8724ea7c36687534b2
Edwin Chan <edwin.chan@surelogic.com>
2010-03-10 19:45:18 +0000

    Fixed to provide qualified type names for method parameters
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@14379 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/client/eclipse/refactor/IRNodeUtil.java
+++ src/com/surelogic/jsure/client/eclipse/refactor/IRNodeUtil.java
@@ -4,22 +4,28 @@ import java.util.*;
 
 import edu.cmu.cs.fluid.ir.*;
 import edu.cmu.cs.fluid.java.DebugUnparser;
+import edu.cmu.cs.fluid.java.JavaNames;
+import edu.cmu.cs.fluid.java.bind.IBinder;
+import edu.cmu.cs.fluid.java.bind.IJavaArrayType;
+import edu.cmu.cs.fluid.java.bind.IJavaDeclaredType;
+import edu.cmu.cs.fluid.java.bind.IJavaType;
+import edu.cmu.cs.fluid.java.bind.IJavaTypeFormal;
 import edu.cmu.cs.fluid.java.operator.*;
 import edu.cmu.cs.fluid.parse.*;
 import edu.cmu.cs.fluid.tree.*;
 
 public class IRNodeUtil {
-	public static IJavaDeclaration convert(IRNode decl) {
+	public static IJavaDeclaration convert(IBinder binder, IRNode decl) {
 		if (decl == null) {
 			return null;
 		}
 		final IRNode parentNode = JJNode.tree.getParentOrNull(decl);
-		final IJavaDeclaration parent = convert(parentNode);
+		final IJavaDeclaration parent = convert(binder, parentNode);
 		final Operator op = JJNode.tree.getOperator(decl);		
 		if (op instanceof Declaration) {
 			if (MethodDeclaration.prototype.includes(op)) {
 				final String name = MethodDeclaration.getId(decl);
-				final String[] paramNames = getParamNames(MethodDeclaration.getParams(decl));
+				final String[] paramNames = getParamTypes(binder, MethodDeclaration.getParams(decl));
 				return new Method((Type) parent, name, paramNames);
 			}
 			if (TypeDeclaration.prototype.includes(op)) {
@@ -41,7 +47,7 @@ public class IRNodeUtil {
 			}
 			if (ConstructorDeclaration.prototype.includes(op)) {
 				Type type = (Type) parent;
-				final String[] paramNames = getParamNames(ConstructorDeclaration.getParams(decl));
+				final String[] paramNames = getParamTypes(binder, ConstructorDeclaration.getParams(decl));
 				return new Method((Type) parent, type.getName(), paramNames);
 			}				
 			if (ParameterDeclaration.prototype.includes(op)) {
@@ -49,6 +55,10 @@ public class IRNodeUtil {
 				final int num        = JJNode.tree.childLocationIndex(parentNode, loc);
 				return new MethodParameter((Method) parent, num); 
 			}
+			if (PackageDeclaration.prototype.includes(op) || ClassInitializer.prototype.includes(op)) {
+				// Ignoring this "declaration"
+				return parent;
+			} 
 			System.out.println("Unexpected declaration: "+DebugUnparser.toString(decl));
 		} 
 		else if (AnonClassExpression.prototype.includes(op)) {
@@ -61,11 +71,34 @@ public class IRNodeUtil {
 		return parent;
 	}
 	
-	private static String[] getParamNames(IRNode params) {
+	private static String[] getParamTypes(IBinder binder, IRNode params) {
 		List<String> names = new ArrayList<String>();
-		for(IRNode pd : JJNode.tree.children(params)) {
-			names.add(ParameterDeclaration.getId(pd));
+		for(final IRNode pd : JJNode.tree.children(params)) {
+			final IRNode type = ParameterDeclaration.getType(pd);
+			final IJavaType t = binder.getJavaType(type);
+			final String name = getParamType(binder, t);
+			names.add(name);
 		}
 		return names.toArray(new String[names.size()]);
+	}
+	
+	private static String getParamType(IBinder binder, IJavaType t) {
+		if (t instanceof IJavaTypeFormal) {
+			IJavaTypeFormal tf = (IJavaTypeFormal) t;
+			return getParamType(binder, tf.getSuperclass(binder.getTypeEnvironment()));
+		} 
+		else if (t instanceof IJavaArrayType) {
+			IJavaArrayType at = (IJavaArrayType) t;
+			StringBuilder sb = new StringBuilder(getParamType(binder, at.getBaseType()));
+			for(int i=0; i<at.getDimensions(); i++) {
+				sb.append("[]");
+			}
+			return sb.toString();
+		}
+		else if (t instanceof IJavaDeclaredType) {
+			IJavaDeclaredType dt = (IJavaDeclaredType) t;
+			return JavaNames.getFullTypeName(dt.getDeclaration());
+		}
+		return t.getName();
 	}
 }
