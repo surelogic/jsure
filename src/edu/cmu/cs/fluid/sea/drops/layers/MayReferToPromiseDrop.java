14e34ecbe5f004be405d9ccd6b32ba4cbc3325b1
Edwin Chan <edwin.chan@surelogic.com>
2010-03-11 21:06:55 +0000

    Added parse rules and promise drops for layers
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/fluid@14393 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/edu/cmu/cs/fluid/sea/drops/layers/MayReferToPromiseDrop.java
@@ -0,0 +1,12 @@
+/*$Header: /cvs/fluid/fluid/.settings/org.eclipse.jdt.ui.prefs,v 1.2 2006/03/27 21:35:50 boyland Exp $*/
+package edu.cmu.cs.fluid.sea.drops.layers;
+
+import com.surelogic.aast.layers.MayReferToNode;
+
+import edu.cmu.cs.fluid.sea.PromiseDrop;
+
+public class MayReferToPromiseDrop extends PromiseDrop<MayReferToNode> {
+	public MayReferToPromiseDrop(MayReferToNode a) {
+		super(a);
+	}
+}
---
+++ src/edu/cmu/cs/fluid/sea/drops/layers/TypeSetPromiseDrop.java
@@ -0,0 +1,12 @@
+/*$Header: /cvs/fluid/fluid/.settings/org.eclipse.jdt.ui.prefs,v 1.2 2006/03/27 21:35:50 boyland Exp $*/
+package edu.cmu.cs.fluid.sea.drops.layers;
+
+import com.surelogic.aast.layers.*;
+
+import edu.cmu.cs.fluid.sea.PromiseDrop;
+
+public class TypeSetPromiseDrop extends PromiseDrop<TypeSetNode> {
+	public TypeSetPromiseDrop(TypeSetNode a) {
+		super(a);
+	}
+}
