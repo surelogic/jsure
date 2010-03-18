576b804cefdacdb3cb6cd5d3d7cd1be01d992e08
Edwin Chan <edwin.chan@surelogic.com>
2010-03-18 15:09:56 +0000

    Added comment
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@14455 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/edu/cmu/cs/fluid/dcf/views/coe/ResultsViewContentProvider.java
+++ src/edu/cmu/cs/fluid/dcf/views/coe/ResultsViewContentProvider.java
@@ -919,6 +919,8 @@ public class ResultsViewContentProvider extends
 		}
 		onPath.add(node);
 
+		// Only used to get consistent results when breaking back edges
+		// NOT for the results view (see ContentNameSorter)
 		final List<Content> children = new ArrayList<Content>(node.children());
 		Collections.sort(children, new Comparator<Content>() {
 			public int compare(Content o1, Content o2) {
