1f5c3360d706015c7ad46fadb9f7e27b6be69be5
Edwin Chan <edwin.chan@surelogic.com>
2010-03-23 20:25:35 +0000

    Factored out Dependencies for use in fluid-javac
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@14516 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/edu/cmu/cs/fluid/analysis/util/PackageLevelPreprocessing.java
+++ src/edu/cmu/cs/fluid/analysis/util/PackageLevelPreprocessing.java
@@ -1,6 +1,5 @@
 package edu.cmu.cs.fluid.analysis.util;
 
-import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,7 +20,6 @@ import com.surelogic.annotation.parse.AnnotationVisitor;
 import com.surelogic.annotation.parse.SLAnnotationsLexer;
 import com.surelogic.annotation.parse.ScopedPromisesLexer;
 import com.surelogic.common.logging.SLLogger;
-import com.surelogic.promise.PromiseDropStorage;
 
 import edu.cmu.cs.fluid.eclipse.Eclipse;
 import edu.cmu.cs.fluid.eclipse.EclipseCodeFile;
@@ -33,10 +31,9 @@ import edu.cmu.cs.fluid.ide.IDE;
 import edu.cmu.cs.fluid.ir.IRNode;
 import edu.cmu.cs.fluid.java.ISrcRef;
 import edu.cmu.cs.fluid.java.JavaNode;
-import edu.cmu.cs.fluid.java.JavaPromise;
 import edu.cmu.cs.fluid.java.bind.ITypeEnvironment;
 import edu.cmu.cs.fluid.java.util.VisitUtil;
-import edu.cmu.cs.fluid.sea.*;
+import edu.cmu.cs.fluid.sea.dependencies.Dependencies;
 import edu.cmu.cs.fluid.sea.drops.*;
 import edu.cmu.cs.fluid.util.AbstractRunner;
 
@@ -80,7 +77,18 @@ public final class PackageLevelPreprocessing extends
 	public void preBuild(IProject p) {
 		ModuleUtil.clearModuleCache();
 		super.preBuild(p);
-		dependencies = new Dependencies();
+		dependencies = new Dependencies() {
+			protected void handlePackage(final PackageDrop pkg) {
+				runVersioned(new AbstractRunner() {
+					public void run() {
+						parsePackagePromises(pkg);
+					}
+				});	
+			}			
+			protected void handleType(CUDrop d) {
+				ConvertToIR.getInstance().registerClass(d.makeCodeInfo());
+			}
+		};
 	}
 
 	@Override
@@ -247,132 +255,5 @@ public final class PackageLevelPreprocessing extends
 		final ITypeEnvironment te = Eclipse.getDefault().getTypeEnv(getProject());
 		AnnotationVisitor v = new AnnotationVisitor(te, pkg.javaOSFileName);
 		v.doAccept(top);
-	}
-	
-	class Dependencies {
-		/**
-		 * To avoid cycles and duplication
-		 */
-		private final Set<Drop> checkedDependents = new HashSet<Drop>();
-		private final Set<Drop> checkedDeponents = new HashSet<Drop>();
-		/**
-		 * The set of CUDrops that need to be reprocessed for promises
-		 */
-		private final Set<CUDrop> reprocess = new HashSet<CUDrop>();
-		/**
-		 * The set of changed CUDrops
-		 */
-		private final Set<CUDrop> changed = new HashSet<CUDrop>();
-		
-		void markAsChanged(CUDrop d) {
-			if (d == null) {
-				return; // Nothing to do
-			}
-			changed.add(d);
-			collect(d);
-		}
-
-		/**
-		 * Collects the CUDrops corresponding to d's dependent drops, 
-		 * so we can reprocess the promises on those
-		 */	
-		private void collect(Drop root) {
-			if (checkedDependents.contains(root)) {
-				return;
-			}
-			checkedDependents.add(root);
-			
-			// Find dependent drops
-			for(Drop d : root.getDependents()) {
-				//System.out.println(root+" <- "+d);
-				findCUDropDeponents(d);
-				collect(d);
-			}				
-		}
-
-		/**
-		 * Recursively check this drop and its deponents for CUDrops
-		 */
-		private void findCUDropDeponents(Drop d) {
-			if (checkedDeponents.contains(d)) {
-				return;
-			}
-			checkedDeponents.add(d);
-			if (d instanceof CUDrop) {
-				reprocess.add((CUDrop) d);				
-				collect(d);
-				/*
-				if (d instanceof PackageDrop) {
-					// I need to reprocess these if the package changed
-					for(Drop dd : d.getDependents()) {
-						if (dd instanceof CUDrop) {
-							reprocess.add((CUDrop) dd);
-						}
-					}
-				}
-				*/
-			}
-			for(Drop deponent : d.getDeponents()) {
-				//System.out.println(d+" -> "+deponent);
-				findCUDropDeponents(deponent);
-			}
-		}
-		/**
-		 * Collect CU deponents of promise warnings
-		 */
-		private void processPromiseWarningDrops() {
-			for(Drop d : Sea.getDefault().getDropsOfType(PromiseWarningDrop.class)) {				
-				findCUDropDeponents(d);
-			}
-		}
-		
-		void finish() {
-			processPromiseWarningDrops();
-			
-			reprocess.removeAll(changed);						
-			/*
-			for(CUDrop d : changed) {
-				System.out.println("Changed:   "+d.javaOSFileName+" "+d.getClass().getSimpleName());
-			}		
-			for(CUDrop d : reprocess) {
-				System.out.println("Reprocess: "+d.javaOSFileName+" "+d.getClass().getSimpleName());
-			}
-			*/
-			IDE.getInstance().setAdapting();
-			try {
-				for(CUDrop d : reprocess) {
-					clearPromiseDrops(d);
-					if (d instanceof PackageDrop) {
-						final PackageDrop pkg = (PackageDrop) d;						
-						for(Drop dependent : pkg.getDependents()) {
-							dependent.invalidate();
-						}						
-						runVersioned(new AbstractRunner() {
-							public void run() {
-								parsePackagePromises(pkg);
-							}
-						});
-					}
-				}
-				// Necessary to process these after package drops 
-				// to ensure that newly created drops don't invalidated
-				for(CUDrop d : reprocess) {
-					// Already cleared above
-					if (!(d instanceof PackageDrop)) {	
-						ConvertToIR.getInstance().registerClass(d.makeCodeInfo());
-					}
-				}
-			} finally {
-				IDE.getInstance().clearAdapting();
-			}
-		}
-
-		private void clearPromiseDrops(CUDrop d) {
-			// Clear out promise drops
-			//System.out.println("Reprocessing "+d.javaOSFileName);
-			for(IRNode n : JavaPromise.bottomUp(d.cu)) {
-				PromiseDropStorage.clearDrops(n);
-			}
-		}
 	}
 }
