955afcabd07222effa662fe06338c58b438b2274
Edwin Chan <edwin.chan@surelogic.com>
2010-03-24 18:32:35 +0000

    Added code to check all references consistently
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/fluid@14524 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/analysis/layers/CycleDetector.java
+++ src/com/surelogic/analysis/layers/CycleDetector.java
@@ -35,17 +35,17 @@ public class CycleDetector extends HashMap<String, Set<String>> {
 		for(Map.Entry<String, Set<String>> e : this.entrySet()) {
 			//System.out.println("Starting from "+e.getKey());
 			seen.clear();
-			checkForCycles(seen, e.getKey(), null);
+			checkForCycles(seen, e.getKey(), null, false);
 		}
 	}
 	
 	public boolean checkOne(String qname) {
 		Set<String> seen = new HashSet<String>();			
 		//System.out.println("Starting from "+computeCurrentName());
-		return checkForCycles(seen, qname, null);
+		return checkForCycles(seen, qname, null, true);
 	}
 	
-	private boolean checkForCycles(Set<String> seen, String here, String last) {
+	private boolean checkForCycles(Set<String> seen, String here, String last, boolean failFast) {
 		if (seen.contains(here)) {
 			//System.out.println("FAIL: "+here+" already seen");
 			reportFailure(here, last);
@@ -61,7 +61,7 @@ public class CycleDetector extends HashMap<String, Set<String>> {
 			return true; // No refs, so no cycle here
 		}
 		for(String ref : references) {
-			if (!checkForCycles(seen, ref, here)) {
+			if (!checkForCycles(seen, ref, here, failFast) && failFast) {
 				return false;
 			}
 		}
