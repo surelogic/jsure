de7977a84fd78247443263a171a17b5e66aa6c47
Edwin Chan <edwin.chan@surelogic.com>
2010-03-26 15:40:37 +0000

    Removed unused fields
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/fluid@14543 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/edu/cmu/cs/fluid/java/bind/UnversionedJavaImportTable.java
+++ src/edu/cmu/cs/fluid/java/bind/UnversionedJavaImportTable.java
@@ -55,14 +55,14 @@ public final class UnversionedJavaImportTable extends AbstractJavaImportTable {
    * A map of strings to Entry objects that hold the scope that
    * the name should be looked up in (again)
    */
-  final Map<String,Entry> direct = new HashMap<String,Entry>();
+  //final Map<String,Entry> direct = new HashMap<String,Entry>();
   
   /**
    * The scopes that are imported.  These implement the
    * imports that end in ".*".
    * A map from the import to an Entry object.
    */
-  final Map<IRNode,Entry> indirect = new HashMap<IRNode,Entry>();
+  //final Map<IRNode,Entry> indirect = new HashMap<IRNode,Entry>();
   
   private class UnversionedInfo extends AbstractDerivedInformation {
     @Override
