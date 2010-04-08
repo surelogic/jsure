b9f471eeda4b8627053edae002c9d6ab18348606
Aaron Greenhouse <aaron.greenhouse@surelogic.com>
2010-04-08 15:57:44 +0000

    Updated oracle: Changed so that PolicyLock also puts the warning about using outer object references as a lock in the reslts view and not as a modeling problem.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/regression@14612 d02ae147-ee12-4a35-9452-91c89e8b2592

--- TestLockSanityChecks/src/qualified_receivers_as_locks/Outer.java
+++ TestLockSanityChecks/src/qualified_receivers_as_locks/Outer.java
@@ -7,36 +7,36 @@ import com.surelogic.RegionLocks;
 
 /**
  * Test that the lock promise scrubber rejects annotations that use a
- * qualified receiver for the lock implementation.  This feature is not 
+ * qualified receiver for the lock implementation.  This feature is not
  * yet supported by the lock analysis (see bug 992).  The analysis implementation
  * currently assumes that lock fields come from the same object as the region
  * that is protected, and using qualified receivers violates this assumption.
  * This is something to be fixed in the future.
- * 
+ *
  * @author aarong
  *
  */
 public class Outer {
   @SuppressWarnings("unused")
   private final Object field = new Object();
-  
+
   @SuppressWarnings("unused")
   @RegionLocks({
     // Rejected with warning
-    @RegionLock("L is qualified_receivers_as_locks.Outer.this protects f" /* is UNASSOCIATED */),
+    @RegionLock("L is qualified_receivers_as_locks.Outer.this protects f" /* is CONSISTENT */),
     // Rejected with warning
-    @RegionLock("L2 is qualified_receivers_as_locks.Outer.this.field protects g" /* is UNASSOCIATED */)
+    @RegionLock("L2 is qualified_receivers_as_locks.Outer.this.field protects g" /* is CONSISTENT */)
   })
   @PolicyLocks({
     // Rejected with warning
-    @PolicyLock("P is qualified_receivers_as_locks.Outer.this" /* is UNASSOCIATED */),
+    @PolicyLock("P is qualified_receivers_as_locks.Outer.this" /* is CONSISTENT */),
     // Rejected with warning
-    @PolicyLock("P2 is qualified_receivers_as_locks.Outer.this.field" /* is UNASSOCIATED */)
+    @PolicyLock("P2 is qualified_receivers_as_locks.Outer.this.field" /* is CONSISTENT */)
   })
   private class Inner {
     private int f;
     private int g;
-    
+
     public void set(int v) {
       f = v;
     }
