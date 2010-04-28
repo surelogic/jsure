c2c96d26177dae53f1c6ed68e47924ec2776061b
Aaron Greenhouse <aaron.greenhouse@surelogic.com>
2010-04-28 20:28:28 +0000

    Regression test for public bug 37: effects on "null" method parameters should not be reported.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/regression@14765 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ TestNoMethodEffectsOnNull/src/test/T.java
@@ -0,0 +1,32 @@
+package test;
+
+import com.surelogic.Region;
+import com.surelogic.RegionEffects;
+import com.surelogic.Regions;
+
+@Regions({
+  @Region("public A"),
+  @Region("public B"),
+  @Region("public C")
+})
+public class T {
+  @RegionEffects("reads a:A, b:B, c:C")
+  private static void method(final T a, final T b, final T c) {
+    // blah
+  }
+
+
+
+  @RegionEffects("reads x:A, y:B, z:C")
+  public static void test(final T x, final T y, final T z) {
+    method(x, y, z);
+
+    method(x, y, null);
+    method(x, null, null);
+    method(null, null, null);
+
+    method(x, y, (null));
+    method(x, (((null))), (T) null);
+    method(((T) null), (T) ((T) ((T) null)), z);
+  }
+}
