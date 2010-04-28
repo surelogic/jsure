576975dfe50522e282f97ded5233df28f25860d2
Aaron Greenhouse <aaron.greenhouse@surelogic.com>
2010-04-28 20:40:08 +0000

    Updated to also test that we have no effects for nonsense like this:
    ((T) null).f = 10;
    
    
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/regression@14766 d02ae147-ee12-4a35-9452-91c89e8b2592

--- TestNoMethodEffectsOnNull/src/test/T.java
+++ TestNoMethodEffectsOnNull/src/test/T.java
@@ -10,12 +10,14 @@ import com.surelogic.Regions;
   @Region("public C")
 })
 public class T {
+	private int f;
+
+
+
   @RegionEffects("reads a:A, b:B, c:C")
   private static void method(final T a, final T b, final T c) {
     // blah
   }
-
-
 
   @RegionEffects("reads x:A, y:B, z:C")
   public static void test(final T x, final T y, final T z) {
@@ -28,5 +30,15 @@ public class T {
     method(x, y, (null));
     method(x, (((null))), (T) null);
     method(((T) null), (T) ((T) ((T) null)), z);
+  }
+
+
+
+  @RegionEffects("writes other:Instance")
+  public static void test2(final T other) {
+  	other.f = 0;
+  	((T) null).f = 10;
+  	((T) (T) (T) (null)).f = 100;
+  	(((T) ((T) (null)))).f = 1000;
   }
 }
