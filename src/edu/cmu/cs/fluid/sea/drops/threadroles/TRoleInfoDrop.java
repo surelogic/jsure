d45677062e25c592b14bb3ecd52c44a868efe1fe
Dean F. Sutherland <dfsuther@cs.cmu.edu>
2010-05-27 14:06:30 +0000

    now remember to implement IThreadRoleDrop
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/fluid@14936 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/edu/cmu/cs/fluid/sea/drops/threadroles/TRoleInfoDrop.java
+++ src/edu/cmu/cs/fluid/sea/drops/threadroles/TRoleInfoDrop.java
@@ -3,7 +3,7 @@ package edu.cmu.cs.fluid.sea.drops.threadroles;
 
 import edu.cmu.cs.fluid.sea.InfoDrop;
 
-public class TRoleInfoDrop extends InfoDrop {
+public class TRoleInfoDrop extends InfoDrop implements IThreadRoleDrop {
 	// No changes from super, except to implement IThreadRoleDrop so that
 	// regression tests can distinguish ThreadRole results from other results.
 }
