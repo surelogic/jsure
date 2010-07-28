415802ae410f4c105767a159ab2aff1c9fb12c31
Edwin Chan <edwin.chan@surelogic.com>
2010-07-28 15:48:35 +0000

    Factored out constants for command names
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@15312 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/jsure/client/eclipse/analysis/ScriptCommands.java
@@ -0,0 +1,15 @@
+package com.surelogic.jsure.client.eclipse.analysis;
+
+public interface ScriptCommands {
+	String IMPORT = "import";
+	String TOUCH_FILE = "touchFile";
+	String PATCH_FILE = "patchFile";
+	String DELETE_FILE = "deleteFile";
+	String CREATE_PROJECT = "createProject";
+	String OPEN_PROJECT = "openProject";
+	String CLOSE_PROJECT = "closeProject";
+    String ADD_NATURE = "addNature";
+    String REMOVE_NATURE = "removeNature";
+    String EXPORT_RESULTS = "exportResults";
+    String COMPARE_RESULTS = "compareResults";
+}
