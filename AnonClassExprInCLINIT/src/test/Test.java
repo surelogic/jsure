4a61f2cce79ed23230220d9a66339e16a8a7aa9a
Aaron Greenhouse <aaron.greenhouse@surelogic.com>
2010-08-24 15:30:20 +0000

    Moved to the regression folder.  I accidently checked this into the wrong spot at first.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/regression@15421 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ AnonClassExprInCLINIT/src/test/Test.java
@@ -0,0 +1,35 @@
+package test;
+
+import com.surelogic.Borrowed;
+import com.surelogic.RegionEffects;
+import com.surelogic.Unique;
+
+public class Test {
+
+	@Unique
+	private static final D d = new D();
+
+	private class Inner {
+		// blah
+	}
+
+	static {
+		/* Used to get a "no 'this' in scope" error here because analysis
+		 * erroneously assumed that we were in a non-static context.  Now this
+		 * should assure just fine.
+		 */
+		final Inner i = new Test().new Inner() {
+		};
+	}
+}
+
+class D {
+	@Unique("return")
+	public D() {
+	}
+
+	@RegionEffects("writes Instance")
+	@Borrowed("this")
+	public void m() {
+	}
+}
