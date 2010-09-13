1a9cdc05a1d8b4849e766fc09bb75ecbdd091da7
Edwin Chan <edwin.chan@surelogic.com>
2010-09-13 20:09:11 +0000

    Changed to work with multiple files
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-tests@15499 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/test/scripting/TouchFile.java
+++ src/com/surelogic/test/scripting/TouchFile.java
@@ -3,21 +3,20 @@ package com.surelogic.test.scripting;
 import java.io.*;
 
 import org.eclipse.core.resources.*;
-import org.eclipse.core.runtime.*;
 
 /**
  * Touch a file, or create it if it doesn't exist
  * 
  * @author Edwin
  */
-public class TouchFile extends AbstractCommand {
-  public boolean execute(ICommandContext context, String... contents) throws CoreException {
-    IFile f = resolveIFile(contents[1], true);
-    if (f.exists()) {
-      f.touch(null);
-    } else {
-      f.create(new ByteArrayInputStream(noBytes), true, null);
-    }  
-    return true;
-  }
+public class TouchFile extends AbstractFileCommand {
+	@Override
+	protected boolean execute(ICommandContext context, IFile f) throws Exception {
+		if (f.exists()) {
+			f.touch(null);
+		} else {
+			f.create(new ByteArrayInputStream(noBytes), true, null);
+		}  
+		return true;
+	}
 }
