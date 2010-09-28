16d0ee962b99fdfed86a4da3fc1045cd0c99296d
Edwin Chan <edwin.chan@surelogic.com>
2010-09-28 18:03:01 +0000

    Generalized findScript() to findFile() to help find analysis settings
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-tests@15583 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/tests/RegressionTest.java
+++ src/com/surelogic/jsure/tests/RegressionTest.java
@@ -305,7 +305,7 @@ public class RegressionTest extends TestCase implements IAnalysisListener {
         continue;
       }
       if (p.isOpen()) {
-    	  File script = findScript(p, false);
+    	  File script = findFile(p, ScriptCommands.NAME, false);
     	  if (script != null || Nature.hasNature(p)) {
     		  assertNull("More than one project to analyze!?!", project);
     		  project = p;
@@ -363,24 +363,24 @@ public class RegressionTest extends TestCase implements IAnalysisListener {
   private ITestOutput output = null;
   private ITest currentTest = null;
   
-  private File findScript(final IProject project, boolean checkParent) {
+  private File findFile(final IProject project, final String file, boolean checkParent) {
 	  final String projectPath  = project.getLocation().toOSString();
 	  File proj   = new File(projectPath);
-	  File script = lookForScript(proj);
-	  if (checkParent && script == null) {
+	  File result = lookForFile(proj, file);
+	  if (checkParent && result == null) {
 		  // Check for script in parent dir if multiple projects
-		  script = lookForScript(proj.getParentFile());
+		  result = lookForFile(proj.getParentFile(), file);
 	  }
-	  return script;
+	  return result;
   }
   
-  private File lookForScript(final File project) {
-	  File script = new File(project, ScriptCommands.NAME);
-	  if (script.exists() && script.isFile()) {
-		  System.out.println("Found script: "+script);
-		  return script;
+  private File lookForFile(final File project, final String file) {
+	  File result = new File(project, file);
+	  if (result.exists() && result.isFile()) {
+		  System.out.println("Found "+file+": "+result);
+		  return result;
 	  }
-	  System.out.println("Couldn't find script: "+script);
+	  System.out.println("Couldn't find "+file+": "+result);
 	  return null;
   }
   
@@ -390,8 +390,7 @@ public class RegressionTest extends TestCase implements IAnalysisListener {
     currentTest = start("Check for analysis settings");
     printActivatedAnalyses();
     
-    final File analyses = new File(projectPath + File.separatorChar
-        + ScriptCommands.ANALYSIS_SETTINGS);
+    final File analyses = findFile(project, ScriptCommands.ANALYSIS_SETTINGS, true);
     if (analyses.exists() && analyses.isFile()) {
       System.out.println("Found project-specific analysis settings.");
       IPreferenceStore store = JSureAnalysisXMLReader.readStateFrom(analyses);
@@ -424,7 +423,7 @@ public class RegressionTest extends TestCase implements IAnalysisListener {
     boolean resultsOk = true;
     
     // Check for script in the project to execute      
-    File script = findScript(project, true);
+    File script = findFile(project, ScriptCommands.NAME, true);
     if (script != null) {
       currentTest = start("Run scripting");
       ScriptReader r = new ScriptReader(project);
