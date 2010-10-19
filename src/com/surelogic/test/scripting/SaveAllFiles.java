e6c9705dff3258073c2061cb07fc1733d9ec50e7
Edwin Chan <edwin.chan@surelogic.com>
2010-10-19 20:24:34 +0000

    Removed old scripting code
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-tests@15691 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/test/scripting/SaveAllFiles.java
+++
@@ -1,7 +0,0 @@
-package com.surelogic.test.scripting;
-
-public class SaveAllFiles extends AbstractCommand {
-  public boolean execute(ICommandContext context, String... contents) {
-    throw new UnsupportedOperationException();
-  }
-}
