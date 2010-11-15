b98c4f1542f614c3cfd64bf938a8c9386498233f
Edwin Chan <edwin.chan@surelogic.com>
2010-11-15 14:46:27 +0000

    Factored out some drag and drop code
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@15892 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/edu/cmu/cs/fluid/dcf/views/coe/ProblemsView.java
+++ src/edu/cmu/cs/fluid/dcf/views/coe/ProblemsView.java
@@ -3,8 +3,6 @@ package edu.cmu.cs.fluid.dcf.views.coe;
 import org.eclipse.jface.action.*;
 import org.eclipse.jface.viewers.*;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.dnd.TextTransfer;
-import org.eclipse.swt.dnd.Transfer;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.actions.ActionFactory;
 
@@ -12,28 +10,16 @@ import edu.cmu.cs.fluid.dcf.views.*;
 import edu.cmu.cs.fluid.sea.*;
 
 public class ProblemsView extends AbstractResultsTableView<IDropInfo> {
-	private final Action f_copy = new Action() {
-		@Override
-		public void run() {
-			clipboard.setContents(new Object[] { getSelectedText() },
-					new Transfer[] { TextTransfer.getInstance() });
-		}
-	};
+	private final Action f_copy = makeCopyAction("Copy", "Copy the selected problem to the clipboard");
 	
 	public ProblemsView() {
 		super(SWT.NONE, IDropInfo.class, new ProblemsViewContentProvider());
 	}
 
 	@Override
-	protected void makeActions() {
-	    f_copy.setText("Copy");
-	    f_copy.setToolTipText("Copy the selected problem to the clipboard");
+	protected void fillGlobalActionHandlers(IActionBars bars) {
+		bars.setGlobalActionHandler(ActionFactory.COPY.getId(), f_copy);
 	}
-
-	@Override
-	  protected void fillGlobalActionHandlers(IActionBars bars) {
-	    bars.setGlobalActionHandler(ActionFactory.COPY.getId(), f_copy);
-	  }
 	
 	@Override
 	protected void fillContextMenu(IMenuManager manager, IStructuredSelection s) {
