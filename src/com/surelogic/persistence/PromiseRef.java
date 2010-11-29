855a895225842de2315d1308c970eee6a551afe5
Edwin Chan <edwin.chan@surelogic.com>
2010-11-29 20:15:57 +0000

    Data structure to hold info about promise refs
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/fluid@15948 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/persistence/PromiseRef.java
@@ -0,0 +1,31 @@
+/*$Header: /cvs/fluid/fluid/.settings/org.eclipse.jdt.ui.prefs,v 1.2 2006/03/27 21:35:50 boyland Exp $*/
+package com.surelogic.persistence;
+
+import com.surelogic.aast.IAASTRootNode;
+import com.surelogic.analysis.*;
+
+import edu.cmu.cs.fluid.ir.IRNode;
+import edu.cmu.cs.fluid.sea.PromiseDrop;
+
+public class PromiseRef {
+	private final String location;
+	private final String promise;
+	private final String contents;
+	private final boolean useImplication;
+	
+	public PromiseRef(String anno, String details, IRNode decl, boolean implies) {
+		final IIRProject p = JavaProjects.getEnclosingProject(decl);
+		location = JavaIdentifier.encodeDecl(p.getTypeEnv().getBinder(), decl);
+		promise = anno;
+		contents = details;
+		useImplication = implies;
+	}
+	
+	public PromiseRef(String anno, String details, IRNode decl) {
+		this(anno, details, decl, true);
+	}
+	
+	public <T extends IAASTRootNode> PromiseRef(PromiseDrop<T> d) {
+		this(d.getPromiseName(), d.getAST().toString(), d.getNode(), false);
+	}
+}
