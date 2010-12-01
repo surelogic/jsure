2f42cfa486d3fe6d07b31afcc6d0a59eda745204
Edwin Chan <edwin.chan@surelogic.com>
2010-12-01 14:07:20 +0000

    Constants for XML persistence
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/fluid@15958 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/persistence/PersistenceConstants.java
@@ -0,0 +1,11 @@
+/*$Header: /cvs/fluid/fluid/.settings/org.eclipse.jdt.ui.prefs,v 1.2 2006/03/27 21:35:50 boyland Exp $*/
+package com.surelogic.persistence;
+
+/**
+ * Constants for XML persistence
+ */
+public interface PersistenceConstants {
+	String COMP_UNIT = "compUnit";
+	String ANALYSIS = "analysis";
+	String RESULT = "result";
+}
