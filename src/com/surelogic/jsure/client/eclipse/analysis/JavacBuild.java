2c7af86c1da6308a85902425e6b5f664f4474697
Edwin Chan <edwin.chan@surelogic.com>
2011-01-13 20:00:38 +0000

    Added code to collect resources for build
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@16151 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/client/eclipse/analysis/JavacBuild.java
+++ src/com/surelogic/jsure/client/eclipse/analysis/JavacBuild.java
@@ -1,23 +1,43 @@
 package com.surelogic.jsure.client.eclipse.analysis;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.core.IJavaProject;
 
-import com.surelogic.common.eclipse.BalloonUtility;
-import com.surelogic.common.eclipse.JDTUtility;
+import com.surelogic.common.eclipse.*;
+
+import edu.cmu.cs.fluid.dc.Majordomo;
+import edu.cmu.cs.fluid.util.Pair;
 
 public class JavacBuild {
+	private static List<Pair<IResource, Integer>> pairUpResources(List<IResource> resources, Integer kind) {
+		if (resources.isEmpty()) {
+			return Collections.emptyList();
+		}
+		List<Pair<IResource, Integer>> result = new ArrayList<Pair<IResource,Integer>>(resources.size());
+		for(IResource r : resources) {
+			result.add(new Pair<IResource, Integer>(r, kind));
+		}
+		return result;
+	}
+	
+	private static final Map<?,?>  buildArgs = 
+		Collections.singletonMap(Majordomo.BUILD_KIND, Integer.toString(IncrementalProjectBuilder.FULL_BUILD));
+	
 	public static void analyze(List<IJavaProject> selectedProjects) {
 		try {
 			for(IJavaProject p : selectedProjects) {			
 				boolean noErrors = JDTUtility.noCompilationErrors(p, new NullProgressMonitor());		
 				if (noErrors) {
-					// TODO Collect resources and CUs
-					JavacDriver.getInstance().registerBuild(p.getProject(), Collections.EMPTY_MAP, null, null);
+					// Collect resources and CUs for build
+					JavaProjectResources jpr = JDTUtility.collectAllResources(p);
+					JavacDriver.getInstance().registerBuild(p.getProject(), buildArgs,							
+							pairUpResources(jpr.resources, IResourceDelta.ADDED), jpr.cus);
 				} else {
 					// TODO what error to print?
 					BalloonUtility.showMessage("Compile Errors in "+p.getElementName(), 
@@ -28,7 +48,7 @@ public class JavacBuild {
 			}
 			JavacEclipse.initialize();
 			System.out.println("Configuring build");	
-			JavacDriver.getInstance().configureBuild(Collections.EMPTY_MAP);
+			JavacDriver.getInstance().configureBuild(buildArgs);
 		} catch (CoreException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
