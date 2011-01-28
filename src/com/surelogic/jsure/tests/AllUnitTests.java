0191ac2dede72ec91913dd01dc4f7ae200039017
Tim Halloran <tim.halloran@surelogic.com>
2011-01-28 20:15:43 +0000

    Put back JUnit tests.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-tests@16449 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/tests/AllUnitTests.java
+++ src/com/surelogic/jsure/tests/AllUnitTests.java
@@ -1,25 +1,31 @@
 package com.surelogic.jsure.tests;
 
 import junit.framework.TestSuite;
+import edu.cmu.cs.fluid.ir.TestIRPackage;
+import edu.cmu.cs.fluid.java.analysis.TestJavaAnalysisPackage;
+import edu.cmu.cs.fluid.sea.test.*;
+import edu.cmu.cs.fluid.tree.TestTreePackage;
+import edu.cmu.cs.fluid.util.TestMultiMap;
+import edu.cmu.cs.fluid.version.TestVersionPackage;
 
 public class AllUnitTests extends TestSuite {
 
 	public AllUnitTests() {
-		//addTest(new TestSuite(TestSea.class));
-		//addTest(new TestSuite(TestDrop.class));
-		//addTest(new TestSuite(TestPromiseDrop.class));
+		addTest(new TestSuite(TestSea.class));
+		addTest(new TestSuite(TestDrop.class));
+		addTest(new TestSuite(TestPromiseDrop.class));
 
-		//addTest(new TestSuite(TestMultiMap.class));
-		//addTest(new TestSuite(TestUtilPackage.class));
-		//addTest(new TestSuite(TestIRPackage.class));
-		//addTest(new TestSuite(TestTreePackage.class));
+		addTest(new TestSuite(TestMultiMap.class));
+		// addTest(new TestSuite(TestUtilPackage.class));
+		addTest(new TestSuite(TestIRPackage.class));
+		addTest(new TestSuite(TestTreePackage.class));
 
 		// Has some problems running with unversioned tests
-		//addTest(new TestSuite(TestVersionPackage.class));
+		addTest(new TestSuite(TestVersionPackage.class));
 
-		//addTest(new TestSuite(TestJavaAnalysisPackage.class));
+		addTest(new TestSuite(TestJavaAnalysisPackage.class));
 		addTest(new TestSuite(TestTaskFramework.class));
-        addTest(new TestSuite(TestSLAnnotationsParser.class));
+		addTest(new TestSuite(TestSLAnnotationsParser.class));
 		addTest(new TestSuite(SLParseTest.class));
 		addTest(new TestSuite(TestScopedPromiseMatching.class));
 		addTest(new TestSuite(TestAASTCloning.class));
--- src/com/surelogic/jsure/tests/TestPersistentSave.java
+++ src/com/surelogic/jsure/tests/TestPersistentSave.java
@@ -1,12 +1,13 @@
 package com.surelogic.jsure.tests;
 
-import edu.cmu.cs.fluid.version.*;
-import junit.framework.*;
+import junit.framework.TestCase;
+import edu.cmu.cs.fluid.version.TestPersistent;
 
 public class TestPersistentSave extends TestCase {
-  public void testSave() {
-//    int failures = TestPersistent.test("--store --quiet --zip --vic --trace "+
-//                        "--testdir ../build/plugins/fluid/src/edu/cmu/cs/fluid/version --test all.tst");
-//    assertEquals(0, failures);
-  }
+	public void testSave() {
+		int failures = TestPersistent
+				.test("--store --quiet --zip --vic --trace "
+						+ "--testdir ../build/plugins/fluid/src/edu/cmu/cs/fluid/version --test all.tst");
+		assertEquals(0, failures);
+	}
 }
