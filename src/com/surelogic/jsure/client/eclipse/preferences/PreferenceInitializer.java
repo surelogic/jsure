0700641e637ff4cb7112e53ca4017f5c0b0c5346
Tim Halloran <tim.halloran@surelogic.com>
2011-02-03 18:13:39 +0000

    Working on splitting out non-UI portions of Eclipse common
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@16624 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/client/eclipse/preferences/PreferenceInitializer.java
+++ src/com/surelogic/jsure/client/eclipse/preferences/PreferenceInitializer.java
@@ -1,22 +1,25 @@
 package com.surelogic.jsure.client.eclipse.preferences;
 
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.jface.preference.IPreferenceStore;
 
-import com.surelogic.common.core.preferences.AbstractPrefInitializer;
 import com.surelogic.jsure.client.eclipse.Activator;
 
 /**
  * Class used to initialize default preference values.
  */
-public class PreferenceInitializer extends AbstractPrefInitializer {
+public class PreferenceInitializer extends AbstractPreferenceInitializer {
 
-  @Override
-  public void initializeDefaultPreferences() {
-    final IPreferenceStore store = Activator.getDefault().getPreferenceStore();
-    store.setDefault(PreferenceConstants.P_PROMPT_PERSPECTIVE_SWITCH, true);
-    store.setDefault(PreferenceConstants.P_AUTO_PERSPECTIVE_SWITCH, false);
-    store.setDefault(PreferenceConstants.P_AUTO_OPEN_MODELING_PROBLEMS_VIEW, true);
-    store.setDefault(PreferenceConstants.P_AUTO_OPEN_PROPOSED_PROMISE_VIEW, true);
-    store.setDefault(PreferenceConstants.P_AUTO_ANALYZE_ON_BUILD, true);
-  }
+	@Override
+	public void initializeDefaultPreferences() {
+		final IPreferenceStore store = Activator.getDefault()
+				.getPreferenceStore();
+		store.setDefault(PreferenceConstants.P_PROMPT_PERSPECTIVE_SWITCH, true);
+		store.setDefault(PreferenceConstants.P_AUTO_PERSPECTIVE_SWITCH, false);
+		store.setDefault(
+				PreferenceConstants.P_AUTO_OPEN_MODELING_PROBLEMS_VIEW, true);
+		store.setDefault(PreferenceConstants.P_AUTO_OPEN_PROPOSED_PROMISE_VIEW,
+				true);
+		store.setDefault(PreferenceConstants.P_AUTO_ANALYZE_ON_BUILD, true);
+	}
 }
