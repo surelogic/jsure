ef3b7bb586cfea2011428578c072fc1e7ab73438
Tim Halloran <tim.halloran@surelogic.com>
2011-02-04 18:33:23 +0000

    Changed constant name
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/fluid@16663 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/edu/cmu/cs/fluid/ide/IDEPreferences.java
+++ src/edu/cmu/cs/fluid/ide/IDEPreferences.java
@@ -4,7 +4,7 @@ public interface IDEPreferences {
 	String PREFIX = "edu.cmu.cs.fluid.";
 	String ALLOW_JAVADOC_ANNOS = PREFIX + "allow.javadoc.annos";
 	String ANALYSIS_THREAD_COUNT = PREFIX + "analysis.thread.count";
-	String DATA_DIRECTORY = PREFIX + "data.directory";
+	String JSURE_DATA_DIRECTORY = PREFIX + "data.directory";
 	String DEFAULT_JRE = PREFIX + "default.JRE";
 
 	/**
