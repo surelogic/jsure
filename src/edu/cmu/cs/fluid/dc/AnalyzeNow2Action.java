12a6c3a9c756947c08fdf7f3c9b294cd8db52f78
Tim Halloran <tim.halloran@surelogic.com>
2011-02-04 21:52:32 +0000

    Fixing preferences.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@16702 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/edu/cmu/cs/fluid/dc/AnalyzeNow2Action.java
+++ src/edu/cmu/cs/fluid/dc/AnalyzeNow2Action.java
@@ -1,30 +1,24 @@
 package edu.cmu.cs.fluid.dc;
 
-import java.util.*;
+import java.util.List;
 
 import org.eclipse.jdt.core.IJavaProject;
 
 import com.surelogic.common.ui.actions.AbstractProjectSelectedMenuAction;
 import com.surelogic.common.ui.dialogs.JavaProjectSelectionDialog;
-import com.surelogic.common.core.preferences.IPreferenceAccessor;
-import com.surelogic.jsure.client.eclipse.analysis.*;
+import com.surelogic.jsure.client.eclipse.analysis.JavacBuild;
 
 public class AnalyzeNow2Action extends AbstractProjectSelectedMenuAction {
-	protected JavaProjectSelectionDialog.Config getDialogInfo(List<IJavaProject> selectedProjects) {
-		return new JavaProjectSelectionDialog.Config("Select project(s) to analyze", "Analyze Project", 
-				null, selectedProjects, new IPreferenceAccessor<Boolean>() {					
-					public void set(Boolean newValue) {
-						// TODO Auto-generated method stub						
-					}					
-					public Boolean get() {
-						// TODO Auto-generated method stub
-						return true;
-					}
-				});
+	protected JavaProjectSelectionDialog.Config getDialogInfo(
+			List<IJavaProject> selectedProjects) {
+		return new JavaProjectSelectionDialog.Config(
+				"Select project(s) to analyze", "Analyze Project", null,
+				selectedProjects);
 	}
-	
+
 	@Override
-	protected void run(List<IJavaProject> selectedProjects,	List<String> projectNames) {
+	protected void run(List<IJavaProject> selectedProjects,
+			List<String> projectNames) {
 		// TODO Check if this includes dependencies
 		// If not, show confirmation dialog
 		JavacBuild.analyze(selectedProjects);
