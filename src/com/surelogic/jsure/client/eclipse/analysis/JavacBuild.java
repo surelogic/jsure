1bac482cb4725708bab81d98317f77d70bc9c794
Tim Halloran <tim.halloran@surelogic.com>
2011-02-07 21:15:32 +0000

    Fixed main menu of JSure
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@16770 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/client/eclipse/analysis/JavacBuild.java
+++ src/com/surelogic/jsure/client/eclipse/analysis/JavacBuild.java
@@ -1,6 +1,7 @@
 package com.surelogic.jsure.client.eclipse.analysis;
 
 import java.util.*;
+import java.util.logging.Level;
 
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceDelta;
@@ -11,51 +12,64 @@ import org.eclipse.jdt.core.IJavaProject;
 
 import com.surelogic.common.core.JDTUtility;
 import com.surelogic.common.core.JavaProjectResources;
+import com.surelogic.common.logging.SLLogger;
 import com.surelogic.common.ui.BalloonUtility;
-
 
 import edu.cmu.cs.fluid.dc.Majordomo;
 import edu.cmu.cs.fluid.util.Pair;
 
 public class JavacBuild {
-	private static List<Pair<IResource, Integer>> pairUpResources(List<IResource> resources, Integer kind) {
+	private static List<Pair<IResource, Integer>> pairUpResources(
+			List<IResource> resources, Integer kind) {
 		if (resources.isEmpty()) {
 			return Collections.emptyList();
 		}
-		List<Pair<IResource, Integer>> result = new ArrayList<Pair<IResource,Integer>>(resources.size());
-		for(IResource r : resources) {
+		List<Pair<IResource, Integer>> result = new ArrayList<Pair<IResource, Integer>>(
+				resources.size());
+		for (IResource r : resources) {
 			result.add(new Pair<IResource, Integer>(r, kind));
 		}
 		return result;
 	}
-	
-	private static final Map<?,?>  buildArgs = 
-		Collections.singletonMap(Majordomo.BUILD_KIND, Integer.toString(IncrementalProjectBuilder.FULL_BUILD));
-	
+
+	private static final Map<?, ?> buildArgs = Collections.singletonMap(
+			Majordomo.BUILD_KIND,
+			Integer.toString(IncrementalProjectBuilder.FULL_BUILD));
+
 	public static void analyze(List<IJavaProject> selectedProjects) {
+		if (selectedProjects == null || selectedProjects.isEmpty())
+			return;
 		try {
-			for(IJavaProject p : selectedProjects) {			
-				boolean noErrors = JDTUtility.noCompilationErrors(p, new NullProgressMonitor());		
+			for (IJavaProject p : selectedProjects) {
+				boolean noErrors = JDTUtility.noCompilationErrors(p,
+						new NullProgressMonitor());
 				if (noErrors) {
 					// Collect resources and CUs for build
-					JavaProjectResources jpr = JDTUtility.collectAllResources(p);
-					JavacDriver.getInstance().registerBuild(p.getProject(), buildArgs,							
-							pairUpResources(jpr.resources, IResourceDelta.ADDED), jpr.cus);
+					JavaProjectResources jpr = JDTUtility
+							.collectAllResources(p);
+					JavacDriver.getInstance()
+							.registerBuild(
+									p.getProject(),
+									buildArgs,
+									pairUpResources(jpr.resources,
+											IResourceDelta.ADDED), jpr.cus);
 				} else {
 					// TODO what error to print?
-					BalloonUtility.showMessage("Compile Errors in "+p.getElementName(), 
-							"JSure is unable to analyze "+p.getElementName()+
-							" due to some compilation errors.  Please fix (or do a clean build).");
+					BalloonUtility
+							.showMessage(
+									"Compile Errors in " + p.getElementName(),
+									"JSure is unable to analyze "
+											+ p.getElementName()
+											+ " due to some compilation errors.  Please fix (or do a clean build).");
 					return;
 				}
 			}
 			JavacEclipse.initialize();
-			System.out.println("Configuring build");	
+			System.out.println("Configuring build");
 			JavacDriver.getInstance().configureBuild(buildArgs, true);
 		} catch (CoreException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}	
+			SLLogger.getLogger().log(Level.SEVERE,
+					"Failure setting up to analyze: " + selectedProjects, e);
+		}
 	}
 }
-
--- src/edu/cmu/cs/fluid/dc/AnalyzeNow2Action.java
+++ src/edu/cmu/cs/fluid/dc/AnalyzeNow2Action.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 import org.eclipse.jdt.core.IJavaProject;
 
+import com.surelogic.common.jobs.NullSLProgressMonitor;
+import com.surelogic.common.jobs.SLStatus;
+import com.surelogic.common.license.SLLicenseProduct;
+import com.surelogic.common.license.SLLicenseUtility;
+import com.surelogic.common.logging.SLLogger;
 import com.surelogic.common.ui.actions.AbstractProjectSelectedMenuAction;
 import com.surelogic.common.ui.dialogs.JavaProjectSelectionDialog;
 import com.surelogic.jsure.client.eclipse.analysis.JavacBuild;
@@ -19,8 +24,18 @@ public class AnalyzeNow2Action extends AbstractProjectSelectedMenuAction {
 	@Override
 	protected void run(List<IJavaProject> selectedProjects,
 			List<String> projectNames) {
-		// TODO Check if this includes dependencies
-		// If not, show confirmation dialog
+
+		/*
+		 * License check: A hack because JSure is not using SLJobs yet.
+		 */
+		final SLStatus failed = SLLicenseUtility.validateSLJob(
+				SLLicenseProduct.JSURE, new NullSLProgressMonitor());
+		if (failed != null) {
+			SLLogger.getLogger().log(failed.getSeverity().toLevel(),
+					failed.getMessage(), failed.getException());
+			return;
+		}
+
 		JavacBuild.analyze(selectedProjects);
 	}
 }
