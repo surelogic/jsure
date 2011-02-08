51fa3dbbaadd76f2004c860d227e1ff31c14ef72
Edwin Chan <edwin.chan@surelogic.com>
2011-02-08 18:52:59 +0000

    Moved initialization to jsure-core-eclipse
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-core-eclipse@16815 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/core/Activator.java
+++ src/com/surelogic/jsure/core/Activator.java
@@ -1,8 +1,13 @@
 package com.surelogic.jsure.core;
 
+import java.io.File;
+
 import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
+import com.surelogic.common.FileUtility;
+import com.surelogic.fluid.javac.Util;
+import com.surelogic.jsure.core.driver.JavacDriver;
 import com.surelogic.jsure.core.preferences.JSurePreferencesUtility;
 
 import edu.cmu.cs.fluid.ide.IDE;
@@ -41,8 +46,31 @@ public class Activator extends Plugin {
 			doubleChecker = new com.surelogic.jsure.core.driver.DoubleChecker();
 			doubleChecker.start(context);
 		}
+		
+		if (!Util.useResultsXML) {
+			clearJSureData();
+		}
+		//monitor.worked(1);
+		
+		// TODO reload persistent data
+		Eclipse.initialize();
+		//monitor.worked(1);
+		
+		// NotificationHub.addAnalysisListener(ConsistencyListener.prototype);
+		JavacDriver.getInstance();		
+		//monitor.worked(1);
 	}
 
+	private void clearJSureData() {
+		for (File f : JSurePreferencesUtility.getJSureDataDirectory()
+				.listFiles()) {
+			if (f.isDirectory()) {
+				FileUtility.recursiveDelete(f, false);
+			}
+			// i.e. don't delete persistent files
+		}
+	}
+	
 	@Override
 	public void stop(BundleContext context) throws Exception {
 		try {
