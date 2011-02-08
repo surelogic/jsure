5850af242078344889f40c54417dc708a822ae6a
Tim Halloran <tim.halloran@surelogic.com>
2011-02-08 21:39:01 +0000

    Fixing main menu of Eclipse
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@16838 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/client/eclipse/actions/NewScanAction.java
+++ src/com/surelogic/jsure/client/eclipse/actions/NewScanAction.java
@@ -4,14 +4,18 @@ import java.util.List;
 
 import org.eclipse.jdt.core.IJavaProject;
 
+import com.surelogic.common.CommonImages;
 import com.surelogic.common.ui.BalloonUtility;
+import com.surelogic.common.ui.SLImages;
 import com.surelogic.common.ui.actions.AbstractProjectSelectedMenuAction;
+import com.surelogic.common.ui.dialogs.JavaProjectSelectionDialog;
 import com.surelogic.common.jobs.NullSLProgressMonitor;
 import com.surelogic.common.jobs.SLStatus;
 import com.surelogic.common.license.SLLicenseProduct;
 import com.surelogic.common.license.SLLicenseUtility;
 import com.surelogic.common.logging.SLLogger;
 import com.surelogic.jsure.core.driver.JavacBuild;
+import com.surelogic.jsure.core.preferences.JSurePreferencesUtility;
 
 public class NewScanAction extends AbstractProjectSelectedMenuAction {
 	@Override
@@ -30,5 +34,16 @@ public class NewScanAction extends AbstractProjectSelectedMenuAction {
 		}
 
 		JavacBuild.analyze(selectedProjects, BalloonUtility.errorListener);
+	}
+
+	@Override
+	protected JavaProjectSelectionDialog.Configuration getDialogInfo(
+			List<IJavaProject> selectedProjects) {
+		return new JavaProjectSelectionDialog.Configuration(
+				"Select project(s) to verify:",
+				"Verify Project",
+				SLImages.getImage(CommonImages.IMG_JSURE_VERIFY),
+				selectedProjects,
+				JSurePreferencesUtility.ALWAYS_ALLOW_USER_TO_SELECT_PROJECTS_TO_SCAN);
 	}
 }
