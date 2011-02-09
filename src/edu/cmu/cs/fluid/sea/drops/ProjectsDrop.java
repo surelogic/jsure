453986812897f1acb557cef5db1a8776b087c164
Tim Halloran <tim.halloran@surelogic.com>
2011-02-09 18:14:39 +0000

    made private fields and fixed comments
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/fluid@16845 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/edu/cmu/cs/fluid/sea/drops/ProjectsDrop.java
+++ src/edu/cmu/cs/fluid/sea/drops/ProjectsDrop.java
@@ -1,81 +1,85 @@
 package edu.cmu.cs.fluid.sea.drops;
 
-import com.surelogic.analysis.*;
+import com.surelogic.analysis.IIRProjects;
 
 import edu.cmu.cs.fluid.sea.Drop;
 import edu.cmu.cs.fluid.sea.Sea;
-import edu.cmu.cs.fluid.sea.xml.*;
+import edu.cmu.cs.fluid.sea.xml.AbstractSeaXmlCreator;
 
 public class ProjectsDrop extends Drop {
-  static ProjectsDrop active = null;
-  IIRProjects projects;
-  
-  private ProjectsDrop(IIRProjects p) {
-	  projects = p;
-	  synchronized (ProjectsDrop.class) {
-		  active = null;
-	  }
-  }
+	static private ProjectsDrop active = null;
+	private IIRProjects projects;
 
-  public static ProjectsDrop ensureDrop(IIRProjects p) {
-	  ProjectsDrop match = null;
-	  for(ProjectsDrop pd : Sea.getDefault().getDropsOfExactType(ProjectsDrop.class)) {
-		  if (pd.projects != p) {
-			  pd.invalidate();
-		  } else {
-			  match = pd;
-		  }
-	  }
-	  if (match != null) {
-		  return match;
-	  }
-	  return new ProjectsDrop(p);
-  }
-  
-  @Override
-  public String getEntityName() {
-	  return "projects-drop";
-  }	
-  
-  @Override
-  public void snapshotAttrs(AbstractSeaXmlCreator s) {
-	  super.snapshotAttrs(s);
-  }
-  
-  public IIRProjects getIIRProjects() {
-	  return projects;
-  }
-  
-  public void setProjects(IIRProjects p) {
-	  projects = p;
-  }
-  
-  public static synchronized ProjectsDrop getDrop() {
-	  if (active != null) {
-		  if (active.isValid()) {
-			  return active;
-		  }
-		  active = null;
-	  }	  
-	  ProjectsDrop p = null;
-	  for (ProjectsDrop pd : Sea.getDefault().getDropsOfExactType(
-			  ProjectsDrop.class)) {
-		  if (p == null) {
-			  p = pd;
-		  } else {
-			  LOG.info("Multiple projects analyzed for JSure: "
-					  + pd.getIIRProjects().getLabel());
-		  }
-	  }
-	  active = p; 	  
-	  return p;
-  }
-  
-  public static IIRProjects getProjects() {
-	  ProjectsDrop pd = getDrop();
-	  if (pd == null) {
-		  return null;
-	  }
-	  return pd.getIIRProjects();
-  }
+	private ProjectsDrop(IIRProjects p) {
+		projects = p;
+		synchronized (ProjectsDrop.class) {
+			active = null;
+		}
+	}
+
+	public static ProjectsDrop ensureDrop(IIRProjects p) {
+		ProjectsDrop match = null;
+		for (ProjectsDrop pd : Sea.getDefault().getDropsOfExactType(
+				ProjectsDrop.class)) {
+			if (pd.projects != p) {
+				pd.invalidate();
+			} else {
+				match = pd;
+			}
+		}
+		if (match != null) {
+			return match;
+		}
+		return new ProjectsDrop(p);
+	}
+
+	@Override
+	public String getEntityName() {
+		return "projects-drop";
+	}
+
+	@Override
+	public void snapshotAttrs(AbstractSeaXmlCreator s) {
+		super.snapshotAttrs(s);
+	}
+
+	public IIRProjects getIIRProjects() {
+		return projects;
+	}
+
+	public void setProjects(IIRProjects p) {
+		projects = p;
+	}
+
+	public static synchronized ProjectsDrop getDrop() {
+		if (active != null) {
+			if (active.isValid()) {
+				return active;
+			}
+			/*
+			 * If the drop is not valid null out our reference.
+			 */
+			active = null;
+		}
+		ProjectsDrop p = null;
+		for (ProjectsDrop pd : Sea.getDefault().getDropsOfExactType(
+				ProjectsDrop.class)) {
+			if (p == null) {
+				p = pd;
+			} else {
+				LOG.warning("Multiple projects analyzed for JSure: "
+						+ pd.getIIRProjects().getLabel());
+			}
+		}
+		active = p;
+		return p;
+	}
+
+	public static IIRProjects getProjects() {
+		ProjectsDrop pd = getDrop();
+		if (pd == null)
+			return null;
+		else
+			return pd.getIIRProjects();
+	}
 }
