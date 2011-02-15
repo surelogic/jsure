f558b1292d66bc96c7ac01bcf6643b093829e857
Tim Halloran <tim.halloran@surelogic.com>
2011-02-15 18:35:17 +0000

    Working on getting regression tests running
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-tests@16888 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/tests/RunARegressionTest.java
+++ src/com/surelogic/jsure/tests/RunARegressionTest.java
@@ -2,14 +2,13 @@ package com.surelogic.jsure.tests;
 
 import junit.framework.TestSuite;
 
-public class AllRegressionTests extends TestSuite {
-	public AllRegressionTests() {
-		System.out.println("CONSTRUCTING ALLREGRESSIONTESTS");
-		
+public class RunARegressionTest extends TestSuite {
+
+	public RunARegressionTest() {
 		addTest(new TestSuite(RegressionTest.class));
 	}
 
 	public static junit.framework.Test suite() {
-		return new AllRegressionTests();
+		return new RunARegressionTest();
 	}
 }
