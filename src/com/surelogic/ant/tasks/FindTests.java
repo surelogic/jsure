5c8c15041194e532e059a60c165d29a2f75b7ba1
Tim Halloran <tim.halloran@surelogic.com>
2011-02-15 19:55:53 +0000

    Working on getting regression tests running
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-tests-ant-tasks@16893 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/ant/tasks/FindTests.java
+++ src/com/surelogic/ant/tasks/FindTests.java
@@ -1,61 +1,70 @@
 package com.surelogic.ant.tasks;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
 
 /**
- * Returns a comma-separated list of regression tests contained at the top-most level of the given directory. For use with 
- * the ant-contrib for and foreach tasks (although it is not strictly required).  The tests
- * will either be directories or zip files.
+ * Returns a comma-separated list of directories or zip files contained at the
+ * top-most level of the given directory. For use with the ant-contrib for and
+ * foreach tasks (although it is not strictly required).
  * 
- * Usage:
- * <findtests basedir="directory to search" fullpaths="true/false" property="property to set"/>
+ * Usage: <findtests basedir="directory to search" fullpaths="true/false"
+ * property="property to set"/>
  * 
- * Required parameters:
- * basedir - The path to the directory to search for directories
- * fullpaths - Boolean indicating whether or not to return the absolute paths, or just the names of the located directories
- * property - The property to set with the comma-separated list
- *
+ * Required parameters: basedir - The path to the directory to search for
+ * directories fullpaths - Boolean indicating whether or not to return the
+ * absolute paths, or just the names of the located directories property - The
+ * property to set with the comma-separated list
+ * 
  * @author Ethan.Urie
  * @author Edwin.Chan
  */
 public class FindTests extends Task {
-	
+
 	private File basedir = null;
 	private boolean fullpaths = false;
 	private String property = null;
-	
-	public void execute()
-	{
-		if(basedir.isDirectory())
-		{
-			File[] dirList = basedir.listFiles(new RegressionTestFilter());
-			StringBuilder list = new StringBuilder();
-			String file = null;
-			
+
+	public void execute() {
+		if (basedir.isDirectory()) {
+			final File[] dirList = basedir
+					.listFiles(new RegressionTestFilter());
+			final List<String> resultList = new ArrayList<String>();
+
 			for (int i = 0; i < dirList.length; i++) {
-				if(fullpaths)
-				{
-					file =  dirList[i].getAbsolutePath();
+				final String path;
+				if (fullpaths) {
+					path = dirList[i].getAbsolutePath();
+				} else {
+					path = dirList[i].getName();
 				}
-				else
-				{
-					file =  dirList[i].getName();
-				}
-				list.append(file);
-				if(i != dirList.length - 1)
-				{
-					list.append(",");
-				}
+				resultList.add(path);
 			}
-			//System.out.println(property+" = "+list);
+
+			/*
+			 * Let's get things into alphabetical order
+			 */
+			Collections.sort(resultList);
+
+			StringBuilder list = new StringBuilder();
+			boolean first = true;
+			for (String path : resultList) {
+				if (first)
+					first = false;
+				else
+					list.append(',');
+
+				list.append(path);
+			}
 			getProject().setProperty(property, list.toString());
-		}
-		else
-		{
-			log(basedir.getAbsolutePath() + " is not a valid directory.", Project.MSG_ERR);
+		} else {
+			log(basedir.getAbsolutePath() + " is not a valid directory.",
+					Project.MSG_ERR);
 		}
 	}
 
@@ -82,5 +91,4 @@ public class FindTests extends Task {
 	public void setFullpaths(boolean fullpaths) {
 		this.fullpaths = fullpaths;
 	}
-
 }
--- src/com/surelogic/ant/tasks/RegressionTestFilter.java
+++ src/com/surelogic/ant/tasks/RegressionTestFilter.java
@@ -12,7 +12,6 @@ class RegressionTestFilter implements FileFilter {
 		} else {
 			rv = f.getName().endsWith(".zip");
 		}
-		//System.out.println(f.getName()+" => "+rv);
 		return rv;
 	}
 }
