72c42e9ced2a2ff35dbfe2a2579b7bb3ead48957
Tim Halloran <tim.halloran@surelogic.com>
2011-02-16 15:06:06 +0000

    Working on getting regression tests running
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-tests@16923 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/tests/EclipseLogHandler.java
+++ src/com/surelogic/jsure/tests/EclipseLogHandler.java
@@ -13,85 +13,77 @@ import com.surelogic.common.logging.SLLogger;
 
 /**
  * @author Edwin
- *
+ * 
  */
 public class EclipseLogHandler extends Handler {
-  public static final EclipseLogHandler prototype = new EclipseLogHandler();
-  private static FileHandler fileLog;
-  
-  /* (non-Javadoc)
-   * @see java.util.logging.Handler#close()
-   */
-  @Override public void close() throws SecurityException {    
-    // does nothing
-  }
+	public static final EclipseLogHandler prototype = new EclipseLogHandler();
+	private static FileHandler fileLog;
 
-  /* (non-Javadoc)
-   * @see java.util.logging.Handler#flush()
-   */
-  @Override public void flush() {
-    // does nothing
-  }
-  
-  /* (non-Javadoc)
-   * @see java.util.logging.Handler#publish(java.util.logging.LogRecord)
-   */
-  @Override public void publish(final LogRecord record) {
-    final Level level = record.getLevel();
-    
-    int severity = IStatus.INFO;
-    if (level == Level.WARNING) {
-      severity = IStatus.WARNING;
-      
-      // ignored
-      return;
-    } 
-    else if (level == Level.SEVERE) {
-      severity = IStatus.ERROR;     
-    } 
-    else {
-      // Ignored
-      return;
-    }
-    //ErrorLog.elog(severity, record.getMessage(), record.getThrown());
-  }
+	@Override
+	public void close() throws SecurityException {
+		// does nothing
+	}
 
-  public static synchronized void init() {
-    final Logger log = SLLogger.getLogger("");
-    for (Handler h : log.getHandlers()) {
-      if (prototype == h) {
-        // Already registered
-        return;
-      }
-    }
-    log.addHandler(prototype);
-  }
+	@Override
+	public void flush() {
+		// does nothing
+	}
 
-  public static synchronized String startFileLog(String name) {
-    if (fileLog == null) {
-      final XMLFormatter xf = new XMLFormatter();
-      try {
-        String file = name+".xml";
-        File f      = new File(file);
-        fileLog = new FileHandler(file);
-        fileLog.setFormatter(xf);
-        fileLog.setLevel(Level.WARNING);
-        SLLogger.getLogger("").addHandler(fileLog);
-        return f.getAbsolutePath();
-      } catch (SecurityException e) {
-        e.printStackTrace();
-      } catch (IOException e) {
-        e.printStackTrace();
-      } 
-    } else {
-      new Throwable("Already created a file log").printStackTrace();      
-    }
-    return null;
-  }
+	@Override
+	public void publish(final LogRecord record) {
+		final Level level = record.getLevel();
 
-  public static synchronized void stopFileLog() {
-	SLLogger.getLogger("").removeHandler(fileLog);
-    fileLog.close();
-    fileLog = null;
-  }
+		int severity = IStatus.INFO;
+		if (level == Level.WARNING) {
+			severity = IStatus.WARNING;
+
+			// ignored
+			return;
+		} else if (level == Level.SEVERE) {
+			severity = IStatus.ERROR;
+		} else {
+			// Ignored
+			return;
+		}
+		// ErrorLog.elog(severity, record.getMessage(), record.getThrown());
+	}
+
+	public static synchronized void init() {
+		final Logger log = SLLogger.getLogger("");
+		for (Handler h : log.getHandlers()) {
+			if (prototype == h) {
+				// Already registered
+				return;
+			}
+		}
+		log.addHandler(prototype);
+	}
+
+	public static synchronized String startFileLog(String name) {
+		if (fileLog == null) {
+			final XMLFormatter xf = new XMLFormatter();
+			try {
+				String file = name + ".xml";
+				File f = new File(file);
+				fileLog = new FileHandler(file);
+				fileLog.setFormatter(xf);
+				fileLog.setLevel(Level.WARNING);
+				SLLogger.getLogger("").addHandler(fileLog);
+				return f.getAbsolutePath();
+			} catch (SecurityException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			new Throwable("Already created a file log").printStackTrace();
+		}
+		return null;
+	}
+
+	public static synchronized void stopFileLog() {
+		SLLogger.getLogger("").removeHandler(fileLog);
+		fileLog.close();
+		fileLog = null;
+	}
 }
