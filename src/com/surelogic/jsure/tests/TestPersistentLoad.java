bc5e6757cf9cb8ddce23aa04d6542fa7747d25b1
Edwin Chan <edwin.chan@surelogic.com>
2011-02-18 15:49:56 +0000

    Updated to match reorganization
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-tests@16980 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/tests/TestPersistentLoad.java
+++ src/com/surelogic/jsure/tests/TestPersistentLoad.java
@@ -7,7 +7,7 @@ public class TestPersistentLoad extends TestCase {
 	public void testLoad() {
 		int failures = TestPersistent
 				.test("--load --zip --quiet --indirect index.arg "
-						+ "--testdir ../build/plugins/fluid/src/edu/cmu/cs/fluid/version --test combined.tst");
+						+ "--testdir junit-test-src/edu/cmu/cs/fluid/version --test combined.tst");
 		assertEquals(0, failures);
 	}
 }
