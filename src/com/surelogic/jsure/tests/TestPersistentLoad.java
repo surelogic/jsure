8260c289123eb1d3b710ff39ed48a2486fd0e224
Edwin Chan <edwin.chan@surelogic.com>
2011-02-18 16:12:26 +0000

    Added code to turn on versioning
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-tests@16981 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/tests/TestPersistentLoad.java
+++ src/com/surelogic/jsure/tests/TestPersistentLoad.java
@@ -1,9 +1,16 @@
 package com.surelogic.jsure.tests;
 
 import junit.framework.TestCase;
+import edu.cmu.cs.fluid.parse.JJNode;
 import edu.cmu.cs.fluid.version.TestPersistent;
 
 public class TestPersistentLoad extends TestCase {
+	@Override
+	protected void setUp() {
+		System.setProperty("fluid.ir.versioning", "true");
+		System.out.println("versioning = "+JJNode.versioningIsOn);
+	}
+	
 	public void testLoad() {
 		int failures = TestPersistent
 				.test("--load --zip --quiet --indirect index.arg "
