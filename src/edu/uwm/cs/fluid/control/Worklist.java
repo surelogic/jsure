1edf9dc2d54b3bc465a41e1eb3d18ffa3b79d91e
Edwin Chan <edwin.chan@surelogic.com>
2011-06-08 13:16:25 +0000

    Changed to use PriorityQueueWorklist
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/fluid@17714 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/edu/uwm/cs/fluid/control/Worklist.java
+++ src/edu/uwm/cs/fluid/control/Worklist.java
@@ -62,6 +62,9 @@ public interface Worklist extends Cloneable {
     public static Worklist makeWorklist(boolean isForward) {
       // TODO: use a quick property to change
       //return new QueueWorklist();
+      if (isForward) {
+    	  return new PriorityQueueWorklist();
+      }
       return new BufferWorklist();
     }
   }
