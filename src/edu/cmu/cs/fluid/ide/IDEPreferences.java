6bdf44b537f7f2ad6c30028b56e4937f15c70636
Tim Halloran <tim.halloran@surelogic.com>
2011-06-08 16:12:33 +0000

    Added timeout warning and flag for the verifying analyses to allow the user to better control the execution times of scans.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/fluid@17721 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/edu/cmu/cs/fluid/ide/IDEPreferences.java
+++ src/edu/cmu/cs/fluid/ide/IDEPreferences.java
@@ -8,7 +8,11 @@ public interface IDEPreferences {
 	String JSURE_DATA_DIRECTORY = PREFIX + "data.directory";
 	String DEFAULT_JRE = PREFIX + "default.JRE";
 	String JSURE_XML_DIRECTORY = PREFIX + "xml.directory";
-	
+
+	String TIMEOUT_WARNING_SEC = PREFIX + "timeoutWarningSec";
+	String TIMEOUT_FLAG = PREFIX + "timeoutFlag";
+	String TIMEOUT_SEC = PREFIX + "timeoutSec";
+
 	/**
 	 * The preference prefix for whether an analysis is on
 	 */
