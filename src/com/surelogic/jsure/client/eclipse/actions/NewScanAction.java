dd63fa930ff49d2707b5218a49b0ecf77252a274
Edwin Chan <edwin.chan@surelogic.com>
2011-06-16 15:07:34 +0000

    Changed to try to wait for any Java builds to finish before checking to see if the code compiles
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@17803 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/client/eclipse/actions/NewScanAction.java
+++ src/com/surelogic/jsure/client/eclipse/actions/NewScanAction.java
@@ -10,13 +10,13 @@ import com.surelogic.common.ui.EclipseUIUtility;
 import com.surelogic.common.ui.SLImages;
 import com.surelogic.common.ui.actions.AbstractProjectSelectedMenuAction;
 import com.surelogic.common.ui.dialogs.JavaProjectSelectionDialog;
-import com.surelogic.common.jobs.NullSLProgressMonitor;
-import com.surelogic.common.jobs.SLStatus;
+import com.surelogic.common.core.jobs.EclipseJob;
+import com.surelogic.common.jobs.*;
 import com.surelogic.common.license.SLLicenseProduct;
 import com.surelogic.common.license.SLLicenseUtility;
 import com.surelogic.common.logging.SLLogger;
 import com.surelogic.jsure.client.eclipse.perspectives.CodeVerificationPerspective;
-import com.surelogic.jsure.core.driver.JavacBuild;
+import com.surelogic.jsure.core.driver.*;
 import com.surelogic.jsure.core.preferences.JSurePreferencesUtility;
 
 public class NewScanAction extends AbstractProjectSelectedMenuAction {
@@ -33,14 +33,22 @@ public class NewScanAction extends AbstractProjectSelectedMenuAction {
 					failed.getMessage(), failed.getException());
 			return;
 		}
-
-		boolean success = JavacBuild.analyze(selectedProjects,
-				BalloonUtility.errorListener);
-		if (success) {
-			// TODO check if we should auto-switch
-			EclipseUIUtility.showPerspective(CodeVerificationPerspective.class
-					.getName());
-		}
+		final SLJob job = new AbstractSLJob("Checking for builds in progress") {
+			@Override
+			public SLStatus run(SLProgressMonitor monitor) {
+				JavacDriver.waitForBuild();
+				
+				boolean success = JavacBuild.analyze(selectedProjects,
+						BalloonUtility.errorListener);
+				if (success) {
+					// TODO check if we should auto-switch
+					EclipseUIUtility.showPerspective(CodeVerificationPerspective.class
+							.getName());
+				}
+				return SLStatus.OK_STATUS;
+			}			
+		};
+		EclipseJob.getInstance().schedule(job);
 	}
 
 	@Override
