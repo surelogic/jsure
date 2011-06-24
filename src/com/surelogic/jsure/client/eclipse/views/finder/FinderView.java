38ecc9ae2ce2bf86147d4d68cb8e18aaae8b350c
Tim Halloran <tim.halloran@surelogic.com>
2011-06-24 20:33:28 +0000

    Working on a new view
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@17899 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/jsure/client/eclipse/views/finder/FinderView.java
@@ -0,0 +1,20 @@
+package com.surelogic.jsure.client.eclipse.views.finder;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public final class FinderView extends ViewPart {
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
