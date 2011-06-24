bf858fbac3700dbc39ae7e69a0384b9d660cae6a
John Boyland <boyland@cs.uwm.edu>
2011-06-24 12:50:29 +0000

    Added lattice
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/fluid@17894 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/analysis/uniqueness/store/State.java
+++ src/com/surelogic/analysis/uniqueness/store/State.java
@@ -1,6 +1,9 @@
 package com.surelogic.analysis.uniqueness.store;
 
+import edu.uwm.cs.fluid.util.AbstractLattice;
+
 /**
+ * Indication of states of variables in a store lattice.
  * Replaces the STATE_* constants as well as the PseudoVariable class.  We can
  * use the enumeration elements SHARED, BORROWED, and UNDEFINED directly instead
  * of creating PseudoVariable wrappers.
@@ -11,4 +14,35 @@ public enum State {
   SHARED,
   BORROWED,
   UNDEFINED;
+  
+  public static class Lattice extends AbstractLattice<State> {
+	@Override
+	public boolean lessEq(State v1, State v2) {
+		return v1.ordinal() <= v2.ordinal();
+	}
+
+	@Override
+	public State top() {
+		return UNDEFINED;
+	}
+
+	@Override
+	public State bottom() {
+		return NULL;
+	}
+
+	@Override
+	public State join(State v1, State v2) {
+		if (lessEq(v1,v2)) return v2;
+		return v1;
+	}
+
+	@Override
+	public State meet(State v1, State v2) {
+		if (lessEq(v1,v2)) return v1;
+		return v2;
+	}
+  }
+  
+  public static final Lattice lattice = new Lattice();
 }
