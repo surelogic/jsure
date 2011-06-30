5ac2b396612bd6cc187ddff5a0b1938c9b715dda
Tim Halloran <tim.halloran@surelogic.com>
2011-06-30 15:48:08 +0000

    Cleaned up extra interface from cascading list control.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@17924 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/client/eclipse/views/finder/FinderView.java
+++ src/com/surelogic/jsure/client/eclipse/views/finder/FinderView.java
@@ -1,14 +1,34 @@
 package com.surelogic.jsure.client.eclipse.views.finder;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
+
+import com.surelogic.common.CommonImages;
+import com.surelogic.common.ui.CascadingList;
+import com.surelogic.common.ui.EclipseUIUtility;
+import com.surelogic.common.ui.RadioArrowMenu;
+import com.surelogic.common.ui.SLImages;
 
 public final class FinderView extends ViewPart {
 
 	@Override
 	public void createPartControl(Composite parent) {
-		// TODO Auto-generated method stub
 
+		final CascadingList finder = new CascadingList(parent, SWT.None);
+		for (int i = 0; i < 10; i++) {
+
+			RadioArrowMenu menu = new RadioArrowMenu(finder);
+			menu.addChoice("Hello 1",
+					SLImages.getImage(CommonImages.IMG_ANNOTATION));
+			menu.addChoice("Hello 2",
+					SLImages.getImage(CommonImages.IMG_ANNOTATION));
+			menu.addChoice("Hello 3",
+					SLImages.getImage(CommonImages.IMG_ANNOTATION));
+			menu.addChoice("Hello 4",
+					SLImages.getImage(CommonImages.IMG_ANNOTATION));
+		}
+		finder.pack();
 	}
 
 	@Override
