96a12990fa0c28a045153ad826eeced32685e9d6
Tim Halloran <tim.halloran@surelogic.com>
2011-08-03 19:31:36 +0000

    Removed FAST viewer -- probably should be moved to Debug views project when we get time.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@18158 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/client/eclipse/views/ast/IFastViewerPage.java
+++
@@ -1,7 +0,0 @@
-package com.surelogic.jsure.client.eclipse.views.ast;
-
-import org.eclipse.ui.views.contentoutline.IContentOutlinePage;
-
-public interface IFastViewerPage extends IContentOutlinePage {
-
-}
