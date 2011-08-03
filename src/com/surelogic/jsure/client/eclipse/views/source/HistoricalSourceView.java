87659dde630521e91d305d76ec6e37301db1f841
Tim Halloran <tim.halloran@surelogic.com>
2011-08-03 18:11:17 +0000

    Displayed information about the source code being displayed in the historical source view.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@18146 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/client/eclipse/views/source/HistoricalSourceView.java
+++ src/com/surelogic/jsure/client/eclipse/views/source/HistoricalSourceView.java
@@ -6,15 +6,60 @@ import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.Collections;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.progress.UIJob;
+
 import com.surelogic.common.ISourceZipFileHandles;
+import com.surelogic.common.ui.jobs.SLUIJob;
 import com.surelogic.common.ui.views.AbstractHistoricalSourceView;
 import com.surelogic.javac.Config;
 import com.surelogic.javac.JavaSourceFile;
 import com.surelogic.javac.Projects;
+import com.surelogic.javac.persistence.JSureScan;
 import com.surelogic.jsure.core.scans.JSureDataDirHub;
 import com.surelogic.jsure.core.scans.JSureScanInfo;
 
-public class HistoricalSourceView extends AbstractHistoricalSourceView {
+public class HistoricalSourceView extends AbstractHistoricalSourceView
+		implements JSureDataDirHub.CurrentScanChangeListener {
+
+	@Override
+	public void createPartControl(Composite parent) {
+		super.createPartControl(parent);
+
+		final JSureScan scan = JSureDataDirHub.getInstance().getCurrentScan();
+		if (scan != null) {
+			setSourceSnapshotTime(scan.getTimeOfScan());
+		}
+		JSureDataDirHub.getInstance().addCurrentScanChangeListener(this);
+	}
+
+	@Override
+	public void dispose() {
+		try {
+			JSureDataDirHub.getInstance().removeCurrentScanChangeListener(this);
+		} finally {
+			super.dispose();
+		}
+	}
+
+	@Override
+	public void currentScanChanged(final JSureScan scan) {
+		final UIJob job = new SLUIJob() {
+			@Override
+			public IStatus runInUIThread(IProgressMonitor monitor) {
+				clearSourceCodeFromView();
+				if (scan != null) {
+					setSourceSnapshotTime(scan.getTimeOfScan());
+				}
+				return Status.OK_STATUS;
+			}
+		};
+		job.setSystem(true);
+		job.schedule();
+	}
 
 	@Override
 	protected ISourceZipFileHandles findSources(String run) {
@@ -46,8 +91,8 @@ public class HistoricalSourceView extends AbstractHistoricalSourceView {
 
 	public static void tryToOpenInEditor(final String pkg, final String type,
 			final String field) {
-		tryToOpenInEditorUsingFieldName(HistoricalSourceView.class, null,
-				pkg, type, field);
+		tryToOpenInEditorUsingFieldName(HistoricalSourceView.class, null, pkg,
+				type, field);
 	}
 
 	public static String tryToMapPath(String path) {
