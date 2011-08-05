7b34a27fe5f6cf6634c72f0b1e5361facb1d2f9f
Tim Halloran <tim.halloran@surelogic.com>
2011-08-05 16:17:59 +0000

    Trying to get information out of the scan.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-debug-views@18171 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/views/debug/oracleDiff/SnapshotDiffContentProvider.java
+++ src/com/surelogic/jsure/views/debug/oracleDiff/SnapshotDiffContentProvider.java
@@ -29,42 +29,43 @@ public class SnapshotDiffContentProvider implements IJSureTreeContentProvider {
 	static {
 		nothingToDiff[0] = new Category(null, "Nothing to diff");
 		nothingToShow[0] = new Category(null, "No differences");
-	}	
+	}
 	private Diff diff;
-	
+
 	@Override
 	public String build() {
-		final JSureScanInfo scan = JSureDataDirHub.getInstance().getCurrentScanInfo();
+		final JSureScanInfo scan = JSureDataDirHub.getInstance()
+				.getCurrentScanInfo();
 		if (scan == null) {
 			diff = null;
 			return null;
 		}
-		final Collection<? extends IDropInfo> info = scan.getRawInfo();
+		final Collection<IDropInfo> info = scan.getDropInfo();
 		if (!info.isEmpty()) {
 			try {
 				File file = null;
-				for(String name : scan.findProjectsLabel().split(", ")) {			
+				for (String name : scan.findProjectsLabel().split(", ")) {
 					final IProject p = EclipseUtility.getProject(name);
 					if (p == null || !p.exists()) {
 						continue;
 					}
 					final File pFile = p.getLocation().toFile();
-					file  = SeaSummary.findSummary(pFile.getAbsolutePath());	
+					file = SeaSummary.findSummary(pFile.getAbsolutePath());
 				}
-				diff = SeaSummary.diff(info, file);				
+				diff = SeaSummary.diff(info, file);
 				return scan.getLabel();
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 		} else {
-				System.out.println("No snapshot to diff against");
-				diff = null;
+			System.out.println("No snapshot to diff against");
+			diff = null;
 		}
 		return null;
 	}
-	
+
 	public Object[] getElements(Object input) {
 		if (diff != null) {
 			Object[] rv = diff.getCategories();
@@ -102,43 +103,41 @@ public class SnapshotDiffContentProvider implements IJSureTreeContentProvider {
 			if (c.file == null) {
 				return c.name;
 			}
-			return c.name+"  in  "+c.file;
-		}
-		else if (element instanceof Entity) {
+			return c.name + "  in  " + c.file;
+		} else if (element instanceof Entity) {
 			Entity e = (Entity) element;
 			StringBuilder sb = new StringBuilder();
 			if (e.isNewer()) {
 				sb.append("New: ");
-			}
-			else if (e.isOld()) {
+			} else if (e.isOld()) {
 				sb.append("Old: ");
 			}
 			sb.append(e.getAttribute(MESSAGE_ATTR));
 			return sb.toString();
 		}
 		return null;
-	}	
-	
+	}
+
 	public Image getImage(Object element) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-		// TODO Auto-generated method stub		
+		// TODO Auto-generated method stub
 	}
 
 	public boolean isLabelProperty(Object element, String property) {
-		// TODO Auto-generated method stub	
+		// TODO Auto-generated method stub
 		return false;
 	}
-	
+
 	public void addListener(ILabelProviderListener listener) {
-		// TODO Auto-generated method stub		
+		// TODO Auto-generated method stub
 	}
 
 	public void removeListener(ILabelProviderListener listener) {
-		// TODO Auto-generated method stub		
+		// TODO Auto-generated method stub
 	}
 
 	public void dispose() {
