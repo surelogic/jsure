3dc0b80405a2a7afbca736e51b9dd9e489a95560
Tim Halloran <tim.halloran@surelogic.com>
2011-08-05 16:49:41 +0000

    Trying to get information out of the scan.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@18172 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/client/eclipse/views/finder/FinderMediator.java
+++ src/com/surelogic/jsure/client/eclipse/views/finder/FinderMediator.java
@@ -1,7 +1,6 @@
 package com.surelogic.jsure.client.eclipse.views.finder;
 
 import java.util.List;
-import java.util.Set;
 
 import com.surelogic.common.ILifecycle;
 import com.surelogic.common.ui.CascadingList;
@@ -9,11 +8,8 @@ import com.surelogic.javac.persistence.JSureScan;
 import com.surelogic.jsure.core.scans.JSureDataDirHub;
 import com.surelogic.jsure.core.scans.JSureScanInfo;
 
-import edu.cmu.cs.fluid.sea.Drop;
-import edu.cmu.cs.fluid.sea.IDropInfo;
-import edu.cmu.cs.fluid.sea.PromiseDrop;
-import edu.cmu.cs.fluid.sea.ResultDrop;
-import edu.cmu.cs.fluid.sea.Sea;
+import edu.cmu.cs.fluid.java.ISrcRef;
+import edu.cmu.cs.fluid.sea.IProofDropInfo;
 
 public final class FinderMediator implements ILifecycle,
 		JSureDataDirHub.CurrentScanChangeListener {
@@ -40,18 +36,28 @@ public final class FinderMediator implements ILifecycle,
 		JSureScanInfo scanInfo = JSureDataDirHub.getInstance()
 				.getCurrentScanInfo();
 
-		Set<IDropInfo> promiseDrops = scanInfo
-				.getDropsOfType(PromiseDrop.class);
+		List<IProofDropInfo> proofDrops = scanInfo.getProofDropInfo();
 
-		System.out.println("- PROMISES - - - - - - - - - - - - - - - - -- ");
-		for (IDropInfo d : promiseDrops) {
-			System.out.println(" PromiseDrop: " + d.getMessage());
-		}
+		System.out.println("- - - - - - - - - - - - - - - - - -- ");
+		for (IProofDropInfo d : proofDrops) {
+			System.out.println("DROP:");
+			System.out.println(" getEntityName()=" + d.getEntityName());
+			System.out.println(" getMessage()=: " + d.getMessage());
+			System.out.println(" getType()=" + d.getType());
+			ISrcRef sr = d.getSrcRef();
+			if (sr == null) {
+				System.out.println("  NO SOURCE REFERENCE");
+			} else {
+				System.out.println("  sr.getProject()=" + sr.getProject());
+				System.out.println("  sr.getPackage()=" + sr.getPackage());
+				System.out.println("  sr.getCUName()=" + sr.getCUName());
+			}
+			System.out
+					.println(" getprovedConsistent()=" + d.provedConsistent());
+			System.out.println(" proofUsesRedDot()=" + d.proofUsesRedDot());
+			System.out.println(" isTimeout()=" + d.isTimeout());
 
-		Set<IDropInfo> resultDrops = scanInfo.getDropsOfType(ResultDrop.class);
-		System.out.println("- RESULTS  - - - - - - - - - - - - - - - - -- ");
-		for (IDropInfo d : resultDrops) {
-			System.out.println(" ResultDrop: " + d.getMessage());
 		}
+		System.out.println("- - - - - - - - - - - - - - - - - -- ");
 	}
 }
