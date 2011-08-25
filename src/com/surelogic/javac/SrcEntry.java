97e545672d6154c081601df7b4dc902ac292eb4a
Edwin Chan <edwin.chan@surelogic.com>
2011-08-25 18:24:32 +0000

    Added SrcEntry objects
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/fluid@18479 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/javac/SrcEntry.java
@@ -0,0 +1,48 @@
+package com.surelogic.javac;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.surelogic.common.xml.Entities;
+import com.surelogic.javac.persistence.JSureProjectsXMLCreator;
+import com.surelogic.javac.persistence.PersistenceConstants;
+
+/**
+ * Used to record the source path
+ * 
+ * @author Edwin
+ */
+public class SrcEntry extends AbstractClassPathEntry {
+	private final Config project;
+	private final String projectRelativePath;
+	
+	public SrcEntry(Config c, String path) {
+		super(true); // TODO is this right?
+		project = c;
+		projectRelativePath = path;
+	}
+
+	public String getProjectRelativePath() {
+		return projectRelativePath;
+	}
+	
+	@Override
+	public void init(JavacProject jp, JavacClassParser loader)
+			throws IOException {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void outputToXML(JSureProjectsXMLCreator creator, int indent,
+			StringBuilder b) {
+		Entities.start(PersistenceConstants.SRC, b, indent);
+		creator.addAttribute(PersistenceConstants.PATH, projectRelativePath);
+		creator.addAttribute(PersistenceConstants.IS_EXPORTED, isExported());
+		Entities.closeStart(b, true);
+	}
+
+	@Override
+	public void relocateJars(File targetDir) throws IOException {
+		// TODO Auto-generated method stub
+	}
+}
