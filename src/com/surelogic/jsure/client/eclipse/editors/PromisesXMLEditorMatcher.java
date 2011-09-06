72a95c8ed5754bc193a2e35675555b39ca74b172
Edwin Chan <edwin.chan@surelogic.com>
2011-09-06 18:28:50 +0000

    Fixed to properly allow multiple editors
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@18612 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/client/eclipse/editors/PromisesXMLEditorMatcher.java
+++ src/com/surelogic/jsure/client/eclipse/editors/PromisesXMLEditorMatcher.java
@@ -3,14 +3,24 @@ package com.surelogic.jsure.client.eclipse.editors;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorMatchingStrategy;
 import org.eclipse.ui.IEditorReference;
+import org.eclipse.ui.PartInitException;
 
 import com.surelogic.xml.TestXMLParserConstants;
 
 public class PromisesXMLEditorMatcher implements IEditorMatchingStrategy {
 	@Override
-	public boolean matches(IEditorReference editorRef, IEditorInput input) {
-		String name = input.getName();
-		return name.endsWith(TestXMLParserConstants.SUFFIX);
+	public boolean matches(final IEditorReference editorRef, final IEditorInput input) {				
+		final String name = input.getName();
+		if (!name.endsWith(TestXMLParserConstants.SUFFIX)) {
+			return false;
+		}		
+		try {
+			IEditorInput eInput = editorRef.getEditorInput();
+			return name.equals(eInput.getName());
+		} catch (PartInitException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 }
