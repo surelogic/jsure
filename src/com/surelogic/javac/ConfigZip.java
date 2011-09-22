60d3812e3da38bed68cdf5c5858a32c04e37dc04
Edwin Chan <edwin.chan@surelogic.com>
2011-09-22 20:24:57 +0000

    Initial code to zip up JavaSourceFiles from a Config
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/fluid@18689 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/javac/ConfigZip.java
@@ -0,0 +1,90 @@
+package com.surelogic.javac;
+
+import java.io.*;
+import java.util.zip.ZipOutputStream;
+
+import com.surelogic.common.*;
+
+/**
+ * A hack to zip up all the files for the Config
+ * 
+ * @author Edwin
+ */
+public class ConfigZip extends AbstractJavaZip<JavaSourceFile> {
+	private final Config config;
+	private final JavaSourceFile root = new JavaSourceFile(null, null, null, false);
+	
+	public ConfigZip(Config c) {
+		config = c;
+	}
+	
+	@Override
+	protected void addAnnotatedResourcesToZip(final ZipOutputStream out, 
+			TempInfo info, final JavaSourceFile ignored) {
+		for(JavaSourceFile f : config.getFiles()) {
+			addAnnotatedFileToZip(out, info, f);
+		}
+	}
+
+	@Override
+	protected InputStream getFileContents(JavaSourceFile res)
+			throws IOException {
+		return new FileInputStream(res.file);
+	}
+
+	@Override
+	protected String getFullPath(JavaSourceFile res) throws IOException {
+		return res.relativePath;
+	}
+
+	@Override
+	protected String[] getIncludedTypes(JavaSourceFile res) {
+		return new String[] { res.qname };
+	}
+
+	@Override
+	protected String getJavaPackageNameOrNull(JavaSourceFile res) {
+		final int lastDot = res.qname.lastIndexOf('.');
+		return lastDot < 0 ? "" : res.qname.substring(0, lastDot);
+	}
+
+	@Override
+	protected String getName(JavaSourceFile res) {
+		final int lastSep = res.relativePath.lastIndexOf(File.pathSeparatorChar);
+		return lastSep < 0 ? res.qname : res.qname.substring(lastSep+1);
+	}
+
+	@Override
+	protected JavaSourceFile getRoot() {
+		return root;
+	}
+
+	@Override
+	protected long getTimestamp(JavaSourceFile res) {
+		return res.file.lastModified();
+	}
+
+	@Override
+	protected boolean isAccessible(JavaSourceFile res) {
+		return true;
+	}
+
+	@Override
+	protected boolean isFile(JavaSourceFile res) {
+		return res != null;
+	}
+	
+	@Override
+	protected JavaSourceFile getFile(JavaSourceFile res, String name) {
+		if (res == getRoot()) {
+			// TODO tools properties
+		}
+		return null;
+	}
+	
+	@Override
+	protected JavaSourceFile[] getMembers(JavaSourceFile res)
+			throws IOException {
+		throw new UnsupportedOperationException();
+	}
+}
