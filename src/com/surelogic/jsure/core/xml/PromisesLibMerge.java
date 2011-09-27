6b7cdb62e147488dca2684d372bedd3b5202aef5
Edwin Chan <edwin.chan@surelogic.com>
2011-09-27 19:45:02 +0000

    Initial code to merge libraries
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-core-eclipse@18724 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/jsure/core/xml/PromisesLibMerge.java
@@ -0,0 +1,75 @@
+package com.surelogic.jsure.core.xml;
+
+import java.io.*;
+import java.net.URISyntaxException;
+
+import com.surelogic.common.FileUtility;
+import com.surelogic.jsure.core.driver.DriverConstants;
+import com.surelogic.jsure.core.driver.JavacEclipse;
+import com.surelogic.jsure.core.preferences.JSurePreferencesUtility;
+import com.surelogic.xml.*;
+
+/**
+ * Code to help merge promises XML between fluid and clients' data directories
+ * 
+ * @author Edwin
+ */
+public final class PromisesLibMerge {
+	public static void merge(boolean toClient) {
+		File fluidDir = null;
+		try {
+			fluidDir = new File(JavacEclipse.getDefault().getResourceRoot().toURI());
+		} catch (URISyntaxException e) {
+			throw new IllegalStateException("Bad URL: "+JavacEclipse.getDefault().getResourceRoot());
+		}
+		final File fLibDir = new File(fluidDir, TestXMLParserConstants.PROMISES_XML_PATH);
+		if (!fLibDir.exists()) {
+			throw new IllegalStateException("Couldn't find "+fLibDir);
+		}
+		final File libDir = new File(JSurePreferencesUtility.getJSureDataDirectory(), DriverConstants.XML_PATH_SEGMENT);
+		if (!libDir.exists()) {			
+			return; // Nothing else to do
+		}		
+		if (toClient) {
+			merge(toClient, libDir, fLibDir);
+		} else {
+			merge(toClient, fLibDir, libDir);
+		}
+	}
+		
+	/**
+	 * @param onlyMerge also copy if false
+	 */
+	private static void merge(final boolean onlyMerge, File to, File from) {
+		if (from.isFile()) {			
+			if (!to.exists()) {
+				// Check if I should copy 
+				if (onlyMerge) {
+					return; // No need to do anything
+				}
+				// Copy 
+				to.getParentFile().mkdirs();
+				FileUtility.copy(from, to);
+			} else {
+				try {					
+					// Merge
+					PackageElement target = PromisesXMLReader.load(to);
+					PackageElement source = PromisesXMLReader.load(from);
+					PromisesXMLMerge.merge(target, source);
+					
+					PromisesXMLWriter w = new PromisesXMLWriter(to);
+					w.write(target);
+				} catch (Exception e) {
+					System.err.println("While merging "+from+" into "+to);
+					e.printStackTrace();
+				}
+			}
+		} 
+		else if (from.isDirectory()) {
+			for(File f : from.listFiles(TestXMLParserConstants.XML_FILTER)) {
+				merge(onlyMerge, new File(to, f.getName()), f);
+			}
+		}
+		// 'from' doesn't exist, so nothing to do
+	}
+}
