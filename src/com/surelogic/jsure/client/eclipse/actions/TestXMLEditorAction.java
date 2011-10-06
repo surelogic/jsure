fb03df6eb8d3d1b7d4daf08b8b40f5af6c4de3ab
Edwin Chan <edwin.chan@surelogic.com>
2011-10-06 19:39:29 +0000

    Added to test PromisesXMLEditor
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@18780 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/jsure/client/eclipse/actions/TestXMLEditorAction.java
@@ -0,0 +1,22 @@
+package com.surelogic.jsure.client.eclipse.actions;
+
+import java.io.File;
+
+import org.eclipse.jface.action.IAction;
+
+import com.surelogic.common.ui.EclipseUIUtility;
+import com.surelogic.common.ui.actions.AbstractMainAction;
+import com.surelogic.jsure.core.driver.JavacEclipse;
+
+import edu.cmu.cs.fluid.ide.IDEPreferences;
+
+public class TestXMLEditorAction extends AbstractMainAction {
+	@Override
+	public void run(IAction action) {
+		final String root = JavacEclipse.getDefault().getStringPreference(IDEPreferences.JSURE_XML_DIRECTORY);
+		final char slash = File.separatorChar;
+		// In jsure demo
+		String path = root+slash+"java"+slash+"awt"+slash+"RenderingHints.promises.xml";
+		EclipseUIUtility.openInEditor(path);
+	}
+}
