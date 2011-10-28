166a38e0fa6a2ce53243bea772a1e1622cbc1927
Edwin Chan <edwin.chan@surelogic.com>
2011-10-28 19:56:17 +0000

    Updated to match changes to the promises XML editor
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@18906 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/client/eclipse/actions/TestXMLEditorAction.java
+++ src/com/surelogic/jsure/client/eclipse/actions/TestXMLEditorAction.java
@@ -1,22 +1,16 @@
 package com.surelogic.jsure.client.eclipse.actions;
 
-import java.io.File;
-
 import org.eclipse.jface.action.IAction;
 
 import com.surelogic.common.ui.EclipseUIUtility;
 import com.surelogic.common.ui.actions.AbstractMainAction;
-import com.surelogic.jsure.core.driver.JavacEclipse;
-
-import edu.cmu.cs.fluid.ide.IDEPreferences;
+import com.surelogic.jsure.client.eclipse.editors.PromisesXMLEditor;
 
 public class TestXMLEditorAction extends AbstractMainAction {
 	@Override
 	public void run(IAction action) {
-		final String root = JavacEclipse.getDefault().getStringPreference(IDEPreferences.JSURE_XML_DIRECTORY);
-		final char slash = File.separatorChar;
 		// In jsure demo
-		String path = root+slash+"test"+slash+"XMLTest.promises.xml";
-		EclipseUIUtility.openInEditor(path);
+		String path = "test"+'/'+"XMLTest.promises.xml";
+		EclipseUIUtility.openInEditor(PromisesXMLEditor.makeInput(path, false), PromisesXMLEditor.class.getName());
 	}
 }
