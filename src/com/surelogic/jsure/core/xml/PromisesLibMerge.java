7e5ea114214dc6957f5bdf24db2c4767703bcd2f
Edwin Chan <edwin.chan@surelogic.com>
2011-10-28 15:10:38 +0000

    Removed unused import
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-core-eclipse@18893 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/core/xml/PromisesLibMerge.java
+++ src/com/surelogic/jsure/core/xml/PromisesLibMerge.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.net.URISyntaxException;
 
 import com.surelogic.common.FileUtility;
-import com.surelogic.jsure.core.driver.DriverConstants;
 import com.surelogic.jsure.core.driver.JavacEclipse;
 import com.surelogic.jsure.core.preferences.JSurePreferencesUtility;
 import com.surelogic.xml.*;
