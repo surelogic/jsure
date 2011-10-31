ec97db842a88aea9621c4fd8ad72904c67f265f7
Edwin Chan <edwin.chan@surelogic.com>
2011-10-31 19:28:48 +0000

    Added code to show class details in the explorer view
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-client-eclipse@18921 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/client/eclipse/views/xml/XMLExplorerView.java
+++ src/com/surelogic/jsure/client/eclipse/views/xml/XMLExplorerView.java
@@ -1,5 +1,8 @@
 package com.surelogic.jsure.client.eclipse.views.xml;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.*;
 import java.util.Map.Entry;
 
@@ -17,9 +20,13 @@ import org.eclipse.swt.widgets.Control;
 
 import com.surelogic.common.CommonImages;
 import com.surelogic.common.ui.SLImages;
-import com.surelogic.jsure.client.eclipse.editors.PromisesXMLEditor;
+import com.surelogic.jsure.client.eclipse.editors.*;
 import com.surelogic.jsure.client.eclipse.views.*;
+import com.surelogic.xml.IJavaElement;
+import com.surelogic.xml.PackageElement;
 import com.surelogic.xml.TestXMLParserConstants;
+
+import edu.cmu.cs.fluid.util.Pair;
 
 public class XMLExplorerView extends AbstractJSureView {	
 	final IJSureTreeContentProvider f_content = new Provider();
@@ -61,7 +68,7 @@ public class XMLExplorerView extends AbstractJSureView {
 		final Object o = selection.getFirstElement();
 		if (o instanceof Type) {
 			Type t = (Type) o;
-			PromisesXMLEditor.openInEditor(t.pkg.name.replace('.', '/')+'/'+t.name+TestXMLParserConstants.SUFFIX, false);
+			PromisesXMLEditor.openInEditor(t.getPath(), false);
 		}
 		else if (o instanceof Package) {
 			Package p = (Package) o;
@@ -78,10 +85,13 @@ public class XMLExplorerView extends AbstractJSureView {
 	
 	private static final Package[] noPackages = new Package[0];
 	
-	class Provider implements IJSureTreeContentProvider {
+	class Provider extends PromisesXMLContentProvider implements IJSureTreeContentProvider {
 		Package[] pkgs = noPackages;
 		
-		@Override
+		Provider() {
+			super(true);
+		}
+		
 		public String build() {
 			final List<Package> l = new ArrayList<Package>();
 			for(Map.Entry<String,Collection<String>> e : PromisesXMLEditor.findAllPromisesXML().entrySet()) {
@@ -103,7 +113,11 @@ public class XMLExplorerView extends AbstractJSureView {
 				Package p = (Package) element;
 				return p.types.length != 0;
 			}
-			return false;
+			if (element instanceof Type) {
+				Type t = (Type) element;
+				return t.hasChildren();
+			}
+			return super.hasChildren(element);
 		}
 		
 		@Override
@@ -112,7 +126,15 @@ public class XMLExplorerView extends AbstractJSureView {
 				Package p = (Package) parent;
 				return p.types;
 			}
-			return noStrings;
+			if (parent instanceof Type) {
+				Type t = (Type) parent;
+				t.buildChildren();
+				if (t.root != null) {
+					return super.getChildren(t.root.getClassElement());
+				}
+			}
+			//return noStrings;
+			return super.getChildren(parent);
 		}
 
 		@Override
@@ -121,11 +143,17 @@ public class XMLExplorerView extends AbstractJSureView {
 				Type t = (Type) element;
 				return t.pkg;
 			}
-			return null;
+			if (element instanceof Package) {
+				return null;
+			}
+			return super.getParent(element);
 		}
 
 		@Override
 		public String getText(Object element) {
+			if (element instanceof IJavaElement) {
+				return super.getText(element);
+			}
 			if (element != null) {
 				return element.toString();
 			}
@@ -140,9 +168,9 @@ public class XMLExplorerView extends AbstractJSureView {
 			if (element instanceof Type) {
 				return SLImages.getImage(CommonImages.IMG_CLASS);
 			}
-			return null;
+			return super.getImage(element);
 		}
-
+		
 		@Override
 		public void dispose() {
 			// TODO Auto-generated method stub
@@ -166,7 +194,7 @@ public class XMLExplorerView extends AbstractJSureView {
 		@Override
 		public void removeListener(ILabelProviderListener listener) {
 			// TODO Auto-generated method stub
-		}
+		} 
 	}
 	
 	static class Package implements Comparable<Package> {
@@ -198,12 +226,21 @@ public class XMLExplorerView extends AbstractJSureView {
 	static class Type implements Comparable<Type> {
 		final Package pkg;
 		final String name;
+		PackageElement root;
 		
 		Type(Package pkg, String name) {
 			this.pkg = pkg;
 			this.name = name;
 		}
-		 
+
+		String getPath() {
+			return pkg.name.replace('.', '/')+'/'+name+TestXMLParserConstants.SUFFIX;
+		}
+
+		boolean hasChildren() {
+			return true;
+		}
+
 		@Override
 		public String toString() {
 			return name;
@@ -213,5 +250,22 @@ public class XMLExplorerView extends AbstractJSureView {
 		public int compareTo(Type o) {
 			return name.compareTo(o.name);
 		}	
+		
+		void buildChildren() {
+			if (root != null) {
+				return;
+			}
+			final String path = getPath();		
+			final Pair<File,?> rv = PromisesXMLEditor.findPromisesXML(path);
+			if (rv != null) {
+				try {
+					InputStream in = new FileInputStream(rv.first());
+					root = PromisesXMLContentProvider.getXML(in);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
