ef4d1803464ec69cfc4b77d56cbae80f0f2a02db
Edwin Chan <edwin.chan@surelogic.com>
2011-11-04 21:00:28 +0000

    Factored out code to PromisesXMLMerge/Reader Changed to load both files if available and merge the two
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-core-eclipse@18965 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/core/xml/PromisesLibMerge.java
+++ src/com/surelogic/jsure/core/xml/PromisesLibMerge.java
@@ -3,7 +3,6 @@ package com.surelogic.jsure.core.xml;
 import java.io.*;
 import java.net.URISyntaxException;
 
-import com.surelogic.common.FileUtility;
 import com.surelogic.jsure.core.driver.JavacEclipse;
 import com.surelogic.jsure.core.preferences.JSurePreferencesUtility;
 import com.surelogic.xml.*;
@@ -46,9 +45,9 @@ public final class PromisesLibMerge {
 			return; // Nothing else to do
 		}		
 		if (toClient) {
-			merge(toClient, libPath, fLibPath);
+			PromisesXMLMerge.merge(toClient, libPath, fLibPath);
 		} else {
-			merge(toClient, fLibPath, libPath);
+			PromisesXMLMerge.merge(toClient, fLibPath, libPath);
 		}
 	}
 	
@@ -67,62 +66,12 @@ public final class PromisesLibMerge {
 			return false;
 		}
 		try {
-			PackageElement fluid = PromisesXMLReader.load(fLibPath);
-			PackageElement local = PromisesXMLReader.load(libPath);			
+			PackageElement fluid = PromisesXMLReader.loadRaw(fLibPath);
+			PackageElement local = PromisesXMLReader.loadRaw(libPath);			
 			return fluid.needsToUpdate(local);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
 		}
-	}
-	
-	/**
-	 * @param onlyMerge also copy (to fluid) if false
-	 */
-	private static void merge(final boolean onlyMerge, File to, File from) {
-		if (from.isFile()) {			
-			if (!to.exists()) {
-				// Check if I should copy 
-				if (onlyMerge) {
-					//System.out.println("Ignoring "+from);
-					return; // No need to do anything
-				}
-				// Copy 
-				System.out.println("Copying "+from+" into "+to);
-				to.getParentFile().mkdirs();
-				FileUtility.copy(from, to);
-			} else {
-				try {					
-					// Merge
-					System.out.println("Merging "+from+" into "+to);
-					PackageElement target = PromisesXMLReader.load(to);
-					PackageElement source = PromisesXMLReader.load(from);
-					PromisesXMLMerge.merge(onlyMerge, target, source);
-					
-					PromisesXMLWriter w = new PromisesXMLWriter(to);
-					w.write(target);
-					if (!onlyMerge) {
-						// Merging all changes to fluid, so they should both be the same afterward
-						// (or the local one should be deleted/empty)
-						// TODO what about conflicts?
-						if (PromisesXMLMerge.onlyKeepDiffs) {
-							from.delete();
-						} else {
-							w = new PromisesXMLWriter(from);
-							w.write(target);
-						}
-					}
-				} catch (Exception e) {
-					System.err.println("While merging "+from+" into "+to);
-					e.printStackTrace();
-				}
-			}
-		} 
-		else if (from.isDirectory()) {
-			for(File f : from.listFiles(TestXMLParserConstants.XML_FILTER)) {
-				merge(onlyMerge, new File(to, f.getName()), f);
-			}
-		}
-		// 'from' doesn't exist, so nothing to do
 	}
 }
