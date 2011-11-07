47bb17a8d2553a81e0e60532120b3cc678d13295
Edwin Chan <edwin.chan@surelogic.com>
2011-11-07 20:36:17 +0000

    Moved LOCAL_XML_PATH to TestXMLParserConstants
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/jsure-core-eclipse@18969 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/jsure/core/driver/DriverConstants.java
+++ src/com/surelogic/jsure/core/driver/DriverConstants.java
@@ -1,6 +1,8 @@
 package com.surelogic.jsure.core.driver;
 
+import com.surelogic.xml.TestXMLParserConstants;
+
 public interface DriverConstants {
 	public static final String BUILD_KIND = "Majordomo.buildKind";
-	public static final String XML_PATH_SEGMENT = "promises-xml";
+	public static final String XML_PATH_SEGMENT = TestXMLParserConstants.LOCAL_XML_PATH;
 }
