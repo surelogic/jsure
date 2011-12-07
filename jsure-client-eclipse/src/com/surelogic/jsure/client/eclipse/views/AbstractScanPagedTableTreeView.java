df1dc58f988515b2dc18a0bd6b0f965304b2c2b3
Edwin Chan <edwin.chan@surelogic.com>
2011-12-07 15:44:10 -0500

    Initial code for building the new proposed promises view
    Former-commit-id: 0755849ab263dc573c8bca19e4ab7f02cce98c8f

---
+++ jsure-client-eclipse/src/com/surelogic/jsure/client/eclipse/views/AbstractScanPagedTableTreeView.java
@@ -0,0 +1,56 @@
+package com.surelogic.jsure.client.eclipse.views;
+
+import org.eclipse.jface.viewers.*;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+
+/**
+ * Switches between a table and a tree
+ * 
+ * @author Edwin
+ */
+public abstract class AbstractScanPagedTableTreeView<T> extends AbstractScanStructuredView<T> {
+	final IJSureTableTreeContentProvider f_content;
+	boolean asTree = true;
+	
+	protected AbstractScanPagedTableTreeView(int style, Class<T> c, 
+			IJSureTableTreeContentProvider content) {
+		super(style, c);
+		f_content = content;
+	}
+
+	@Override
+	protected StructuredViewer[] newViewers(Composite parent, int extraStyle) {
+		final TableViewer tableViewer = AbstractScanTableView.makeTableViewer(parent, extraStyle, f_content);		
+		final TreeViewer treeViewer = new TreeViewer(parent, SWT.H_SCROLL
+				| SWT.V_SCROLL | extraStyle);
+
+		treeViewer.setContentProvider(f_content);
+		treeViewer.setLabelProvider(f_content);
+		return new StructuredViewer[] { tableViewer, treeViewer };
+	}
+
+	@Override
+	protected final int getViewIndex() {
+		return asTree ? 1 : 0;
+	}
+	
+	@Override
+	protected String updateViewer() {
+		return f_content.build();
+	}
+
+	@Override
+	protected final void appendText(StringBuilder sb, Object elt) {
+		if (asTree) {
+			sb.append(f_content.getText(elt));
+		} else {
+			for(int i=0; i<f_content.getColumnLabels().length; i++) {
+				if (i != 0) {
+					sb.append(' ');
+				}
+				sb.append(f_content.getColumnText(elt, i));
+			}
+		}		
+	}	
+}
---
+++ jsure-client-eclipse/src/com/surelogic/jsure/client/eclipse/views/IJSureTableTreeContentProvider.java
@@ -0,0 +1,5 @@
+package com.surelogic.jsure.client.eclipse.views;
+
+public interface IJSureTableTreeContentProvider extends IResultsTableContentProvider, IJSureTreeContentProvider {
+	// Nothing else to add
+}
