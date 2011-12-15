928762e1427bb42390c1d10f174ae4e282b90d17
Nathan Boy <nathan.boy@gmail.com>
2011-12-15 15:26:48 -0500

    Fixed performance bug that caused big projects to take a very long time refactoring promises.
    Former-commit-id: fa7c0f6f1abe312a3031e0edbc24cff4ea9ec5c7

--- jsure-client-eclipse/src/com/surelogic/jsure/client/eclipse/refactor/ProposedPromisesChange.java
+++ jsure-client-eclipse/src/com/surelogic/jsure/client/eclipse/refactor/ProposedPromisesChange.java
@@ -1,7 +1,14 @@
 package com.surelogic.jsure.client.eclipse.refactor;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.jdt.core.ICompilationUnit;
@@ -13,37 +20,38 @@ import org.eclipse.ltk.core.refactoring.TextFileChange;
 import org.eclipse.text.edits.TextEdit;
 
 import com.surelogic.common.core.JDTUtility;
-import com.surelogic.common.ui.refactor.PromisesAnnotationRewriter;
 import com.surelogic.common.refactor.AnnotationDescription;
+import com.surelogic.common.refactor.AnnotationDescription.CU;
 import com.surelogic.common.refactor.Field;
 import com.surelogic.common.refactor.IJavaDeclaration;
-import com.surelogic.common.refactor.AnnotationDescription.CU;
+import com.surelogic.common.ui.refactor.PromisesAnnotationRewriter;
 import com.surelogic.javac.JavacTypeEnvironment;
 
 import edu.cmu.cs.fluid.java.ISrcRef;
 import edu.cmu.cs.fluid.java.bind.PromiseFramework;
-import edu.cmu.cs.fluid.sea.*;
+import edu.cmu.cs.fluid.sea.IProposedPromiseDropInfo;
 
 public class ProposedPromisesChange {
 	private final List<? extends IProposedPromiseDropInfo> drops;
 	private final String label;
-	
-	public ProposedPromisesChange(final List<? extends IProposedPromiseDropInfo> drops) {
+
+	public ProposedPromisesChange(
+			final List<? extends IProposedPromiseDropInfo> drops) {
 		this.drops = drops;
-		
+
 		// Just compute the label from the projects involved
 		//
 		// Get the projects involved (no dups)
 		Collection<String> projects = new HashSet<String>();
-		for(IProposedPromiseDropInfo p : drops) {
+		for (IProposedPromiseDropInfo p : drops) {
 			projects.add(p.getTargetProjectName());
 		}
 		// Sort them
 		projects = new ArrayList<String>(projects);
 		Collections.sort((List<String>) projects);
 		final StringBuilder sb = new StringBuilder();
-		for(String p : projects) {
-			final int len = sb.length(); 
+		for (String p : projects) {
+			final int len = sb.length();
 			if (len > 0) {
 				if (len > 50) {
 					sb.append(" ...");
@@ -69,10 +77,10 @@ public class ProposedPromisesChange {
 	void change(final CompositeChange root) {
 		final Set<String> projects = new HashSet<String>();
 		final Map<CU, Set<AnnotationDescription>> map = new HashMap<CU, Set<AnnotationDescription>>();
-		for (final IProposedPromiseDropInfo drop : drops) {	
+		for (final IProposedPromiseDropInfo drop : drops) {
 			projects.add(drop.getTargetProjectName());
 			projects.add(drop.getFromProjectName());
-			
+
 			final AnnotationDescription ann = desc(drop);
 			final CU cu = ann.getCU();
 			Set<AnnotationDescription> set = map.get(cu);
@@ -84,19 +92,22 @@ public class ProposedPromisesChange {
 		}
 		final Map<CU, Set<AnnotationDescription>> promiseMap = new HashMap<CU, Set<AnnotationDescription>>();
 		final Map<CU, Set<AnnotationDescription>> assumeMap = new HashMap<CU, Set<AnnotationDescription>>();
-		final PromisesAnnotationRewriter rewrite = new PromisesAnnotationRewriter(PromiseFramework.getInstance().getAllowsMultipleAnnosSet());
+		final PromisesAnnotationRewriter rewrite = new PromisesAnnotationRewriter(
+				PromiseFramework.getInstance().getAllowsMultipleAnnosSet());
 		try {
-			for(final String proj : projects) {
+			for (final String proj : projects) {
 				if (proj.startsWith(JavacTypeEnvironment.JRE_NAME)) {
 					// All binaries, so use assumption
 					continue;
 				}
-				final IJavaProject selectedProject = JDTUtility.getJavaProject(proj);
+				final IJavaProject selectedProject = JDTUtility
+						.getJavaProject(proj);
 				for (final IPackageFragment frag : selectedProject
 						.getPackageFragments()) {
-					for (final ICompilationUnit unit : frag.getCompilationUnits()) {
-						final CU check = new CU(proj, frag.getElementName(), unit
-								.getElementName());
+					for (final ICompilationUnit unit : frag
+							.getCompilationUnits()) {
+						final CU check = new CU(proj, frag.getElementName(),
+								unit.getElementName());
 						promiseMap.put(check, map.remove(check));
 					}
 				}
@@ -113,31 +124,34 @@ public class ProposedPromisesChange {
 					set.add(ann);
 				}
 			}
-			for(final String proj : projects) {
+			for (final String proj : projects) {
 				if (proj.startsWith(JavacTypeEnvironment.JRE_NAME)) {
 					continue;
 				}
-				final IJavaProject selectedProject = JDTUtility.getJavaProject(proj);
+				final IJavaProject selectedProject = JDTUtility
+						.getJavaProject(proj);
 				for (final IPackageFragment frag : selectedProject
 						.getPackageFragments()) {
-					for (final ICompilationUnit unit : frag.getCompilationUnits()) {
-						final CU check = new CU(proj, frag.getElementName(), unit.getElementName());
-						rewrite.setCompilationUnit(unit);
+					for (final ICompilationUnit unit : frag
+							.getCompilationUnits()) {
+						final CU check = new CU(proj, frag.getElementName(),
+								unit.getElementName());
 						final Set<AnnotationDescription> promiseSet = promiseMap
-						.get(check);
-						if (promiseSet != null) {
-							rewrite.writeAnnotations(promiseSet);
-						}
+								.get(check);
 						final Set<AnnotationDescription> assumeSet = assumeMap
-						.get(check);
-						if (assumeSet != null) {
-							rewrite.writeAssumptions(assumeSet);
-						}
+								.get(check);
 						if (promiseSet != null || assumeSet != null) {
+							rewrite.setCompilationUnit(unit);
+							if (promiseSet != null) {
+								rewrite.writeAnnotations(promiseSet);
+							}
+							if (assumeSet != null) {
+								rewrite.writeAssumptions(assumeSet);
+							}
 							final TextEdit textEdit = rewrite.getTextEdit();
 							final IFile file = (IFile) unit.getResource();
-							final TextFileChange change = new TextFileChange(file
-									.getName(), file);
+							final TextFileChange change = new TextFileChange(
+									file.getName(), file);
 							change.setEdit(textEdit);
 							root.add(change);
 						}
@@ -156,15 +170,17 @@ public class ProposedPromisesChange {
 	 * @param b
 	 * @return
 	 */
-	static AnnotationDescription desc(final IProposedPromiseDropInfo drop) {	
-		//final IBinder b = proj.getTypeEnv().getBinder();
-		final IJavaDeclaration target = drop.getTargetInfo();//IRNodeUtil.convert(b, drop.getNode());
-		
+	static AnnotationDescription desc(final IProposedPromiseDropInfo drop) {
+		// final IBinder b = proj.getTypeEnv().getBinder();
+		final IJavaDeclaration target = drop.getTargetInfo();// IRNodeUtil.convert(b,
+																// drop.getNode());
+
 		// @Assume cannot be on a field, so we just stick it on the parent type
 		// in that case
-		IJavaDeclaration from = drop.getFromInfo();// = IRNodeUtil.convert(b, drop.getRequestedFrom());
-		//System.out.println("Target = "+target);
-		//System.out.println("From   = "+from);
+		IJavaDeclaration from = drop.getFromInfo();// = IRNodeUtil.convert(b,
+													// drop.getRequestedFrom());
+		// System.out.println("Target = "+target);
+		// System.out.println("From   = "+from);
 		if (from instanceof Field) {
 			from = ((Field) from).getTypeContext();
 		}
@@ -172,11 +188,14 @@ public class ProposedPromisesChange {
 		final String annotation = drop.getAnnotation();
 		final String contents = drop.getContents();
 		ISrcRef srcRef = drop.getSrcRef();
-		final CU cu = new CU(drop.getTargetProjectName(), srcRef.getPackage(), srcRef.getRelativePath());
+		final CU cu = new CU(drop.getTargetProjectName(), srcRef.getPackage(),
+				srcRef.getRelativePath());
 		srcRef = drop.getAssumptionRef();
-		final CU assumptionCU = new CU(drop.getFromProjectName(), srcRef.getPackage(), srcRef.getRelativePath());
-		return new AnnotationDescription(annotation, contents, drop.getReplacedContents(), target,
-				assumptionTarget, cu, assumptionCU);
+		final CU assumptionCU = new CU(drop.getFromProjectName(),
+				srcRef.getPackage(), srcRef.getRelativePath());
+		return new AnnotationDescription(annotation, contents,
+				drop.getReplacedContents(), target, assumptionTarget, cu,
+				assumptionCU);
 	}
 
 	public String getSelectedProjectNames() {
