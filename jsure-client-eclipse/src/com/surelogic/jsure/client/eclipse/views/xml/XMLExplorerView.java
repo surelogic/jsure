c9db3204f85a413f86a2cadeecfe9580c9edbd84
Edwin Chan <edwin.chan@surelogic.com>
2011-12-15 10:34:20 -0500

    Added "Find XML" to view menu and toolbar
    Former-commit-id: 0a2ce6b1598d1535d10c244d70c136f62b1e8c37

--- jsure-client-eclipse/src/com/surelogic/jsure/client/eclipse/views/xml/XMLExplorerView.java
+++ jsure-client-eclipse/src/com/surelogic/jsure/client/eclipse/views/xml/XMLExplorerView.java
@@ -33,6 +33,7 @@ import com.surelogic.common.XUtil;
 import com.surelogic.common.i18n.I18N;
 import com.surelogic.common.ui.SLImages;
 import com.surelogic.common.ui.jobs.SLUIJob;
+import com.surelogic.jsure.client.eclipse.actions.FindXMLForTypeAction;
 import com.surelogic.jsure.client.eclipse.editors.PromisesXMLContentProvider;
 import com.surelogic.jsure.client.eclipse.editors.PromisesXMLEditor;
 import com.surelogic.jsure.client.eclipse.views.AbstractJSureView;
@@ -125,6 +126,8 @@ public class XMLExplorerView extends AbstractJSureView {
 		}
 	};
 
+	private final Action f_findXML = new FindXMLForTypeAction();
+	
 	@Override
 	protected Control buildViewer(Composite parent) {
 		f_viewer = new TreeViewer(parent, SWT.H_SCROLL | SWT.V_SCROLL);
@@ -143,6 +146,11 @@ public class XMLExplorerView extends AbstractJSureView {
 
 	@Override
 	protected void makeActions() {
+		f_findXML.setText("Find XML For Type...");
+		f_findXML.setToolTipText("Find the promises XML file for a type");
+		f_findXML.setImageDescriptor(SLImages
+				.getImageDescriptor(CommonImages.IMG_JSURE_LOGO));
+		
 		f_toggleShowDiffs.setImageDescriptor(SLImages
 				.getImageDescriptor(CommonImages.IMG_ANNOTATION_DELTA));
 		f_toggleShowDiffs.setToolTipText(I18N
@@ -169,6 +177,7 @@ public class XMLExplorerView extends AbstractJSureView {
 	@Override
 	protected void fillLocalPullDown(IMenuManager manager) {
 		manager.add(f_actionCollapseAll);
+		manager.add(f_findXML);
 		manager.add(new Separator());
 		manager.add(f_toggleShowDiffs);
 	}
@@ -176,6 +185,7 @@ public class XMLExplorerView extends AbstractJSureView {
 	@Override
 	protected void fillLocalToolBar(IToolBarManager manager) {
 		manager.add(f_actionCollapseAll);
+		manager.add(f_findXML);
 		manager.add(new Separator());
 		manager.add(f_toggleShowDiffs);
 	}
