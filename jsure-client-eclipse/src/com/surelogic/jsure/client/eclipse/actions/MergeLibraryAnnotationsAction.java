e92c2020dfe9ac9e59fb00a16c14985ce5e17d22
Edwin Chan <edwin.chan@surelogic.com>
2012-02-02 10:19:58 -0500

    Changed to use the workspace's XML directory
    Former-commit-id: c9d7e52ae45fe956349d5e7e6d8a4d2589db0e07

---
+++ jsure-client-eclipse/src/com/surelogic/jsure/client/eclipse/actions/MergeLibraryAnnotationsAction.java
@@ -0,0 +1,48 @@
+package com.surelogic.jsure.client.eclipse.actions;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.FileDialog;
+
+import com.surelogic.common.FileUtility;
+import com.surelogic.common.TextArchiver;
+import com.surelogic.common.logging.SLLogger;
+import com.surelogic.common.ui.EclipseUIUtility;
+import com.surelogic.common.ui.actions.AbstractMainAction;
+import com.surelogic.jsure.core.preferences.JSurePreferencesUtility;
+
+public class MergeLibraryAnnotationsAction extends AbstractMainAction {
+	private static final FileUtility.TempFileFilter filter = 
+		new FileUtility.TempFileFilter("textArchiver", "destDir");
+	
+	@Override
+	public void run(IAction action) {
+		// choose a text file
+		FileDialog d = new FileDialog(EclipseUIUtility.getShell(), SWT.OPEN);
+		String path = d.open();
+		if (path != null) {
+			File srcArchive = new File(path);
+			if (srcArchive.exists()) {
+				System.out.println("Length of "+srcArchive+" : "+srcArchive.length());
+			} else {
+				System.out.println("Can't find "+srcArchive);
+			}
+			// Make a temp directory
+			try {				
+				File destDir = JSurePreferencesUtility.getJSureXMLDirectory();//filter.createTempFolder();
+				TextArchiver.unarchive(srcArchive, destDir);
+				System.out.println("Unarchived to "+destDir);
+			} catch (IOException e) {
+				MessageDialog.openError(EclipseUIUtility.getShell(), "Error while merging library annotations", 
+						e.getMessage());
+				SLLogger.getLogger().log(Level.WARNING, "Error while merging library annotations", e);
+			}
+
+		}
+	}
+}
