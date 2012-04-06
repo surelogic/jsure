41cddd6feab96f5e29d2b7bc66adbcc5e6a99b53
Tim Halloran <tim.halloran@surelogic.com>
2012-04-06 11:51:09 -0400

    Added preference to load all classes for annotation consistency testing
    Former-commit-id: e01bdcef1a97ee5e5780c54f75cf99a6eb5fe4ba

--- fluid/src/edu/cmu/cs/fluid/ide/IDEPreferences.java
+++ fluid/src/edu/cmu/cs/fluid/ide/IDEPreferences.java
@@ -8,29 +8,21 @@ public interface IDEPreferences {
 	String JSURE_DATA_DIRECTORY = PREFIX + "data.directory";
 	String DEFAULT_JRE = PREFIX + "default.JRE";
 	String JSURE_XML_DIRECTORY = PREFIX + "xml.directory";
+	String LOAD_ALL_CLASSES = PREFIX + "load.all.classes";
 
 	String TIMEOUT_WARNING_SEC = PREFIX + "timeoutWarningSec";
 	String TIMEOUT_FLAG = PREFIX + "timeoutFlag";
 	String TIMEOUT_SEC = PREFIX + "timeoutSec";
 
-	String[] BOOL_PREFS_TO_SYNC = {
-		ALLOW_JAVADOC_ANNOS,
-		TIMEOUT_FLAG,			
-	};
-	
-	String[] INT_PREFS_TO_SYNC = {
-		TOOL_MEMORY_MB,
-		ANALYSIS_THREAD_COUNT,
-		TIMEOUT_WARNING_SEC,
-		TIMEOUT_SEC,
-	};
-			
-	String[] STR_PREFS_TO_SYNC = {
-		JSURE_DATA_DIRECTORY,
-		// DEFAULT_JRE, 
-		JSURE_XML_DIRECTORY,
-	};
-	
+	String[] BOOL_PREFS_TO_SYNC = { ALLOW_JAVADOC_ANNOS, TIMEOUT_FLAG, };
+
+	String[] INT_PREFS_TO_SYNC = { TOOL_MEMORY_MB, ANALYSIS_THREAD_COUNT,
+			TIMEOUT_WARNING_SEC, TIMEOUT_SEC, };
+
+	String[] STR_PREFS_TO_SYNC = { JSURE_DATA_DIRECTORY,
+			// DEFAULT_JRE,
+			JSURE_XML_DIRECTORY, };
+
 	/**
 	 * The preference prefix for whether an analysis is on
 	 */
