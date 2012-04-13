43d5e37d0b635864de51d2407a1daf20b38e06c7
Edwin Chan <edwin.chan@surelogic.com>
2012-04-13 16:13:45 -0400

    Added javadoc
    Former-commit-id: eef61f18443e4d03ba96243b60cf1b283d23eb63

--- jsure-client-eclipse/src/com/surelogic/jsure/client/eclipse/views/IJSureTreeContentProvider.java
+++ jsure-client-eclipse/src/com/surelogic/jsure/client/eclipse/views/IJSureTreeContentProvider.java
@@ -3,5 +3,8 @@ package com.surelogic.jsure.client.eclipse.views;
 import com.surelogic.common.ui.views.ITreeLabelContentProvider;
 
 public interface IJSureTreeContentProvider extends ITreeLabelContentProvider {
+	/**
+	 * @return a non-null value for a valid scan
+	 */
 	String build();
 }
