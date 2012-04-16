22f71a435808601762672a4add7bf72ca94630bb
Edwin Chan <edwin.chan@surelogic.com>
2012-04-16 15:04:02 -0400

    Added view to explore the actual annotations in a scan
    Former-commit-id: 752673561d2cadd197b54f7a8a904626f2f4ec71

--- fluid/src/com/surelogic/persistence/JavaIdentifier.java
+++ fluid/src/com/surelogic/persistence/JavaIdentifier.java
@@ -154,6 +154,9 @@ public final class JavaIdentifier {
 			*/
 				return null;
 			//}
+		} else if (ParameterDeclaration.prototype.includes(op)) {
+			IRNode func = VisitUtil.getEnclosingClassBodyDecl(decl);
+			id = JJNode.getInfoOrNull(func);
 		} else {
 			id = JJNode.getInfoOrNull(decl);
 			if (id == null) {
@@ -234,7 +237,13 @@ public final class JavaIdentifier {
 	}
 	
 	public static String extractDecl(String typePrefix, String code) {
+		if (code.contains("arg0")) {
+			System.out.println("Checking out arg0");
+		}
 		String[] parts = code.split(SEPARATOR);		
+		if (parts.length == 3) {
+			return parts[2];
+		}
 		if (parts.length < 4) {
 			throw new IllegalArgumentException("Bad encoding: "+code);
 		}
@@ -246,7 +255,7 @@ public final class JavaIdentifier {
 		} else {
 			sb.append(parts[2]).append('.');			
 		}
-		for(int i=3; i<parts.length; i++) {
+		for(int i=3; i<parts.length && i<5; i++) {
 			sb.append(parts[i]);
 		}
 		return sb.toString();
