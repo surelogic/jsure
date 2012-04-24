dfa2696a52984b1bf7dac9f7c81c40d154381c29
Tim Halloran <tim.halloran@surelogic.com>
2012-04-24 11:25:38 -0400

    Removed import
    Former-commit-id: c2d830d1274f41de637257f510082b116d33db6b

--- jsure-client-eclipse/src/com/surelogic/jsure/client/eclipse/views/results/ProblemsViewContentProvider.java
+++ jsure-client-eclipse/src/com/surelogic/jsure/client/eclipse/views/results/ProblemsViewContentProvider.java
@@ -7,7 +7,6 @@ import java.util.Set;
 import org.eclipse.swt.graphics.Image;
 
 import com.surelogic.common.CommonImages;
-import com.surelogic.common.tool.ToolProperties;
 import com.surelogic.common.ui.SLImages;
 import com.surelogic.jsure.core.scans.JSureDataDirHub;
 import com.surelogic.jsure.core.scans.JSureScanInfo;
