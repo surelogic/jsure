24981e84c4a56a77c1799dfca099ee0a3c5195c3
Aaron Greenhouse <aarong@surelogic.com>
2012-08-21 15:09:56 -0400

    Fixed the message for AnnotationBoundsPromiseDrop
    Former-commit-id: 17e84392606923d7e21c531f55f41dde105a8761

--- fluid/src/edu/cmu/cs/fluid/java/bind/Messages.java
+++ fluid/src/edu/cmu/cs/fluid/java/bind/Messages.java
@@ -37,6 +37,7 @@ public final class Messages {
   public static final int SingletonDrop = 133;
   public static final int RegionAnnotation_borrowedInRegionDrop = 134;
   public static final int InstantiatedGeneric = 135;
+  public static final int AnnotationBounds = 136;
   
   /* 2011-01-18: Edwin says these are referenced from code that will become
    * obsolete and deleted.
--- fluid/src/edu/cmu/cs/fluid/sea/drops/promises/AnnotationBoundsPromiseDrop.java
+++ fluid/src/edu/cmu/cs/fluid/sea/drops/promises/AnnotationBoundsPromiseDrop.java
@@ -1,9 +1,12 @@
 package edu.cmu.cs.fluid.sea.drops.promises;
 
+import com.surelogic.aast.java.NamedTypeNode;
 import com.surelogic.aast.promise.AnnotationBoundsNode;
 import com.surelogic.annotation.scrub.ValidatedDropCallback;
 
 import edu.cmu.cs.fluid.java.JavaGlobals;
+import edu.cmu.cs.fluid.java.JavaNames;
+import edu.cmu.cs.fluid.java.bind.Messages;
 import edu.cmu.cs.fluid.sea.drops.BooleanPromiseDrop;
 
 public final class AnnotationBoundsPromiseDrop extends
@@ -13,7 +16,31 @@ public final class AnnotationBoundsPromiseDrop extends
     super(a); 
     setCategory(JavaGlobals.LOCK_ASSURANCE_CAT);
   }
+  
+  @Override
+  protected void computeBasedOnAST() {
+    final AnnotationBoundsNode ast = getAST();
+    setResultMessage(Messages.AnnotationBounds,
+        getNameList(ast.getContainable()),
+        getNameList(ast.getImmutable()),
+        getNameList(ast.getThreadSafe()),
+        JavaNames.getTypeName(getNode()));
+  }
 
+  private String getNameList(final NamedTypeNode[] list) {
+    final StringBuilder sb = new StringBuilder();
+    boolean first = true;
+    for (final NamedTypeNode namedType : list) {
+      if (!first) {
+        sb.append(", ");
+      } else {
+        first = false;
+      }
+      sb.append(namedType.getType());      
+    }
+    return sb.toString();
+  }
+  
   public void validated(final AnnotationBoundsPromiseDrop pd) {
     pd.setVirtual(true);
     pd.setSourceDrop(this);
