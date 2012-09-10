dd122b91d5fa5c15e9e9031943f8e60e4f311733
Tim Halloran <tim.halloran@surelogic.com>
2012-09-10 13:51:44 -0400

    Updated Javadoc.
    Former-commit-id: b31b4c77a50be312a36f4d72e626a58c0e6ffc7e

--- fluid/src/edu/cmu/cs/fluid/sea/InfoDrop.java
+++ fluid/src/edu/cmu/cs/fluid/sea/InfoDrop.java
@@ -2,11 +2,11 @@ package edu.cmu.cs.fluid.sea;
 
 /**
  * Drops for reporting inferred or information to the user, "i" results.
+ * <p>
+ * The only subtype of this should be {@link WarningDrop}. This type is not
+ * intended to be otherwise subtyped.
  */
 public class InfoDrop extends IRReferenceDrop implements IReportedByAnalysisDrop {
-
-  public InfoDrop() {
-  }
 
   public interface Factory {
     InfoDrop create();
--- fluid/src/edu/cmu/cs/fluid/sea/WarningDrop.java
+++ fluid/src/edu/cmu/cs/fluid/sea/WarningDrop.java
@@ -1,8 +1,11 @@
 package edu.cmu.cs.fluid.sea;
 
 /**
- * Drops for reporting warnings NOT for scrubber warnings -- use
- * PromiseWarningDrop instead
+ * Drops for reporting warnings that a particular verifying analyis wants to
+ * bring to the attention of the tool user.
+ * <p>
+ * This type is <b>not</b> used for scrubber warnings&mdash;for that purpose use
+ * {@link PromiseWarningDrop}.
  */
 public final class WarningDrop extends InfoDrop {
   public WarningDrop() {
