8262777caad9d1e690f3de0eac1e1dda3d706823
Tim Halloran <tim.halloran@surelogic.com>
2012-09-10 15:02:06 -0400

    Working on ISrcRef
    Former-commit-id: 83db89c02fc33a45fb8c5c58f83dff92cc950182

--- fluid/src/edu/cmu/cs/fluid/java/DummySrcRef.java
+++ fluid/src/edu/cmu/cs/fluid/java/DummySrcRef.java
@@ -1,31 +1,26 @@
-/*$Header: /cvs/fluid/fluid/src/edu/cmu/cs/fluid/java/DummySrcRef.java,v 1.5 2008/06/30 15:23:12 chance Exp $*/
 package edu.cmu.cs.fluid.java;
 
 public class DummySrcRef extends AbstractSrcRef {
-  public static final ISrcRef prototype = new DummySrcRef();
+
   public static final ISrcRef undefined = new DummySrcRef();
-  
+
   private DummySrcRef() {
-	  // Nothing to do
+    // no other instances
   }
 
   public String getCUName() {
-	  // TODO Auto-generated method stub
-	  return null;
+    return null;
   }
 
   public Long getHash() {
-	  // TODO Auto-generated method stub
-	  return null;
+    return null;
   }
 
   public String getPackage() {
-	  // TODO Auto-generated method stub
-	  return null;
+    return null;
   }
 
   public String getProject() {
-	  // TODO Auto-generated method stub
-	  return null;
+    return null;
   }
 }
