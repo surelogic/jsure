c9584f73f21c548b21673efc1c8a27827bd58151
Edwin Chan <edwin.chan@surelogic.com>
2012-09-11 13:22:01 -0400

    Added consistency checks for MustInvokeOnOverride
    Former-commit-id: 6d9b744c7be2cc4cc2be9ebfff92682f9abb3a1b

--- fluid/src/com/surelogic/annotation/rules/StructureRules.java
+++ fluid/src/com/surelogic/annotation/rules/StructureRules.java
@@ -6,7 +6,11 @@ import com.surelogic.aast.promise.*;
 import com.surelogic.promise.*;
 
 import edu.cmu.cs.fluid.ir.IRNode;
+import edu.cmu.cs.fluid.java.JavaNames;
+import edu.cmu.cs.fluid.java.JavaNode;
+import edu.cmu.cs.fluid.java.bind.IBinding;
 import edu.cmu.cs.fluid.java.bind.PromiseFramework;
+import edu.cmu.cs.fluid.java.util.VisitUtil;
 import edu.cmu.cs.fluid.sea.PromiseDrop;
 import edu.cmu.cs.fluid.sea.drops.promises.*;
 
@@ -21,7 +25,7 @@ public class StructureRules extends AnnotationRules {
 		return instance;
 	}
 
-	public static MustInvokeOnOverridePromiseDrop getSingletonDrop(IRNode type) {
+	public static MustInvokeOnOverridePromiseDrop getMustInvokeDrop(IRNode type) {
 		return getDrop(mustInvokeOnOverrideRule.getStorage(), type);
 	}
 	
@@ -44,14 +48,47 @@ public class StructureRules extends AnnotationRules {
 
 		@Override
 		protected IAnnotationScrubber makeScrubber() {
-			return new AbstractAASTScrubber<MustInvokeOnOverrideNode, MustInvokeOnOverridePromiseDrop>(this) {
+			return new AbstractAASTScrubber<MustInvokeOnOverrideNode, MustInvokeOnOverridePromiseDrop>
+			           (this, ScrubberType.INCLUDE_OVERRIDDEN_METHODS_BY_HIERARCHY) {
 				@Override
 				protected PromiseDrop<MustInvokeOnOverrideNode> makePromiseDrop(MustInvokeOnOverrideNode a) {
+				  	if (isInFinalContext(a.getPromisedFor())) {
+			    		return null; // illegal
+			    	}
 					// TODO scrub
 					final MustInvokeOnOverridePromiseDrop d = new MustInvokeOnOverridePromiseDrop(a);
 					return storeDropIfNotNull(a, d);
 				}
+				
+			    @Override
+			    protected boolean processUnannotatedMethodRelatedDecl(final IRNode unannodMethod) {				    
+			    	if (isInFinalContext(unannodMethod)) {
+			    		return true; // ok
+			    	}
+			    	// check if parent(s) has @MustInvokeOnOverride
+			    	for(IBinding overridden : getContext().getBinder(unannodMethod).findOverriddenParentMethods(unannodMethod)) {
+			    		if (getMustInvokeDrop(overridden.getNode()) != null) {
+			    			getContext().reportError(unannodMethod, "Should be marked with @"+MUST_INVOKE_ON_OVERRIDE+
+			    					" like the method it overrides: "+JavaNames.getFullName(overridden.getNode()));
+			    			return false;
+			    		}			    		
+			    	}
+			    	return true; // ok
+			    }
 			};
 		}
+	}
+	
+	static boolean isInFinalContext(IRNode method) {
+    	final boolean isFinal = JavaNode.getModifier(method, JavaNode.FINAL);
+    	if (isFinal) {
+    		return true; 
+    	}
+    	final IRNode type = VisitUtil.getEnclosingType(method);
+    	final boolean typeIsFinal = JavaNode.getModifier(type, JavaNode.FINAL);
+    	if (typeIsFinal) {
+    		return true; 
+    	}
+    	return false;
 	}
 }
