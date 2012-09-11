0636043db4ed99c35013612bfe1a2c16af8c235b
Tim Halloran <tim.halloran@surelogic.com>
2012-09-11 13:47:53 -0400

    Working on proof drops
    Former-commit-id: d479e5dedc0440a1b2bfe2d58409d311b52e17f2

--- fluid/src/edu/cmu/cs/fluid/sea/drops/layers/LayerPromiseDrop.java
+++ fluid/src/edu/cmu/cs/fluid/sea/drops/layers/LayerPromiseDrop.java
@@ -8,36 +8,32 @@ import edu.cmu.cs.fluid.ir.IRNode;
 import edu.cmu.cs.fluid.java.util.VisitUtil;
 
 public class LayerPromiseDrop extends AbstractReferenceCheckDrop<LayerNode> {
-	public LayerPromiseDrop(LayerNode a) {
-		super(a);
-		//System.out.println("Creating layer: "+a);
-	}
-	/*
-	@Override
-	protected void invalidate_internal() {
-		System.out.println("Invalidating layer: "+getAST());
-	}
-	*/
-	public String getId() {
-		return getAAST().getId();
-	}
-	public int getResultMessageKind() {
-		return 350;
-	}
-	public Object[] getArgs(IRNode binding, IRNode type, IRNode context) {
-		return new Object[] { binding, VisitUtil.getClosestType(context) };
-	}
-	public boolean isPartOf(IRNode type) {
-		// Compute qualified name for this layer
-		final IRNode cu    = VisitUtil.getEnclosingCompilationUnit(getNode());
-		final String qname = VisitUtil.getPackageName(cu)+'.'+getAAST().getId();
-		// Check if matches the layer for the type 
-		final InLayerPromiseDrop bindInLayer = LayerRules.getInLayerDrop(type);
-		if (bindInLayer == null) {
-			return false;
-		}
-		return bindInLayer.getAAST().getLayers().matches(qname);
-	}	
+  public LayerPromiseDrop(LayerNode a) {
+    super(a);
+    // System.out.println("Creating layer: "+a);
+  }
+
+  public String getId() {
+    return getAAST().getId();
+  }
+
+  public int getResultMessageKind() {
+    return 350;
+  }
+
+  public Object[] getArgs(IRNode binding, IRNode type, IRNode context) {
+    return new Object[] { binding, VisitUtil.getClosestType(context) };
+  }
+
+  public boolean isPartOf(IRNode type) {
+    // Compute qualified name for this layer
+    final IRNode cu = VisitUtil.getEnclosingCompilationUnit(getNode());
+    final String qname = VisitUtil.getPackageName(cu) + '.' + getAAST().getId();
+    // Check if matches the layer for the type
+    final InLayerPromiseDrop bindInLayer = LayerRules.getInLayerDrop(type);
+    if (bindInLayer == null) {
+      return false;
+    }
+    return bindInLayer.getAAST().getLayers().matches(qname);
+  }
 }
-
-
--- fluid/src/edu/cmu/cs/fluid/sea/drops/promises/AbstractLockListPromiseDrop.java
+++ fluid/src/edu/cmu/cs/fluid/sea/drops/promises/AbstractLockListPromiseDrop.java
@@ -1,62 +1,41 @@
-/*$Header: /cvs/fluid/fluid/.settings/org.eclipse.jdt.ui.prefs,v 1.2 2006/03/27 21:35:50 boyland Exp $*/
 package edu.cmu.cs.fluid.sea.drops.promises;
 
-import com.surelogic.aast.promise.*;
+import com.surelogic.aast.promise.AbstractLockListNode;
 
-import edu.cmu.cs.fluid.ir.IRNode;
-import edu.cmu.cs.fluid.java.DebugUnparser;
 import edu.cmu.cs.fluid.java.JavaNames;
 import edu.cmu.cs.fluid.java.bind.Messages;
-import edu.cmu.cs.fluid.sea.*;
+import edu.cmu.cs.fluid.sea.Drop;
+import edu.cmu.cs.fluid.sea.PromiseDrop;
 
-public abstract class AbstractLockListPromiseDrop<N extends AbstractLockListNode> 
-extends PromiseDrop<N> {
-	AbstractLockListPromiseDrop(N node) {
-		super(node);
-	}
-	
-	private IRNode lockNode; // used for early lock declaration
+public abstract class AbstractLockListPromiseDrop<N extends AbstractLockListNode> extends PromiseDrop<N> {
+  AbstractLockListPromiseDrop(N node) {
+    super(node);
+  }
 
-	/**
-	 * @return Returns the lockNode.
-	 */
-	@Deprecated
-	public final IRNode getLockNode() {
-		return lockNode;
-	}
+  /**
+   * Need to clean up lock models.
+   * 
+   * @see edu.cmu.cs.fluid.sea.Drop#deponentInvalidAction(Drop)
+   */
+  @Override
+  protected void deponentInvalidAction(Drop invalidDeponent) {
+    super.deponentInvalidAction(invalidDeponent);
+    LockModel.purgeUnusedLocks();
+  }
 
-	/**
-	 * @param lockNode The lockNode to set.
-	 */
-	@Deprecated
-	public final void setLockNode(IRNode lockNode) {
-		this.lockNode = lockNode;
-	}
-	
-	/**
-	 * Need to clean up lock models.
-	 * 
-	 * @see edu.cmu.cs.fluid.sea.Drop#deponentInvalidAction(Drop)
-	 */
-	@Override
-	protected void deponentInvalidAction(Drop invalidDeponent) {
-		super.deponentInvalidAction(invalidDeponent);
-		LockModel.purgeUnusedLocks();
-	}
+  /**
+   * @see edu.cmu.cs.fluid.sea.PromiseDrop#isCheckedByAnalysis()
+   */
+  @Override
+  public boolean isCheckedByAnalysis() {
+    return true;
+  }
 
-	/**
-	 * @see edu.cmu.cs.fluid.sea.PromiseDrop#isCheckedByAnalysis()
-	 */
-	@Override
-	public boolean isCheckedByAnalysis() {
-		return true;
-	}
-	
-	  
-	@Override
-	protected void computeBasedOnAST() {
-		String lock = getAAST() != null ? getAAST().toString() : DebugUnparser.toString(lockNode);
-		setResultMessage(Messages.LockAnnotation_requiresLockDrop, 
-				lock, JavaNames.genMethodConstructorName(getNode()));
-	}
+  @Override
+  protected void computeBasedOnAST() {
+    if (getAAST() != null) {
+      setResultMessage(Messages.LockAnnotation_requiresLockDrop, getAAST().toString(),
+          JavaNames.genMethodConstructorName(getNode()));
+    }
+  }
 }
