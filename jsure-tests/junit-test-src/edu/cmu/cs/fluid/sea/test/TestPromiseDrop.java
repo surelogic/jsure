4c3d1250776d191a4a2485669dea1260972104d8
Tim Halloran <tim.halloran@surelogic.com>
2012-09-11 14:14:22 -0400

    Making promise drops thread safe.
    Former-commit-id: 4df693d4b2ab35aa0197eff9653c763df1bfcdb8

--- jsure-tests/junit-test-src/edu/cmu/cs/fluid/sea/test/TestPromiseDrop.java
+++ jsure-tests/junit-test-src/edu/cmu/cs/fluid/sea/test/TestPromiseDrop.java
@@ -15,12 +15,12 @@ public class TestPromiseDrop extends TestCase {
 
   static private final IRNode i = new PlainIRNode();
 
-  static class LockPromiseDrop extends PromiseDrop<IAASTRootNode> {
+  static class TestLockPromiseDrop extends PromiseDrop<IAASTRootNode> {
 	  // Nothing to add
   }
 
   @SuppressWarnings("unused")
-  private LockPromiseDrop d1, d2, d3, d4, d5, d6, d7;
+  private TestLockPromiseDrop d1, d2, d3, d4, d5, d6, d7;
 
   public void testPromiseDropBasics() {
     assertFalse("promise should be unchecked at this point", d1
@@ -31,7 +31,7 @@ public class TestPromiseDrop extends TestCase {
   @Override
   protected void setUp() throws Exception {
     super.setUp();
-    d1 = new LockPromiseDrop();
+    d1 = new TestLockPromiseDrop();
     d1.setNode(i);
   }
 
