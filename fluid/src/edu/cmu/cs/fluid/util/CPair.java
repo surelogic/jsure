7b9c4d9e95c477bc5690d9e9b9c4ede838b2af07
Edwin Chan <edwin.chan@surelogic.com>
2012-09-20 14:05:38 -0400

    Added CPair which is a Comparable Pair
    Former-commit-id: 082ef09c3dbe2628a0eb34241736d9ea521e52b1

---
+++ fluid/src/edu/cmu/cs/fluid/util/CPair.java
@@ -0,0 +1,20 @@
+package edu.cmu.cs.fluid.util;
+
+/**
+ * Comparable Pair
+ */
+public final class CPair<T1 extends Comparable<T1>, T2 extends Comparable<T2>> 
+extends Pair<T1,T2> implements Comparable<CPair<T1,T2>>{
+	public CPair(T1 o1, T2 o2) {
+		super(o1, o2);
+	}
+
+	@Override
+	public int compareTo(CPair<T1, T2> o) {
+		int rv = first().compareTo(o.first());
+		if (rv == 0) {
+			rv = second().compareTo(o.second());
+		}
+		return rv;
+	}
+}
