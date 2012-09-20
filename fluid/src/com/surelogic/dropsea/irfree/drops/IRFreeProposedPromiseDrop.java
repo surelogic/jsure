8f6884aba41202a5142acda0feb04f54527b9e4c
Tim Halloran <tim.halloran@surelogic.com>
2012-09-20 12:19:02 -0400

    Proposed promise converted.
    Former-commit-id: 543df311f8a2ea085c270f4edefabc4817d4f22d

--- fluid/src/com/surelogic/dropsea/irfree/drops/IRFreeProposedPromiseDrop.java
+++ fluid/src/com/surelogic/dropsea/irfree/drops/IRFreeProposedPromiseDrop.java
@@ -13,9 +13,12 @@ import static com.surelogic.common.jsure.xml.AbstractXMLReader.REPLACED_CONTENTS
 import static com.surelogic.common.jsure.xml.AbstractXMLReader.TARGET_INFO;
 import static com.surelogic.common.jsure.xml.AbstractXMLReader.TARGET_PROJECT;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
 
+import com.surelogic.common.i18n.I18N;
+import com.surelogic.common.logging.SLLogger;
 import com.surelogic.common.refactor.IJavaDeclaration;
 import com.surelogic.common.refactor.JavaDeclInfo;
 import com.surelogic.common.xml.Entity;
@@ -47,74 +50,84 @@ public final class IRFreeProposedPromiseDrop extends IRFreeDrop implements IProp
   private JavaDeclInfo f_fromInfo;
   private JavaDeclInfo f_targetInfo;
   private ISrcRef f_assumptionRef;
-  private Map<String, String> f_annoAttributes = null;
-  private Map<String, String> f_replacedAttributes = null;
+  private final Map<String, String> f_annoAttributes = new HashMap<String, String>();
+  private final Map<String, String> f_replacedAttributes = new HashMap<String, String>();
+  private final String f_JavaAnnotation;
+  private final String f_annotation;
+  private final String f_contents;
+  private final String f_replacedAnnotation;
+  private final String f_replacedContents;
+  private final String f_targetProjectName;
+  private final String f_fromProjectName;
+  private final Origin f_origin;
 
   void setAssumptionRef(ISrcRef value) {
     f_assumptionRef = value;
   }
 
   void setAnnoAttributes(Map<String, String> value) {
-    f_annoAttributes = value;
+    f_annoAttributes.clear();
+    f_annoAttributes.putAll(value);
   }
 
   void setReplacedAttributes(Map<String, String> value) {
-    f_replacedAttributes = value;
+    f_replacedAttributes.clear();
+    f_replacedAttributes.putAll(value);
   }
 
   IRFreeProposedPromiseDrop(Entity e, Class<?> irClass) {
     super(e, irClass);
+
+    f_JavaAnnotation = e.getAttribute(JAVA_ANNOTATION);
+    f_annotation = e.getAttribute(ANNOTATION_TYPE);
+    f_contents = e.getAttribute(CONTENTS);
+    f_replacedAnnotation = e.getAttribute(REPLACED_ANNO);
+    f_replacedContents = e.getAttribute(REPLACED_CONTENTS);
+    f_targetProjectName = e.getAttribute(TARGET_PROJECT);
+    f_fromProjectName = e.getAttribute(FROM_PROJECT);
+
+    final String origin = e.getAttribute(ORIGIN);
+    Origin result = Origin.MODEL; // default
+    if (origin != null) {
+      try {
+        result = Origin.valueOf(origin);
+      } catch (Exception e1) {
+        SLLogger.getLogger().log(Level.WARNING, I18N.err(249, origin, ORIGIN), e1);
+      }
+    }
+    f_origin = result;
   }
 
   public Map<String, String> getAnnoAttributes() {
-    if (f_annoAttributes != null)
-      return f_annoAttributes;
-    else
-      return Collections.emptyMap();
+    return f_annoAttributes;
   }
 
   public Map<String, String> getReplacedAttributes() {
-    if (f_replacedAttributes != null)
-      return f_replacedAttributes;
-    else
-      return Collections.emptyMap();
+    return f_replacedAttributes;
   }
 
   public String getJavaAnnotation() {
-    return getEntity().getAttribute(JAVA_ANNOTATION);
+    return f_JavaAnnotation;
   }
 
   public String getAnnotation() {
-    return getEntity().getAttribute(ANNOTATION_TYPE);
+    return f_annotation;
   }
 
   public String getContents() {
-    return getEntity().getAttribute(CONTENTS);
+    return f_contents;
   }
 
   public String getReplacedAnnotation() {
-    return getEntity().getAttribute(REPLACED_ANNO);
+    return f_replacedAnnotation;
   }
 
   public String getReplacedContents() {
-    return getEntity().getAttribute(REPLACED_CONTENTS);
+    return f_replacedContents;
   }
 
   public Origin getOrigin() {
-    final String origin = getEntity().getAttribute(ORIGIN);
-    Origin result = Origin.MODEL;
-    if (origin == null) {
-      /*
-       * The scan is old and doesn't have an origin, just return a default.
-       */
-      return result;
-    }
-    try {
-      result = Origin.valueOf(origin);
-    } catch (Exception ignoreTakeDefault) {
-      // Ignore we set a default
-    }
-    return result;
+    return f_origin;
   }
 
   public boolean isAbductivelyInferred() {
@@ -123,11 +136,11 @@ public final class IRFreeProposedPromiseDrop extends IRFreeDrop implements IProp
   }
 
   public String getTargetProjectName() {
-    return getEntity().getAttribute(TARGET_PROJECT);
+    return f_targetProjectName;
   }
 
   public String getFromProjectName() {
-    return getEntity().getAttribute(FROM_PROJECT);
+    return f_fromProjectName;
   }
 
   public ISrcRef getAssumptionRef() {
