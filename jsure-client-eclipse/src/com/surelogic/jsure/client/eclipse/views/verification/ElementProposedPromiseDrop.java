f9c1cdb954d4e2166be708d35648a682a681732e
Tim Halloran <tim.halloran@surelogic.com>
2012-09-25 12:14:13 -0400

    New class
    Former-commit-id: ec10f20b862dd2d081112569b3128f0ef10fe68a

---
+++ jsure-client-eclipse/src/com/surelogic/jsure/client/eclipse/views/verification/ElementProposedPromiseDrop.java
@@ -0,0 +1,48 @@
+package com.surelogic.jsure.client.eclipse.views.verification;
+
+import com.surelogic.NonNull;
+import com.surelogic.Nullable;
+import com.surelogic.common.CommonImages;
+import com.surelogic.common.i18n.I18N;
+import com.surelogic.dropsea.IProposedPromiseDrop;
+
+final class ElementProposedPromiseDrop extends ElementDrop {
+
+  protected ElementProposedPromiseDrop(Element parent, IProposedPromiseDrop proposedPromiseDrop) {
+    super(parent);
+    if (proposedPromiseDrop == null)
+      throw new IllegalArgumentException(I18N.err(44, "proposedPromiseDrop"));
+    f_proposedPromiseDrop = proposedPromiseDrop;
+  }
+
+  private final IProposedPromiseDrop f_proposedPromiseDrop;
+
+  @Override
+  @NonNull
+  IProposedPromiseDrop getDrop() {
+    return f_proposedPromiseDrop;
+  }
+
+  @Override
+  int getImageFlags() {
+    return 0;
+  }
+
+  @Override
+  @Nullable
+  String getImageName() {
+    return CommonImages.IMG_ANNOTATION_ABDUCTIVE;
+  }
+
+  @Override
+  @NonNull
+  Element[] constructChildren() {
+    return EMPTY;
+  }
+
+  @Override
+  public int compareTo(Element o) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+}
