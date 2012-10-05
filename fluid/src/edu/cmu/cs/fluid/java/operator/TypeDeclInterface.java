c4c05c9d2fbd69cbfedbc9610ec9d3c696fd29b7
Edwin Chan <edwin.chan@surelogic.com>
2012-10-05 16:42:58 -0400

    Added getKind()
    Former-commit-id: 39e465f79b652b74c511d7615e3569835e411f32

--- fluid/src/edu/cmu/cs/fluid/java/operator/TypeDeclInterface.java
+++ fluid/src/edu/cmu/cs/fluid/java/operator/TypeDeclInterface.java
@@ -1,9 +1,12 @@
 // $Header: /cvs/fluid/fluid/src/edu/cmu/cs/fluid/java/operator/TypeDeclInterface.java,v 1.5 2007/09/20 13:56:42 chance Exp $
 package edu.cmu.cs.fluid.java.operator;
 
+import com.surelogic.common.ref.IDecl;
+
 import edu.cmu.cs.fluid.ir.IRNode;
 import edu.cmu.cs.fluid.java.promise.IHasReceiverDecl;
  
 public interface TypeDeclInterface extends ReferenceTypeInterface, IHasReceiverDecl { 
   IRNode get_Body(IRNode n);
+  IDecl.Kind getKind();
 }
