d378f290000cc55f1f11f0cb2747c5894b106ddd
Tim Halloran <tim.halloran@surelogic.com>
2012-10-09 14:10:37 -0400

    Code to clear out unconnected results
    Former-commit-id: e1afbf4040fc718a55ef8a27303f6157858f1a79

--- fluid/src/com/surelogic/dropsea/ir/AbstractSeaConsistencyProofHook.java
+++ fluid/src/com/surelogic/dropsea/ir/AbstractSeaConsistencyProofHook.java
@@ -7,12 +7,10 @@ package com.surelogic.dropsea.ir;
  */
 public abstract class AbstractSeaConsistencyProofHook implements SeaConsistencyProofHook {
 
-//  @Override
   public void preConsistencyProof(Sea sea) {
     // by default do nothing
   }
 
-//  @Override
   public void postConsistencyProof(Sea sea) {
     // by default do nothing
   }
--- fluid/src/com/surelogic/dropsea/ir/drops/ClearOutUnconnectedResultsProofHook.java
+++ fluid/src/com/surelogic/dropsea/ir/drops/ClearOutUnconnectedResultsProofHook.java
@@ -1,7 +1,21 @@
 package com.surelogic.dropsea.ir.drops;
 
+import com.surelogic.dropsea.IDrop;
 import com.surelogic.dropsea.ir.AbstractSeaConsistencyProofHook;
+import com.surelogic.dropsea.ir.AnalysisResultDrop;
+import com.surelogic.dropsea.ir.DropPredicate;
+import com.surelogic.dropsea.ir.Sea;
 
 public class ClearOutUnconnectedResultsProofHook extends AbstractSeaConsistencyProofHook {
 
+  @Override
+  public void preConsistencyProof(Sea sea) {
+    sea.invalidateMatching(new DropPredicate() {
+      public boolean match(IDrop d) {
+        if (d instanceof AnalysisResultDrop)
+          return !((AnalysisResultDrop) d).checksAPromise();
+        return false;
+      }
+    });
+  }
 }
