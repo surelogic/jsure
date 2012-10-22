5d94295c443cb930327498aa25e1409eeca8c2c7
Edwin Chan <edwin.chan@surelogic.com>
2012-10-22 14:42:56 -0400

    Added constants for various kinds of offsets
    Former-commit-id: 98f7a25a77dbc601da6803c6de46fe8bc15d8f07

--- fluid/src/com/surelogic/dropsea/IDiffInfo.java
+++ fluid/src/com/surelogic/dropsea/IDiffInfo.java
@@ -14,7 +14,19 @@ public interface IDiffInfo {
 
   public static final String FAST_TREE_HASH = "fAST-tree-hash";
   public static final String FAST_CONTEXT_HASH = "fAST-context-hash";
+  /**
+   * Offset from the first statement/expression/decl in the enclosing declaration
+   * (unless it's a parameter, in which case it's from the start of the enclosing
+   * method)
+   */
+  public static final String DECL_RELATIVE_OFFSET = "decl-relative-offset";
+  /**
+   * Offset from the end of the text representing the enclosing declaration
+   */
+  public static final String DECL_END_RELATIVE_OFFSET = "decl-end-relative-offset";
 
+  public static final int UNKNOWN = -1;
+  
   /*
    * Interface to key/value pairs.
    */
