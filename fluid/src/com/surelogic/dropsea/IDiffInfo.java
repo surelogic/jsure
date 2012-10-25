f7849abc0a070f34805b5bd4fe6c6add85d07206
Tim Halloran <tim.halloran@surelogic.com>
2012-10-25 17:20:21 -0400

    Fixed Javadoc
    Former-commit-id: c59d0a1f0d49a3b76c0b12f0261abf13686e699a

--- fluid/src/com/surelogic/dropsea/IDiffInfo.java
+++ fluid/src/com/surelogic/dropsea/IDiffInfo.java
@@ -26,9 +26,12 @@ public interface IDiffInfo {
   public static final String DECL_END_RELATIVE_OFFSET = "decl-end-relative-offset";
 
   /**
-   * Another string sent from the analysis, very rarely, to indicated that even
-   * though everything else matches, the drop might not really be the
-   * same&mdash;this value also needs to be compared.
+   * A string sent from the analysis, very rarely, to indicate that even though
+   * everything else matches the drop might not really be the same&mdash;this
+   * value also needs to be compared.
+   * <p>
+   * Use of this is rare and typically only for analysis result drops, however,
+   * it can be used on any drop at all.
    */
   public static final String ANALYSIS_DIFF_HINT = "analysis-diff-hint";
 
