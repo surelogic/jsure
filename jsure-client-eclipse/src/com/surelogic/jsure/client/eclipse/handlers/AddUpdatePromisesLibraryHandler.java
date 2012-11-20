9bb0c28d33cba0baac02a17af62d77c86fd5e845
Tim Halloran <tim.halloran@surelogic.com>
2012-11-20 10:10:01 -0500

    Fixes to actions and handlers for UI
    Former-commit-id: 5e3b9eff46a057a89df6ead26b257f69655c1769

--- jsure-client-eclipse/src/com/surelogic/jsure/client/eclipse/handlers/AddUpdatePromisesLibraryHandler.java
+++ jsure-client-eclipse/src/com/surelogic/jsure/client/eclipse/handlers/AddUpdatePromisesLibraryHandler.java
@@ -19,42 +19,32 @@ import com.surelogic.common.ui.jobs.SLUIJob;
 import com.surelogic.jsure.client.eclipse.PromisesJarUtility;
 import com.surelogic.jsure.core.preferences.JSurePreferencesUtility;
 
-public class AddUpdatePromisesLibraryHandler extends
-		AbstractProjectSelectedMenuHandler implements IHandler {
+public class AddUpdatePromisesLibraryHandler extends AbstractProjectSelectedMenuHandler implements IHandler {
 
-	@Override
-	public void runActionOn(List<IJavaProject> selectedProjects) {
-		/*
-		 * License check: A hack because JSure is not using SLJobs yet.
-		 */
-		final SLStatus failed = SLLicenseUtility.validateSLJob(
-				SLLicenseProduct.JSURE, new NullSLProgressMonitor());
-		if (failed != null) {
-			SLLogger.getLogger().log(failed.getSeverity().toLevel(),
-					failed.getMessage(), failed.getException());
-			return;
-		}
+  @Override
+  public void runActionOn(List<IJavaProject> selectedProjects) {
+    /*
+     * License check: A hack because JSure is not using SLJobs yet.
+     */
+    final SLStatus failed = SLLicenseUtility.validateSLJob(SLLicenseProduct.JSURE, new NullSLProgressMonitor());
+    if (failed != null) {
+      SLLogger.getLogger().log(failed.getSeverity().toLevel(), failed.getMessage(), failed.getException());
+      return;
+    }
 
-		for (IJavaProject jp : selectedProjects) {
-			System.out
-					.println("scheduled job for " + jp.getProject().getName());
-			SLUIJob job = PromisesJarUtility
-					.getAddUpdatePromisesLibraryUIJob(jp);
-			job.schedule();
-		}
-	}
+    for (IJavaProject jp : selectedProjects) {
+      System.out.println("scheduled job for " + jp.getProject().getName());
+      SLUIJob job = PromisesJarUtility.getAddUpdatePromisesLibraryUIJob(jp);
+      job.schedule();
+    }
+  }
 
-	@Override
-	protected JavaProjectSelectionDialog.Configuration getDialogInfo(
-			List<IJavaProject> selectedProjects) {
-		return new JavaProjectSelectionDialog.Configuration(
-				"Select project(s) to add or update the "
-						+ LibResources.PROMISES_JAR
-						+ " within so that annotations\ncan be added to the project's code:",
-				"Add/Update Promises Library",
-				SLImages.getImage(CommonImages.IMG_JSURE_VERIFY),
-				selectedProjects,
-				JSurePreferencesUtility.ALWAYS_ALLOW_USER_TO_SELECT_PROJECTS_TO_UPDATE_JAR,
-				JSurePreferencesUtility.LAST_TIME_PROJECTS_TO_UPDATE_JAR);
-	}
+  @Override
+  protected JavaProjectSelectionDialog.Configuration getDialogInfo(List<IJavaProject> selectedProjects) {
+    return new JavaProjectSelectionDialog.Configuration("Select project(s) to add or update the " + LibResources.PROMISES_JAR
+        + " within so that annotations\ncan be added to the project's code:", "Add/Update Promises Library",
+        SLImages.getImage(CommonImages.IMG_JAR), selectedProjects,
+        JSurePreferencesUtility.ALWAYS_ALLOW_USER_TO_SELECT_PROJECTS_TO_UPDATE_JAR,
+        JSurePreferencesUtility.LAST_TIME_PROJECTS_TO_UPDATE_JAR);
+  }
 }
