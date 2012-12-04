e4c23b837db3827df960277c1b07be301ccf63d9
Edwin Chan <edwin.chan@surelogic.com>
2012-12-04 10:46:43 -0500

    Changed to cache the set of granules for a comp unit
    Former-commit-id: e9ace0ee674042e6808a755605281eb0a507b4cc

--- fluid/src/edu/cmu/cs/fluid/java/bind/UnversionedJavaBinder.java
+++ fluid/src/edu/cmu/cs/fluid/java/bind/UnversionedJavaBinder.java
@@ -7,9 +7,7 @@ import java.util.logging.Level;
 
 import org.apache.commons.collections15.MultiMap;
 
-import com.surelogic.InRegion;
-import com.surelogic.RequiresLock;
-import com.surelogic.ThreadSafe;
+import com.surelogic.*;
 import com.surelogic.analysis.ConcurrentAnalysis;
 import com.surelogic.common.concurrent.ConcurrentHashSet;
 import com.surelogic.common.concurrent.ConcurrentMultiHashMap;
@@ -61,6 +59,11 @@ public class UnversionedJavaBinder extends AbstractJavaBinder implements ICompUn
 		  return JavaMemberTable.makeBatchTable(typeEnvironment.getObjectType());
 	  }
   };
+  
+  /**
+   * Shared across binders
+   */
+  private static final ConcurrentMap<IRNode, List<IRNode>> granules = new ConcurrentHashMap<IRNode, List<IRNode>>();
   
   public UnversionedJavaBinder(final ITypeEnvironment tEnv) {
     super(tEnv);
@@ -177,20 +180,36 @@ public class UnversionedJavaBinder extends AbstractJavaBinder implements ICompUn
   protected void reset() {
 	  clearAll(true);
   }
+
+  private static Iterable<IRNode> getGranules(final IRNode cu) {
+	  List<IRNode> rv = granules.get(cu);
+	  if (rv == null) {
+		  rv = new ArrayList<IRNode>();
+		  for (IRNode n : JJNode.tree.topDown(cu)) {
+			  final Operator op = JJNode.tree.getOperator(n);
+			  if (AbstractJavaBinder.isGranule(n, op)) {
+				  rv.add(n);
+			  }
+		  }
+		  if (rv.isEmpty()) {
+			  rv = Collections.emptyList();
+		  }
+		  // TODO sync?
+		  granules.put(cu, rv);
+	  }
+	  return rv;
+  }
   
   public void bindCompUnit(final IRNode cu, final String name) {
-      for (IRNode n : JJNode.tree.topDown(cu)) {
-          final Operator op = JJNode.tree.getOperator(n);
-          if (AbstractJavaBinder.isGranule(n, op)) {
-            try {
-              ensureBindingsOK(n);
-            } catch (RuntimeException e) {
+	  for (IRNode n : getGranules(cu)) {
+		  try {
+			  ensureBindingsOK(n);
+		  } catch (RuntimeException e) {
               SLLogger.getLogger().log(Level.SEVERE,
                   "Error while binding " + DebugUnparser.toString(n) + " in " + name, e);
               throw e;
-            }
-          }
-        }
+		  }
+	  }
   }
   
   /**
@@ -198,11 +217,7 @@ public class UnversionedJavaBinder extends AbstractJavaBinder implements ICompUn
    */
   public synchronized void astChanged(IRNode cu) {
     // If so, we need to clear all the cached data
-    for(final IRNode n : JJNode.tree.topDown(cu)) {
-      final Operator op = JJNode.tree.getOperator(n);
-      if (!isGranule(n, op)) {
-        continue;
-      }
+	for (IRNode n : getGranules(cu)) {
       IGranuleBindings b1 = allGranuleBindings.remove(n);
       IGranuleBindings b2 = partialGranuleBindings.remove(n);
       boolean changed = false;
@@ -229,6 +244,7 @@ public class UnversionedJavaBinder extends AbstractJavaBinder implements ICompUn
       }
       */
     }
+	granules.remove(cu);
     JavaTypeFactory.clearCaches();
   }
 
