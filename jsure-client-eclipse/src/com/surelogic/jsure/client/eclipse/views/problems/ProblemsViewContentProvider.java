c485924f1b323db6d0bfeff2654648ce2c49e4e0
Tim Halloran <tim.halloran@surelogic.com>
2012-12-20 12:34:01 -0500

    Bug fix to | vs ||
    Former-commit-id: 16373dcc0cf75225bf0214f820af3d85d429f388

--- jsure-client-eclipse/src/com/surelogic/jsure/client/eclipse/views/problems/ProblemsViewContentProvider.java
+++ jsure-client-eclipse/src/com/surelogic/jsure/client/eclipse/views/problems/ProblemsViewContentProvider.java
@@ -59,7 +59,7 @@ public class ProblemsViewContentProvider implements ITreeContentProvider, IViewD
   private Element[] f_root = null;
 
   boolean isEmpty() {
-    return f_root == null | f_root.length == 0;
+    return f_root == null || f_root.length == 0;
   }
 
   void changeContentsToCurrentScan(@NonNull final JSureScanInfo scan, @Nullable final ScanDifferences diff,
