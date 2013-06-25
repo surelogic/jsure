45b5a8ea8dc6d4b8ddf0dfdf328ede0bfba155bf
edwin <edwin.chan@surelogic.com>
2013-06-25 16:44:13 -0400

    Fixed computeAlternateDeclForUnparse() to handle enum constants properly
    Former-commit-id: 2aee23999123392c2b0566e8edcc05364f40003f

--- fluid/src/com/surelogic/dropsea/ir/drops/BooleanPromiseDrop.java
+++ fluid/src/com/surelogic/dropsea/ir/drops/BooleanPromiseDrop.java
@@ -5,8 +5,10 @@ import com.surelogic.aast.IAASTRootNode;
 import com.surelogic.dropsea.ir.PromiseDrop;
 
 import edu.cmu.cs.fluid.ir.IRNode;
-import edu.cmu.cs.fluid.java.operator.VariableDeclarator;
+import edu.cmu.cs.fluid.java.operator.*;
 import edu.cmu.cs.fluid.java.util.VisitUtil;
+import edu.cmu.cs.fluid.parse.JJNode;
+import edu.cmu.cs.fluid.tree.Operator;
 
 
 public class BooleanPromiseDrop<A extends IAASTRootNode> extends PromiseDrop<A> {
@@ -15,7 +17,9 @@ public class BooleanPromiseDrop<A extends IAASTRootNode> extends PromiseDrop<A> 
   }
   
   public static IRNode computeAlternateDeclForUnparse(final IRNode node) {
-	  if (VariableDeclarator.prototype.includes(node)) {
+	  final Operator op = JJNode.tree.getOperator(node);
+	  // Not VariableDeclaration, since it includes ParameterDeclaration
+	  if (VariableDeclarator.prototype.includes(op) || EnumConstantDeclaration.prototype.includes(op)) {
 		  return null;
 	  }
 	  IRNode rv = VisitUtil.getEnclosingClassBodyDecl(node);
