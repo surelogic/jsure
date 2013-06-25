229af3460317e690b4796af83abca26812f778ad
edwin <edwin.chan@surelogic.com>
2013-06-25 16:43:35 -0400

    Fixed message computation to deal properly with 'this', 'return', and parameters
    Former-commit-id: e1b989709feadf81d1dc39abb500a8be6fc72f78

--- fluid/src/com/surelogic/dropsea/ir/drops/VouchPromiseDrop.java
+++ fluid/src/com/surelogic/dropsea/ir/drops/VouchPromiseDrop.java
@@ -3,6 +3,7 @@ package com.surelogic.dropsea.ir.drops;
 import com.surelogic.aast.promise.VouchSpecificationNode;
 import com.surelogic.dropsea.ir.PromiseDrop;
 
+import edu.cmu.cs.fluid.ir.IRNode;
 import edu.cmu.cs.fluid.java.JavaGlobals;
 
 /**
@@ -12,5 +13,10 @@ public class VouchPromiseDrop extends PromiseDrop<VouchSpecificationNode> {
   public VouchPromiseDrop(VouchSpecificationNode a) {
     super(a);
     setCategorizingMessage(JavaGlobals.VOUCH_CAT);
+  }
+
+  @Override
+  protected IRNode useAlternateDeclForUnparse() {
+	  return BooleanPromiseDrop.computeAlternateDeclForUnparse(getNode());
   }
 }
