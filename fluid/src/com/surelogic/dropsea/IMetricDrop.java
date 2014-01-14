9f63f9896b0b5fcb8bb158eeb2dc33a16ac592e5
Tim Halloran <tim.halloran@surelogic.com>
2014-01-14 16:29:24 -0500

    Removed duration -- no need, the creating IR node creates a JavaRef we can use.
    Former-commit-id: e4d1d93755dd8b4ec2bcbb6e0bb38ef54a1b06ce

--- fluid/src/com/surelogic/dropsea/IMetricDrop.java
+++ fluid/src/com/surelogic/dropsea/IMetricDrop.java
@@ -40,21 +40,6 @@ public interface IMetricDrop extends IDrop, ISnapshotDrop {
    */
   final String SCAN_TIME_DURATION_NS = "scan-time-duration-ns";
 
-  /**
-   * The static scope Java declaration that this scan time report is about. For
-   * example, a compilation unit, a type, a method, or a field. Normally this
-   * value is a compilation unit or a Java declaration.
-   * <p>
-   * The scope for a particular compilation unit and analysis should be
-   * consistent. All reports should be at the same level, e.g., method or
-   * compilation.
-   * <p>
-   * Values are always {@link IJavaRef}.
-   * <p>
-   * <b>MANDITORY</b> This value should be in each scan time report.
-   */
-  final String SCAN_TIME_SCOPE = "scan-time-scope";
-
   /*
    * SLOC
    */
