45f1d83990bc5bdb50399dab39a1610797efc6b1
edwin <edwin.chan@surelogic.com>
2014-01-22 17:15:23 -0500

    Added code to catch NPE from clearing caches
    Former-commit-id: 472a53c02fe757da98faba735f6e3144f92e3d74

--- fluid/src/com/surelogic/analysis/AbstractWholeIRAnalysis.java
+++ fluid/src/com/surelogic/analysis/AbstractWholeIRAnalysis.java
@@ -1,6 +1,7 @@
 /*$Header: /cvs/fluid/fluid/src/com/surelogic/analysis/AbstractWholeIRAnalysis.java,v 1.5 2008/09/08 17:43:38 chance Exp $*/
 package com.surelogic.analysis;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.surelogic.common.logging.SLLogger;
@@ -28,9 +29,13 @@ public abstract class AbstractWholeIRAnalysis<T extends IBinderClient, Q extends
 
   @Override
   public void postAnalysis(IIRProject p) {
-    clearCaches();
-    CachedSet.clearCache();
-    LabelList.clearCache();
+	try {
+		clearCaches();
+		CachedSet.clearCache();
+		LabelList.clearCache();
+	} catch(NullPointerException e) {
+		LOG.log(Level.INFO, "Ignoring NPE", e);
+	}
   }
 
   protected abstract void clearCaches();
