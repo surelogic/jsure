4c1f6f379aab4750ff333093d1d28b689b29f950
Tim Halloran <tim.halloran@surelogic.com>
2014-02-13 16:04:29 -0500

    Fixed the IRFree scoped promise drop to implement IScopedPromiseDrop
    Former-commit-id: f06e543a6d9e167408adaf6a887158f282328ef7

--- fluid/src/com/surelogic/dropsea/irfree/drops/IRFreeScopedPromiseDrop.java
+++ fluid/src/com/surelogic/dropsea/irfree/drops/IRFreeScopedPromiseDrop.java
@@ -1,8 +1,9 @@
 package com.surelogic.dropsea.irfree.drops;
 
 import com.surelogic.common.xml.Entity;
+import com.surelogic.dropsea.IScopedPromiseDrop;
 
-public final class IRFreeScopedPromiseDrop extends IRFreePromiseDrop {
+public final class IRFreeScopedPromiseDrop extends IRFreePromiseDrop implements IScopedPromiseDrop {
 
   IRFreeScopedPromiseDrop(Entity e, Class<?> irClass) {
     super(e, irClass);
