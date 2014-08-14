404d3ea1e1d953969208d1f0757d047d0628e8e3
Edwin <edwin.chan@surelogic.com>
2014-08-14 17:29:45 -0400

    Added code to handle scan cancellation
    Former-commit-id: 409514b0ff0baa5c76f1a1746b61b7a935bc7936

--- jsure-client-eclipse/src/com/surelogic/jsure/client/eclipse/EclipseScanCrashReporter.java
+++ jsure-client-eclipse/src/com/surelogic/jsure/client/eclipse/EclipseScanCrashReporter.java
@@ -5,6 +5,7 @@ import java.io.File;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.progress.UIJob;
 
 import com.surelogic.Singleton;
@@ -13,6 +14,7 @@ import com.surelogic.common.core.EclipseUtility;
 import com.surelogic.common.jobs.SLStatus;
 import com.surelogic.common.license.SLLicenseProduct;
 import com.surelogic.common.serviceability.scan.IScanCrashReporter;
+import com.surelogic.common.ui.EclipseUIUtility;
 import com.surelogic.common.ui.jobs.SLUIJob;
 import com.surelogic.common.ui.serviceability.SendServiceMessageWizard;
 
@@ -48,6 +50,18 @@ public final class EclipseScanCrashReporter implements IScanCrashReporter {
 								+ " "
 								+ EclipseUtility.getVersion(Activator
 										.getDefault()), status, scanLog);
+				return Status.OK_STATUS;
+			}
+		};
+		job.schedule();
+	}
+
+	@Override
+	public void reportScanCancellation(final String msg) {
+		final UIJob job = new SLUIJob() {
+			@Override
+			public IStatus runInUIThread(IProgressMonitor monitor) {
+				MessageDialog.openInformation(EclipseUIUtility.getShell(), "JSure Scan was Cancelled", msg);
 				return Status.OK_STATUS;
 			}
 		};
