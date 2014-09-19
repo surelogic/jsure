05a148b29e6ffc2388d773df84a36bf56fd5a58c
edwin <edwin.chan@surelogic.com>
2014-09-19 15:18:44 -0400

    Updated metrics to consider the annotations on the enclosing type
    Former-commit-id: 106364e045fe118e511d9e8eed17dec53955e35d

--- fluid/src/com/surelogic/analysis/concurrency/driver/ConcurrentStateMetrics.java
+++ fluid/src/com/surelogic/analysis/concurrency/driver/ConcurrentStateMetrics.java
@@ -9,6 +9,7 @@ import com.surelogic.dropsea.KeyValueUtility;
 import com.surelogic.dropsea.ir.MetricDrop;
 import com.surelogic.dropsea.ir.drops.RegionModel;
 import com.surelogic.dropsea.ir.drops.VouchFieldIsPromiseDrop;
+import com.surelogic.dropsea.ir.drops.type.constraints.*;
 
 import edu.cmu.cs.fluid.ir.IRNode;
 import edu.cmu.cs.fluid.java.JavaNode;
@@ -21,7 +22,6 @@ import edu.cmu.cs.fluid.java.bind.IJavaType;
 import edu.cmu.cs.fluid.java.operator.FieldDeclaration;
 import edu.cmu.cs.fluid.java.operator.VariableDeclarators;
 import edu.cmu.cs.fluid.java.util.VisitUtil;
-import edu.cmu.cs.fluid.parse.JJNode;
 
 /**
  * Scans classes to compute metrics about what kinds of fields we have in each class
@@ -55,80 +55,143 @@ public class ConcurrentStateMetrics {
 		counts.recordAsDrop();
 	}
 
+	enum PartStatus { NO_POLICY, NOT_THREADSAFE, THREADSAFE, IMMUTABLE }
+	
 	class FieldCounts {
 		final IJavaDeclaredType clazz;
 		final LockUtils lockUtils;
+		final PartStatus instancePart, staticPart;
+		
 		int threadSafe = 0, immutable = 0, locked = 0, threadConfined = 0, other = 0;
+		int notThreadSafe = 0, finalFields = 0;
 
 		FieldCounts(IRNode t, LockUtils utils) {
 			clazz = (IJavaDeclaredType) binder.getTypeEnvironment().convertNodeTypeToIJavaType(t);
 			lockUtils = utils;
-		}
-
+			
+			// Figure out what annotations are on the type decl
+			PartStatus iPart = PartStatus.NO_POLICY, sPart = PartStatus.NO_POLICY;
+			final ImmutablePromiseDrop immutable = LockRules.getImmutableType(t);
+			switch (immutable.getAppliesTo()) {
+			case InstanceAndStatic:
+				sPart = PartStatus.IMMUTABLE;				
+			case Instance:
+				iPart = PartStatus.IMMUTABLE;
+				break;
+			case Static:
+				sPart = PartStatus.IMMUTABLE;
+				break;
+			}
+			final ThreadSafePromiseDrop threadSafe = LockRules.getThreadSafeType(t);
+			switch (threadSafe.getAppliesTo()) {
+			case InstanceAndStatic:
+				sPart = PartStatus.THREADSAFE;				
+			case Instance:
+				iPart = PartStatus.THREADSAFE;
+				break;
+			case Static:
+				sPart = PartStatus.THREADSAFE;
+				break;
+			}
+			NotThreadSafePromiseDrop notThreadSafe = LockRules.getNotThreadSafe(t);
+			switch (notThreadSafe.getAppliesTo()) {
+			case InstanceAndStatic:
+				sPart = PartStatus.NOT_THREADSAFE;				
+			case Instance:
+				iPart = PartStatus.NOT_THREADSAFE;
+				break;
+			case Static:
+				sPart = PartStatus.NOT_THREADSAFE;
+				break;
+			}
+			instancePart = iPart;
+			staticPart = sPart;
+		}	
+		
 		void incrForArray(IRNode field) {
-			handleEachDecl(field, false);
+			handleEachDecl(field, false, PartStatus.NO_POLICY);
 		}
 
 		void incrForRef(final IRNode field, final IJavaSourceRefType st) {
 			if (LockRules.isImmutableType(st.getDeclaration())) {
-				immutable += numDecls(field);
+				handleEachDecl(field, false, PartStatus.IMMUTABLE);
 			}
 			else if (LockRules.isThreadSafeType(st.getDeclaration())) {
-				threadSafe += numDecls(field);
+				handleEachDecl(field, false, PartStatus.THREADSAFE);
 			}
-			else handleEachDecl(field, false);
-		}
+			else handleEachDecl(field, false, PartStatus.NO_POLICY);
+		} 
 
-		void incrForPrim(final IRNode field) {
-			if (JavaNode.getModifier(field, JavaNode.FINAL)) {					
-				immutable += numDecls(field);
-			} else {
-				handleEachDecl(field, true);
-			}
+		void incrForPrim(final IRNode field) {				
+			handleEachDecl(field, true, PartStatus.NO_POLICY);
 		}			
-				
-		private int numDecls(IRNode field) {
-			IRNode decls = FieldDeclaration.getVars(field);
-			return JJNode.tree.numChildren(decls);
-		}
 		
-		private void handleEachDecl(final IRNode field, final boolean isPrim) {
+		private void handleEachDecl(final IRNode field, final boolean isPrim, final PartStatus annoOnType) {
+			final boolean isStatic = JavaNode.getModifier(field, JavaNode.STATIC);
+			final PartStatus annoOnEnclosingType = isStatic ? staticPart : instancePart;
+			
+			final boolean isFinal = JavaNode.getModifier(field, JavaNode.FINAL);
 			final IRNode decls = FieldDeclaration.getVars(field);
 			for(final IRNode vd : VariableDeclarators.getVarIterator(decls)) {
-				VouchFieldIsPromiseDrop vouch = LockRules.getVouchFieldIs(vd);
-				if (vouch != null) {
-					if (isPrim && vouch.isFinal() || vouch.isImmutable()) {
-						immutable++;
-						continue;
-					}
-					else if (vouch.isThreadSafe()) {
-						threadSafe++;
-						continue;
-					}
-				}
 				IRegion region = RegionModel.getInstance(vd);
 				if (lockUtils.getLockForRegion(clazz, region) != null) {
 					locked++;
+					continue;
 				}
 				else if (LockRules.getThreadConfinedDrop(vd) != null) {
 					threadConfined++;
-				}			
-				else {
-					other++;
+					continue;
+				}	
+				
+				PartStatus annoForField = annoOnType;
+				boolean fieldIsFinal = isFinal;
+				VouchFieldIsPromiseDrop vouch = LockRules.getVouchFieldIs(vd);
+				if (vouch != null) {
+					if (vouch.isFinal()) {
+						fieldIsFinal = true;
+					}
+					else if (vouch.isImmutable()) {
+						annoForField = PartStatus.IMMUTABLE;
+					}
+					else if (vouch.isThreadSafe() && annoOnType != PartStatus.IMMUTABLE) {
+						annoForField = PartStatus.THREADSAFE;
+					}
+				}
+				if (fieldIsFinal && (isPrim || annoForField == PartStatus.IMMUTABLE)) {
+					finalFields++;
+				} else {
+					PartStatus anno = (annoOnEnclosingType.ordinal() > annoForField.ordinal()) ? annoOnEnclosingType : annoForField;
+					switch (anno) {
+					case IMMUTABLE:
+						immutable++;
+						break;
+					case THREADSAFE:
+						threadSafe++;
+						break;
+					case NOT_THREADSAFE:
+						notThreadSafe++;
+						break;
+					case NO_POLICY:
+					default:
+						other++;
+					}				
 				}
 			}
 		}
 		
 		void recordAsDrop() {
-			if (immutable + threadSafe + locked + threadConfined + other == 0) {
+			if (immutable + threadSafe + locked + threadConfined + other + notThreadSafe + finalFields== 0) {
 				return;
 			}
 			final MetricDrop d = new MetricDrop(clazz.getDeclaration(), IMetricDrop.Metric.STATE_WRT_CONCURRENCY);			
 			if (immutable > 0) {
-				addMetric(d, IMetricDrop.CONCURR_IMMUTABLE_COUNT, immutable);
+				addMetric(d, IMetricDrop.CONCURR_IMMUTABLE_COUNT, immutable + finalFields);
 			}
 			if (threadSafe > 0) {
 				addMetric(d, IMetricDrop.CONCURR_THREADSAFE_COUNT, threadSafe);
+			}
+			if (notThreadSafe > 0) {
+				addMetric(d, IMetricDrop.CONCURR_NOTTHREADSAFE_COUNT, notThreadSafe);
 			}
 			if (locked > 0) {
 				addMetric(d, IMetricDrop.CONCURR_LOCK_PROTECTED_COUNT, locked);
