7c8920d3e6956283ca927c9795f2cb9c0ef64a48
tim <hallorant@gmail.com>
2014-09-22 16:45:59 -0400

    Constrained visibility to uses.
    Former-commit-id: 5292029789c500a3f0414486b8e5b09a8174663e

--- jsure-client-eclipse/src/com/surelogic/jsure/client/eclipse/views/source/HistoricalSourceView.java
+++ jsure-client-eclipse/src/com/surelogic/jsure/client/eclipse/views/source/HistoricalSourceView.java
@@ -1,8 +1,6 @@
 package com.surelogic.jsure.client.eclipse.views.source;
 
 import java.io.File;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Collections;
 
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -19,126 +17,89 @@ import com.surelogic.common.ref.IDecl;
 import com.surelogic.common.ref.IJavaRef;
 import com.surelogic.common.ui.jobs.SLUIJob;
 import com.surelogic.common.ui.views.AbstractHistoricalSourceView;
-import com.surelogic.common.java.*;
-import com.surelogic.javac.Projects;
 import com.surelogic.javac.persistence.JSureScan;
 import com.surelogic.javac.persistence.JSureScanInfo;
 import com.surelogic.jsure.core.scans.JSureDataDirHub;
 
-public class HistoricalSourceView extends AbstractHistoricalSourceView
-		implements JSureDataDirHub.CurrentScanChangeListener {
+public class HistoricalSourceView extends AbstractHistoricalSourceView implements JSureDataDirHub.CurrentScanChangeListener {
 
-	@Override
-	public void createPartControl(Composite parent) {
-		super.createPartControl(parent);
+  @Override
+  public void createPartControl(Composite parent) {
+    super.createPartControl(parent);
 
-		final JSureScan scan = JSureDataDirHub.getInstance().getCurrentScan();
-		if (scan != null) {
-			setSourceSnapshotTime(scan.getTimeOfScan());
-		}
-		JSureDataDirHub.getInstance().addCurrentScanChangeListener(this);
-	}
+    final JSureScan scan = JSureDataDirHub.getInstance().getCurrentScan();
+    if (scan != null) {
+      setSourceSnapshotTime(scan.getTimeOfScan());
+    }
+    JSureDataDirHub.getInstance().addCurrentScanChangeListener(this);
+  }
 
-	@Override
-	public void dispose() {
-		try {
-			JSureDataDirHub.getInstance().removeCurrentScanChangeListener(this);
-		} finally {
-			super.dispose();
-		}
-	}
+  @Override
+  public void dispose() {
+    try {
+      JSureDataDirHub.getInstance().removeCurrentScanChangeListener(this);
+    } finally {
+      super.dispose();
+    }
+  }
 
-	@Override
-	public void currentScanChanged(final JSureScan scan) {
-		final UIJob job = new SLUIJob() {
-			@Override
-			public IStatus runInUIThread(IProgressMonitor monitor) {
-				clearSourceCodeFromView();
-				if (scan != null) {
-					setSourceSnapshotTime(scan.getTimeOfScan());
-				}
-				return Status.OK_STATUS;
-			}
-		};
-		job.setSystem(true);
-		job.schedule();
-	}
+  @Override
+  public void currentScanChanged(final JSureScan scan) {
+    final UIJob job = new SLUIJob() {
+      @Override
+      public IStatus runInUIThread(IProgressMonitor monitor) {
+        clearSourceCodeFromView();
+        if (scan != null) {
+          setSourceSnapshotTime(scan.getTimeOfScan());
+        }
+        return Status.OK_STATUS;
+      }
+    };
+    job.setSystem(true);
+    job.schedule();
+  }
 
-	// Run in UI?
-	@Override
-	@NonNull
-	protected ISourceZipFileHandles findSources(String run) {
-		final JSureScanInfo info = OLD.equals(run) ? 
-				JSureDataDirHub.getInstance().getLastMatchingScanInfo() : 
-				JSureDataDirHub.getInstance().getCurrentScanInfo();
-		final ISourceZipFileHandles zips;
-		if (info == null) {
-			zips = new ISourceZipFileHandles() {
-				@Override
+  // Run in UI?
+  @Override
+  @NonNull
+  protected ISourceZipFileHandles findSources(String run) {
+    final JSureScanInfo info = OLD.equals(run) ? JSureDataDirHub.getInstance().getLastMatchingScanInfo() : JSureDataDirHub
+        .getInstance().getCurrentScanInfo();
+    final ISourceZipFileHandles zips;
+    if (info == null) {
+      zips = new ISourceZipFileHandles() {
+        @Override
         public Iterable<File> getSourceZips() {
-					return Collections.emptyList();
-				}
-			};
-		} else {
-			setSourceSnapshotTime(info.getJSureRun().getTimeOfScan());
-			zips = new ISourceZipFileHandles() {
-				@Override
+          return Collections.emptyList();
+        }
+      };
+    } else {
+      setSourceSnapshotTime(info.getJSureRun().getTimeOfScan());
+      zips = new ISourceZipFileHandles() {
+        @Override
         public Iterable<File> getSourceZips() {
-					return info.getJSureRun().getSourceZips();
-				}
-			};
-		}
-		return zips;
-	}
-	
-	public static void tryToOpenInEditor(final IJavaRef javaRef, final boolean tryToUseOld) {
-	  if (javaRef == null) return;
-	  tryToOpenInEditor(javaRef.getPackageName(),
-        DeclUtil.getTypeNameDollarSignOrNull(javaRef.getDeclaration()), 
+          return info.getJSureRun().getSourceZips();
+        }
+      };
+    }
+    return zips;
+  }
+
+  public static void tryToOpenInEditor(final IJavaRef javaRef, final boolean tryToUseOld) {
+    if (javaRef == null)
+      return;
+    tryToOpenInEditor(javaRef.getPackageName(), DeclUtil.getTypeNameDollarSignOrNull(javaRef.getDeclaration()),
         javaRef.getLineNumber(), tryToUseOld);
-	}
-	
-	public static void tryToOpenInEditor(final IDecl decl, final boolean tryToUseOld) {
-	  if (decl == null) return;
-	  tryToOpenInEditor(DeclUtil.getPackageNameOrEmpty(decl),
-		        DeclUtil.getTypeNameDollarSignOrNull(decl), 
-		        1, tryToUseOld);
-	}
+  }
 
-	public static void tryToOpenInEditor(final String pkg, final String type,
-			int lineNumber, final boolean tryToUseOld) {
-		tryToOpenInEditor(HistoricalSourceView.class, tryToUseOld ? OLD : null, pkg, 
-				type == null ? SLUtility.PACKAGE_INFO : type,
-				lineNumber);
-	}
+  public static void tryToOpenInEditor(final IDecl decl, final boolean tryToUseOld) {
+    if (decl == null)
+      return;
+    tryToOpenInEditor(DeclUtil.getPackageNameOrEmpty(decl), DeclUtil.getTypeNameDollarSignOrNull(decl), 1, tryToUseOld);
+  }
 
-	public static void tryToOpenInEditor(final String pkg, final String type,
-			final String field) {
-		tryToOpenInEditorUsingFieldName(HistoricalSourceView.class, null, pkg,
-				type, field);
-	}
-
-	public static String tryToMapPath(String path) {
-		final JSureScanInfo info = JSureDataDirHub.getInstance()
-				.getCurrentScanInfo();
-		final Projects projects = info.getProjects();
-		if (projects == null) {
-			return path;
-		}
-		try {
-			final URI uri = new URI(path);
-			for (Config config : projects.getConfigs()) {
-				JavaSourceFile f = config.mapPath(uri);
-				if (f != null) {
-					String mapped = f.file.toURI().toString();
-					if (mapped != null) {
-						return mapped;
-					}
-				}
-			}
-		} catch (URISyntaxException e) {
-			// Nothing to do
-		}
-		return path;
-	}
+  private static void tryToOpenInEditor(final String pkg, final String type, int lineNumber, final boolean tryToUseOld) {
+    tryToOpenInEditor(HistoricalSourceView.class, tryToUseOld ? OLD : null, pkg, type == null ? SLUtility.PACKAGE_INFO : type,
+        lineNumber);
+  }
 }
