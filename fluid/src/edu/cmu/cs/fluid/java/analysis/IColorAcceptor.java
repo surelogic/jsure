03aa4d866e8186c6b1857d4e945b21ca4873d35d
edwin <edwin.chan@surelogic.com>
2014-09-24 12:29:27 -0400

    Removed yet more thread role stuff
    Former-commit-id: 624140011867c1303d165589cdc72875e9b36eb1

--- fluid/src/edu/cmu/cs/fluid/java/analysis/IColorAcceptor.java
+++
@@ -1,9 +0,0 @@
-/*$Header: /cvs/fluid/fluid/src/edu/cmu/cs/fluid/java/analysis/IColorAcceptor.java,v 1.2 2007/07/09 14:08:29 chance Exp $*/
-package edu.cmu.cs.fluid.java.analysis;
-
-
-@Deprecated
-public interface IColorAcceptor {
-  public void accept(
-      IColorVisitor visitor);
-}
