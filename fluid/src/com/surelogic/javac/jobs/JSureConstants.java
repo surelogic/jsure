86eb5ea9b35b68e32c7d5095a8ef060406d5ffdb
edwin <edwin.chan@surelogic.com>
2015-02-25 13:44:35 -0500

    Fixed LocalJSureJob to set up classpath with jsure-common
    Former-commit-id: c47dc7fd5aa3688f2fe3def3dfc77fe1f5133b8d

--- fluid/src/com/surelogic/javac/jobs/JSureConstants.java
+++ fluid/src/com/surelogic/javac/jobs/JSureConstants.java
@@ -1,6 +1,7 @@
 package com.surelogic.javac.jobs;
 
 public final class JSureConstants {
+	public static final String JSURE_COMMON_PLUGIN_ID = "com.surelogic.jsure.common";
 	public static final String JSURE_ANALYSIS_PLUGIN_ID = "com.surelogic.jsure.analysis";
 	public static final String JSURE_TESTS_PLUGIN_ID = "com.surelogic.jsure.tests";
 	public static final String JUNIT_PLUGIN_ID = "org.junit";
