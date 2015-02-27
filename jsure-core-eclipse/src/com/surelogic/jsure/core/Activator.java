865fa13a30f3221521d0b7d702ef20a6216da519
edwin <edwin.chan@surelogic.com>
2015-02-27 13:54:19 -0500

    Moved analyzeInVM() to DebugDriver
    Former-commit-id: 673713aa86e642f282d68bb673ac6293d2893de2

--- jsure-core-eclipse/src/com/surelogic/jsure/core/Activator.java
+++ jsure-core-eclipse/src/com/surelogic/jsure/core/Activator.java
@@ -1,11 +1,15 @@
 package com.surelogic.jsure.core;
 
 import java.io.File;
+import java.util.*;
+import java.util.logging.Level;
 
-import org.eclipse.core.runtime.Plugin;
+import org.eclipse.core.runtime.*;
 import org.osgi.framework.BundleContext;
 
 import com.surelogic.common.FileUtility;
+import com.surelogic.common.java.JavaProject;
+import com.surelogic.common.logging.SLLogger;
 import com.surelogic.java.persistence.JSureScan;
 import com.surelogic.jsure.core.driver.JavacDriver;
 import com.surelogic.jsure.core.preferences.JSurePreferencesUtility;
@@ -56,13 +60,35 @@ public class Activator extends Plugin {
 		Eclipse.initialize();
 
 		// NotificationHub.addAnalysisListener(ConsistencyListener.prototype);
-		JavacDriver.getInstance();
+		JavacDriver.initInstance(getDriver());
 
 		// Clean out empty paths in client's XML promises
 		PromisesLibMerge.removeEmptyPathsOnClient();
 
 		// Try to update the client's XML
 		PromisesLibMerge.mergeJSureToLocal();
+	}
+
+	@SuppressWarnings({ "unchecked" })
+	private JavacDriver<? extends JavaProject> getDriver() {
+		IExtensionRegistry reg = Platform.getExtensionRegistry();
+		IExtensionPoint ep = reg.getExtensionPoint("com.surelogic.jsure.core.jsureDriver");
+		IExtension[] extensions = ep.getExtensions();
+		for (int i = 0; i < extensions.length; i++) {
+			IExtension ext = extensions[i];
+			IConfigurationElement[] ce =
+					ext.getConfigurationElements();
+			for (int j = 0; j < ce.length; j++) {
+				try {
+					Object obj = ce[j].createExecutableExtension("class");
+					return (JavacDriver<? extends JavaProject>) obj;
+				} catch (CoreException e) {
+					SLLogger.getLogger().log(Level.SEVERE, "Error while instantiating extension "+ext.getUniqueIdentifier(), e);
+					continue;
+				}
+			}
+		}
+		return null;
 	}
 
 	private void cleanupJSureData() {
---
+++ jsure-debug-views/src/com/surelogic/jsure/debug/DebugDriver.java
@@ -0,0 +1,58 @@
+package com.surelogic.jsure.debug;
+
+import java.io.File;
+
+import com.surelogic.common.java.JavaProjectSet;
+import com.surelogic.common.jobs.SLProgressMonitor;
+import com.surelogic.dropsea.ir.utility.ClearStateUtility;
+import com.surelogic.javac.JavacProject;
+import com.surelogic.javac.Projects;
+import com.surelogic.javac.Util;
+import com.surelogic.javac.jobs.RemoteJSureRun;
+import com.surelogic.jsure.core.driver.JavacDriver;
+
+public class DebugDriver extends JavacDriver<JavacProject> {
+	public DebugDriver() {
+		super(Projects.javaFactory);
+	}
+
+	@Override
+	protected AnalysisJob makeAnalysisJob(JavaProjectSet<JavacProject> newProjects, File target, File zips, boolean useSeparateJVM) {
+		return new AnalysisJob(null, newProjects, target, zips, useSeparateJVM) {
+			@Override
+			protected boolean analyzeInVM(SLProgressMonitor monitor) throws Exception {
+				File tmpLocation;
+				/*
+				if (clearBeforeAnalysis || oldProjects == null) {
+					// ClearProjectListener.clearJSureState();
+
+					tmpLocation = Util.openFiles(projects, true);
+				} else {
+					tmpLocation = Util.openFiles(oldProjects, projects, true);
+				}
+				*/
+				if (oldProjects != null) {
+					throw new IllegalStateException("Can't delta off another scan");
+				}
+				tmpLocation = Util.openFiles((Projects) projects, true);
+				boolean ok = tmpLocation != null;
+				// Persist the Sea
+				//final File tmpLocation = RemoteJSureRun.snapshot(System.out, projects.getLabel(), projects.getRunDir());
+				/*
+		    SeaStats.createSummaryZip(new File(projects.getRunDir(), RemoteJSureRun.SUMMARIES_ZIP), Sea.getDefault().getDrops(),
+		        SeaStats.splitByProject, SeaStats.STANDARD_COUNTERS);
+		    System.out.println("Finished " + RemoteJSureRun.SUMMARIES_ZIP);
+				 */
+
+				// Create empty files
+				/*
+		    final File log = new File(projects.getRunDir(), RemoteJSureRun.LOG_NAME);
+		    log.createNewFile();
+				 */
+				ClearStateUtility.clearAllState();
+				RemoteJSureRun.renameToFinalName(System.out, projects.getRunDir(), tmpLocation);
+				return ok;
+			}
+		};
+	}
+}
